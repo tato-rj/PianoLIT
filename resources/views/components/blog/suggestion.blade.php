075b8417ff4a9b63f33df439f958ca0ba93ef995
Arthur Villar <=>
2019-03-26 13:54:06 -0400

    Increase suggestion card size for mobile

--- resources/views/components/blog/suggestion.blade.php
+++ resources/views/components/blog/suggestion.blade.php
@@ -1,4 +1,4 @@
-<div class="col-lg-3 col-md-6 col-12 p-2">
+<div class="col-lg-3 col-md-6 col-12 p-2 suggestion-card">
 	<div class="card w-100 hover-shadow t-2">
 		<a class="link-none" href="{{route('posts.show', $suggestion->slug)}}">
 			<div class="card-img-top bg-align-center" style="background-image: url({{$suggestion->cover_image()}}); height: 100px"></div>
