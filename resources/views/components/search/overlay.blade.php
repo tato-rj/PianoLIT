5b9528efedd2008bb529e45c447716a361513efc
Arthur Villar <=>
2019-03-26 23:10:27 -0400

    Fix scrolling issue on overlay

--- resources/views/components/search/overlay.blade.php
+++ resources/views/components/search/overlay.blade.php
@@ -1,6 +1,6 @@
 @component('layouts.overlay', ['name' => 'search', 'opacity' => 1])
 <div class="container py-5">
-	<div class="row" style="overflow-y: scroll;">
+	<div class="row">
 		<div class="col-lg-10 col-12 mx-auto">
 			<input type="text" name="search" class="border-bottom p-4 text-lead rounded-0 bg-transparent border-grey w-100 mb-4" 
 				style="font-size: 3em; border-top: 0; border-left: 0; border-right: 0;" placeholder="What's on your mind?" data-url="{{route('api.blog.search')}}">
