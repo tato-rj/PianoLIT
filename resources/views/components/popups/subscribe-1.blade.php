a515e18104bad4ebab1ea4ad30c3b8895bac4837
Arthur Villar <=>
2019-03-27 23:30:37 -0400

    Popup upgrade

--- resources/views/components/popups/subscribe-1.blade.php
+++ resources/views/components/popups/subscribe-1.blade.php
@@ -1,18 +1,18 @@
 @component('layouts.overlay', ['name' => 'subscribe'])
-<div class="row flex-center w-100 h-100">
-	<div class="col-lg-4 col-md-8 col-10 mx-auto rounded text-center p-6 bg-white shadow-light">
+<div class="row align-items-center h-100">
+	<div class="col-lg-4 col-md-8 col-md-8 col-11 mx-auto rounded text-center p-6 bg-white shadow-light">
 		<img src="{{asset('images/misc/subscribe-flow.svg')}}" width="200" class="mb-4">
 		<div class="mb-4">
 			<div>Enjoy our latest posts directly in your inbox</div>
 		</div>
 		<form method="POST" action="{{route('subscriptions.store')}}">
 			@csrf
-			<div class="input-group mb-2">
-				<input type="email" name="email" class="form-control p-3" style="border-top-left-radius: 32px; border-bottom-left-radius: 32px;" placeholder="Your email address">
-				<div class="input-group-append">
-					<button type="submit" class="btn btn-primary btn-wide" style="border-top-right-radius: 32px; border-bottom-right-radius: 32px;">Sign up</button>
-				</div>
+			<div class="form-group">
+				<input type="email" name="email" class="form-control input-center" placeholder="Your email address">
 			</div>
+
+			<button type="submit" class="btn btn-primary btn-block mb-2">Sign up</button>
+			
 			<div class="text-muted"><small>Ps: we'll never share your email with anyone</small></div>
 		</form>
 	</div>
--- resources/views/layouts/overlay.blade.php
+++ resources/views/layouts/overlay.blade.php
@@ -1,6 +1,6 @@
 <div id="{{$name}}-overlay" class="position-fixed w-100 h-100vh z-40" style="top: 0; left: 0; display: none; overflow-y: auto">
 	<button data-target="#{{$name}}-overlay" class="close-overlay absolute-top-right bg-transparent border-0"><img src="{{asset('images/icons/close-dark.svg')}}" width="20"></button>
-	<div class="w-100 h-100" style="background: rgba(255,255,255,{{$opacity ?? 0.94}});">
+	<div class="container-fluid w-100 h-100" style="background: rgba(255,255,255,{{$opacity ?? 0.94}});">
 
 			{{$slot}}
 
