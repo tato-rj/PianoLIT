67e39eb03ebf81c594a038c0953b48fcbc2596ee
Arthur Villar <=>
2019-03-28 21:13:26 -0400

    Fix overlay close button color

--- resources/views/layouts/overlay.blade.php
+++ resources/views/layouts/overlay.blade.php
@@ -1,5 +1,6 @@
 <div id="{{$name}}-overlay" class="position-fixed w-100 h-100vh z-40" style="top: 0; left: 0; display: none; overflow-y: auto">
-	<button class="absolute-top-right bg-transparent border-0"><img data-target="#{{$name}}-overlay" class="close-overlay" src="{{asset('images/icons/close-dark.svg')}}" width="20"></button>
+	<button class="absolute-top-right bg-transparent border-0">
+		<img data-target="#{{$name}}-overlay" class="close-overlay mt-2" src="{{empty($background) ? asset('images/icons/close-dark.svg') : asset('images/icons/close.svg')}}" width="30"></button>
 	<div data-target="#{{$name}}-overlay" class="close-overlay d-flex justify-content-center align-items-{{$position}} w-100 h-100" style="background: rgba({{$background ?? '255,255,255,0.94'}});">
 
 			{{$slot}}
