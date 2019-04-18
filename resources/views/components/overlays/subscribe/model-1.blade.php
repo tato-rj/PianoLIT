0daf0f758f2180f464f936f9627b4728b3ff67ff
Arthur Villar <=>
2019-04-18 12:52:21 -0400

    Fix subscription modal

--- resources/views/components/overlays/subscribe/model-1.blade.php
+++ resources/views/components/overlays/subscribe/model-1.blade.php
@@ -6,7 +6,16 @@
 			<div>Enjoy our latest posts directly in your inbox</div>
 		</div>
 		
-		@include('components.form.subscription')
+		<form method="POST" action="{{route('subscriptions.store')}}">
+			@csrf
+			<input type="hidden" name="subscription_name" placeholder="Your name here">
+			<div class="form-group">
+				<input required type="email" name="email" placeholder="EMAIL ADDRESS" class="input-center form-control w-100 input-light">
+			</div>
+			@include('components/form/error', ['field' => 'email'])
+			<button type="submit" class="btn btn-primary shadow btn-block mb-2">{{$label ?? 'SIGN UP'}}</button>
+			<div class="text-muted"><small>Ps: we'll never share your email with anyone</small></div>
+		</form>
 	</div>
 </div>
 @endcomponent
