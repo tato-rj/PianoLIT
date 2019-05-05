dcea65daac89f3d4356349918678737ec4d23c0e
Arthur Villar <=>
2019-05-05 19:55:00 -0400

    Check user id instead of searching in the table before incrementing a piece views count

--- app/Http/Controllers/PieceViewsController.php
+++ app/Http/Controllers/PieceViewsController.php
@@ -9,7 +9,7 @@ class PieceViewsController extends Controller
 {
     public function store(Request $request)
     {
-    	if (! in_array($request->user_id, ['196', '244']));
+    	if ($request->user_id != 196 && $request->user_id != 244);
 	        Piece::findOrFail($request->piece_id)->views()->create(['user_id' => $request->user_id]);
      
         return response(200);
