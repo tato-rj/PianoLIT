6b063ed2d39edb86e8af4002115e5ac525d4b3d0
Arthur Villar <=>
2019-05-05 19:24:32 -0400

    Ignore pieces views from our personal accounts

--- app/Http/Controllers/PieceViewsController.php
+++ app/Http/Controllers/PieceViewsController.php
@@ -9,7 +9,10 @@ class PieceViewsController extends Controller
 {
     public function store(Request $request)
     {
-        Piece::findOrFail($request->piece_id)->views()->create(['user_id' => $request->user_id]);
+    	$user = User::findOrFail($request->user_id);
+
+    	if (! in_array($user->email, ['arthurvillar@gmail.com', 'mark@twain.com']));
+	        Piece::findOrFail($request->piece_id)->views()->create(['user_id' => $request->user_id]);
      
         return response(200);
     }
