be1a531232075d9c6d3dec6564d0fe49abcabcef
Arthur Villar <=>
2019-05-07 14:27:32 -0400

    Improve graphs page

--- resources/views/admin/pages/pieces/play-icon.blade.php
+++ resources/views/admin/pages/pieces/play-icon.blade.php
@@ -7,6 +7,6 @@
 	<div class="position-absolute rounded-circle bg-secondary text-white font-weight-bold d-flex align-items-center justify-content-center" style="bottom: 0; right: 2px; width: 10px; height: 10px; font-size: .5em" title="This pieces has only {{$piece->recordingsAvailable}} {{str_plural('recording', $piece->recordingsAvailable)}}">{{$piece->recordingsAvailable}}</div>
 	@endif
 </div>
-@elseif($piece->youtube_array)
+@else
 	<a href="#" title="This piece has no recordings" class="text-muted mr-1"><i class="fas fa-microphone"></i></a>
 @endif
