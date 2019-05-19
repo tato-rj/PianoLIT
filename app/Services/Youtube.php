ed3e8b0e7c718cae9908b505c651591afd4a42ad
Arthur Villar <=>
2019-05-19 13:52:17 -0400

    Add highlight video

--- app/Services/Youtube.php
+++ app/Services/Youtube.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 class Youtube
 {
+	protected $highlight = '4V5OoQ8OLjY';
 	protected $favorites = ['RA_oZiqinFs', '78QlBf04SXs', 'CV17BV-GNvw', 'ucFvIzoBEeE', 'o6QzqCFTfDM', 'InZvYHx5Huw', '0VG-x-qlfUw', 'Sql0OJu0vwY', 'EKeESVEBhqY'];
 
 	public function favorites($number)
@@ -12,6 +13,6 @@ class Youtube
 
 		shuffle($codes);
 		
-		return array_slice($codes, 0, $number);
+		return array_unshift(array_slice($codes, 0, $number), $this->highlight);
 	}
 }
