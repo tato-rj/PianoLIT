92676faf9173853da17790094e6a2ad81e441f28
Arthur Villar <=>
2019-06-02 13:52:44 -0400

    Separate composers with few pieces on admin stats page

--- resources/views/admin/pages/stats/row.blade.php
+++ resources/views/admin/pages/stats/row.blade.php
@@ -6,7 +6,7 @@
 		</div>
 	    <canvas id="{{$id}}" class="w-100" height="300" data-records="{{$data}}"></canvas>
 		@if(!empty($footer))
-		<div class="ml-2 mt-4">
+		<div class="ml-2 mt-2">
 			<span class="badge badge-warning mr-2">Note</span><small>{{$footer}}</small>
 		</div>
 		@endif
