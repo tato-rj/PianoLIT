2fe6c6debb4f58a7fa67bfdf06a152c1d4e1f2b1
Arthur Villar <=>
2019-06-06 01:50:21 -0400

    Create stats page for composers

--- resources/views/admin/pages/stats/composers/birthdays.blade.php
+++ resources/views/admin/pages/stats/composers/birthdays.blade.php
@@ -1,7 +1,7 @@
 <div class="border py-4 px-3 h-100">
     <div class="ml-2 mb-4">
         <h4 class="mb-1"><strong><i class="fas fa-birthday-cake mr-2"></i>Upcoming birthdays</strong></h4>
-        <p class="text-muted">There {{str_plural('is', $upcomingBirthdays->count())}} {{$upcomingBirthdays->count()}} {{str_plural('birthday', $upcomingBirthdays->count())}} over the next 10 days.</p>
+        <p class="text-muted">We found {{$upcomingBirthdays->count()}} {{str_plural('birthday', $upcomingBirthdays->count())}} over the next 10 days.</p>
     </div>
     <div class="mx-2">
         @if($upcomingBirthdays->count() > 0)
--- resources/views/admin/pages/stats/composers/deathdays.blade.php
+++ resources/views/admin/pages/stats/composers/deathdays.blade.php
@@ -1,7 +1,7 @@
 <div class="border py-4 px-3 h-100">
     <div class="ml-2 mb-4">
         <h4 class="mb-1"><strong><i class="fas fa-skull mr-2"></i>Upcoming deathdays</strong></h4>
-        <p class="text-muted">There {{str_plural('is', $upcomingDeathdays->count())}} {{$upcomingDeathdays->count()}} {{str_plural('deathday', $upcomingDeathdays->count())}} over the next 10 days.</p>
+        <p class="text-muted">We found {{$upcomingDeathdays->count()}} {{str_plural('deathday', $upcomingDeathdays->count())}} over the next 10 days.</p>
     </div>
     <div class="mx-2">
         @if($upcomingDeathdays->count() > 0)
