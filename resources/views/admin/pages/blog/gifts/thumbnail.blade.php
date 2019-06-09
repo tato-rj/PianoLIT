4a2f6cb4177114ef0e673044da04f668f97d0b7b
Arthur Villar <=>
2019-06-09 16:54:24 -0400

    Adjust space between copy and delete thumbnails

--- resources/views/admin/pages/blog/gifts/thumbnail.blade.php
+++ resources/views/admin/pages/blog/gifts/thumbnail.blade.php
@@ -4,7 +4,7 @@
 		<div class="show-on-hover">
 		    <div class="m-0 absolute-center z-10 text-center">
 		        <a class="link-none" href="">
-		        	<div class="cursor-pointer text-warning clip mb-2" data-clipboard-text="/storage/{{$file}}" data-toggle="tooltip" title="Copied!" data-trigger="manual"><strong>copy</strong></div>
+		        	<div class="cursor-pointer text-warning clip mb-4" data-clipboard-text="/storage/{{$file}}" data-toggle="tooltip" title="Copied!" data-trigger="manual"><strong>copy</strong></div>
 		        </a>
 		        <button class="border-0 bg-transparent p-0 text-danger cursor-pointer remove-file font-weight-bold" data-path="{{route('admin.posts.gifts.destroy', ['path' => $file])}}">delete</button>
 		    </div>
