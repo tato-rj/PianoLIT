30dcb7cd201df5be104ef7802a51a25d2e025eb7
Arthur Villar <=>
2019-06-10 14:23:46 -0400

    Create circle of fifths page

--- resources/views/tools/circle/index.blade.php
+++ resources/views/tools/circle/index.blade.php
@@ -2,37 +2,26 @@
 
 @push('header')
 <style type="text/css">
-#circle {
-	border: 6px solid #f8f9fa;
-	border-radius: 50%;
-	width: 420px;
-	height: 420px;
-	position: relative;
+svg#target {
+	transition-duration: .5s;
+	z-index: 1;
 }
-
-.key-letter {
-    color: lightgrey;
-    background: #e7ebee; 
-    width: 80px;
-    height: 80px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 50%;
-    font-size: 2.5em;
-    font-weight: bold;
-    position: absolute;
-    top: -40px;
-    left: 160px;
+div#highlight {
+    left: 50%;
+    top: -12px;
+    transform: translateX(-50%);
+    z-index: -1;
+    width: 112%;
+    opacity: .2;
 }
-
-.key-letter:hover {
-	color: #1876f6;
-	background: #e7ebee;
+g#LETTERS > g {
+	opacity: 0.5;
+	transition: .2s;
+	-webkit-filter: grayscale(100%); /* Safari 6.0 - 9.0 */
+	filter: grayscale(100%);
 }
-
-.key-letter:nth-of-type(1) {
-	
+g.key {
+	opacity: 1!important;
 }
 </style>
 @endpush
@@ -40,9 +29,23 @@
 @section('content')
 <div class="container">
 	<div class="row h-100vh">
-		<div class="col-6 p-5">
-			<div id="circle">
-				<div class="key-letter t-2 cursor-pointer">C</div>
+		<div class="col-lg-6 col-md-6 col-12 p-5">
+			<div id="wheel-container" class="w-100 position-relative">
+				@include('tools.circle.wheel')
+			</div>
+		</div>
+		<div class="col-lg-6 col-md-6 col-12" id="labels-container" style="display: none;">
+			<div class="mb-3">
+				<label class="text-muted m-0">KEY</label>
+				<h4 id="key-name"></h4>
+			</div>
+			<div class="mb-3">
+				<label class="text-muted m-0">RELATIVE</label>
+				<h4 id="key-relative"></h4>
+			</div>
+			<div class="mb-3">
+				<label class="text-muted m-0">NEIGHBORS</label>
+				<div id="key-neighbors"></div>
 			</div>
 		</div>
 	</div>
@@ -50,5 +53,125 @@
 @endsection
 
 @push('scripts')
+<script type="text/javascript">
+///////////////////////////////
+// -------  rotate  -------- //
+///////////////////////////////
 
+(function() {
+  let init, rotate, start, stop,
+  	$labels = $('#labels-container'),
+	$letters = $('g#LETTERS > g'),
+    active = false,
+    angle = 0,
+    rotation = 0,
+    startAngle = 0,
+    center = {
+      x: 0,
+      y: 0
+    },
+    R2D = 180 / Math.PI,
+    rot = document.getElementById('target');
+
+  init = function() {
+    rot.addEventListener("mousedown", start, false);
+
+    $(document).bind('mousemove', function(event) {
+      if (active === true) {
+        event.preventDefault();
+	  	$letters.removeClass('key');
+	  	$labels.fadeOut();
+        rotate(event);
+      }
+    });
+    $(document).bind('mouseup', function(event) {
+		event.preventDefault();
+		stop(event);
+	    setTimeout( function() {
+	    	highlightKey();
+	    }, 600);
+    });
+  };
+
+  start = function(e) {
+    e.preventDefault();
+
+    let bb = this.getBoundingClientRect(),
+      t = bb.top,
+      l = bb.left,
+      h = bb.height,
+      w = bb.width,
+      x, y;
+    center = {
+      x: l + (w / 2),
+      y: t + (h / 2)
+    };
+    x = e.clientX - center.x;
+    y = e.clientY - center.y;
+    startAngle = R2D * Math.atan2(y, x);
+
+    return active = true;
+  };
+
+  rotate = function(e) {
+    e.preventDefault();
+    let x = e.clientX - center.x,
+      y = e.clientY - center.y,
+      d = R2D * Math.atan2(y, x);
+
+    rotation = snap(d - startAngle);
+
+    $(rot).css('transform', "rotate(" + (angle + rotation) + "deg)");
+  };
+
+  stop = function() {
+    angle += rotation;
+    return active = false;
+  };
+
+  snap = function(rotation) {
+  	return Math.ceil(rotation/30)*30;
+  };
+
+  highlightKey = function(angle) {
+  	let offsets = [];  	
+  	$letters.each(function() {
+  		offsets.push($(this).offset().top);
+  	});
+  	
+  	offsets.sort();
+  	
+  	$letters.each(function() {
+  		if ($(this).offset().top == offsets[0]) {
+  			$(this).addClass('key');
+  			showDescription();
+	  		return false;
+	  	}
+  	});
+
+  	if ($('g.key').length > 1)
+  		$('g').removeClass('key');
+  };
+
+  showDescription = function() {
+  	let $key = $('g.key');
+  	let relatives = $key.attr('key-major-relatives');
+
+	$('#key-name').text($key.attr('key-major') || '');
+	$('#key-relative').text($key.attr('key-minor') || '');
+
+	if (relatives) {
+		$('#key-neighbors').html('');
+		relatives.split('|').forEach(function(relative) {
+			$('#key-neighbors').append('<span class="shadow-sm badge badge-light mr-2">'+relative+'</span>');
+		});
+	}
+
+	$labels.fadeIn();
+  };
+
+  init();
+
+}).call(this);
+</script>
 @endpush
