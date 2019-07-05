ee86cdec6d2786766905c13ac885d66b679b2297
Arthur Villar <=>
2019-07-05 14:37:48 -0400

    Add reference example

--- resources/views/admin/pages/blog/post/references/layout.blade.php
+++ resources/views/admin/pages/blog/post/references/layout.blade.php
@@ -1,6 +1,7 @@
 <div class="rounded bg-light px-3 py-2 mb-3">
 	<div class="d-flex justify-content-between pb-1 mb-3 text-brand border-bottom">
-	  <p class="m-0"><strong>REFERENCES</strong></p>
+	  <div><strong>REFERENCES</strong></div>
+    <div><small class="text-muted"><strong>ex:</strong> James, H. (1937). The ambassadors. New York, NY: Scribner.</small></div>
 	</div>
 
   @include('admin.pages.blog.post.references.input', [
