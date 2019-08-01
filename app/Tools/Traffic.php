7d422dee4fcc8d1c0262a97f961aacf53d2e7c0c
Arthur Villar <=>
2019-08-01 10:55:59 -0400

    Update IP

--- app/Tools/Traffic.php
+++ app/Tools/Traffic.php
@@ -4,7 +4,11 @@ namespace App\Tools;
 
 class Traffic
 {
-	protected $ignore = ['73.126.141.15', '24.215.182.64', '104.129.138.238'];
+	protected $ignore = [
+		'73.126.137.37', 
+		'24.215.182.64', 
+		'104.129.138.238'
+	];
 
 	public function isRealVisitor()
 	{
