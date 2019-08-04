97da9f516ace454308cd85e81a6129837666cd0d
Arthur Villar <=>
2019-08-04 15:39:39 -0400

    Fix missing third in a diminished chord

--- resources/views/tools/chords/results/labels/type.blade.php
+++ resources/views/tools/chords/results/labels/type.blade.php
@@ -2,11 +2,19 @@
 <div class="p-2 text-muted">
 	<p>Once each interval has been named, we first need to determine if the chord is major, minor, diminished or augmented. To do that, all we need is the <strong>3rd</strong> and the <strong>5th</strong>.</p>
 	@php
-	$third = (new \App\Resources\ChordFinder\Label([]))->find($inversion, 3);
-	$fifth = (new \App\Resources\ChordFinder\Label([]))->find($inversion, 5);
+		$third = (new \App\Resources\ChordFinder\Label([]))->find($inversion, 3);
+		$fifth = (new \App\Resources\ChordFinder\Label([]))->find($inversion, 5);
+
+		if ($third) {
+			$thirdType = 'has a <strong>' . $third['name'] . '</strong>';
+		} elseif ($fifth['type'] == 'diminished') {
+			$thirdType = '<strong>is missing the 3rd</strong>, but it has a diminished 5th, so we treat it as having a minor 3rd'
+		} else {
+			$thirdType = '<strong>is missing the 3rd</strong>, so we treat it as having a major 3rd';
+		}
 	@endphp
 	<p>In this case, the chord 
-		{!! $third ? 'has a <strong>' . $third['name'] . '</strong>' : '<strong>is missing the 3rd</strong>, so we treat it as having a major 3rd' !!} 
+		{!! $thirdType !!} 
 		and it 
 		{!! $fifth ? 'has a <strong>' . $fifth['name'] . '</strong>' : '<strong>is missing the 5th</strong>, so we assume it is a perfect 5th' !!}. 
 		That is why we consider it <strong>{{$inversion['label']['type'] ?? 'major'}}</strong>.
