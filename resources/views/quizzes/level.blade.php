3f92c002648e16e725aeb5f279f667f99676ac3e
Arthur Villar <=>
2019-08-22 13:56:21 -0400

    Add a new level to quizzes

--- resources/views/quizzes/level.blade.php
+++ resources/views/quizzes/level.blade.php
@@ -3,7 +3,7 @@
 	<label>This quiz is <strong>{{$quiz->level->name}}</strong></label>
 	<div class="w-100 d-flex px-5" style="height: 12px">
 		@for($i=0; $i<5; $i++)
-		<div class="m-1 bg-{{$i > $quiz->level->index ? 'light' : $quiz->level->color}} rounded h-100 flex-grow-1" style="opacity: {{4 + $i/6}}"></div>
+		<div class="m-1 bg-{{$i > $quiz->level->index ? 'light' : $quiz->level->color}} rounded h-100 flex-grow-1" style="opacity: {{(4 + $i)/10}}"></div>
 		@endfor
 	</div>
 </div>
