b608ef36db329854e3c3bb0dd4bd42de5db0f85e
Arthur Villar <=>
2019-08-22 13:58:44 -0400

    Add a new level to quizzes

--- resources/views/quizzes/level.blade.php
+++ resources/views/quizzes/level.blade.php
@@ -1,6 +1,6 @@
 <div class="text-center mb-5">
-	<div class="text-muted"><small>DISCLAIMER</small></div>
-	<label>This quiz is <strong>{{$quiz->level->name}}</strong></label>
+	<div class="badge badge-pill badge-light"><small>DISCLAIMER</small></div>
+	<label class="d-block">This quiz is <strong>{{$quiz->level->name}}</strong></label>
 	<div class="w-100 d-flex px-5" style="height: 12px">
 		@for($i=0; $i<5; $i++)
 		<div class="m-1 bg-{{$i > $quiz->level->index ? 'light' : $quiz->level->color}} rounded h-100 flex-grow-1" style="opacity: {{(4 + $i)/10}}"></div>
