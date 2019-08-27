321796d7defa445fc67258494557c31225855020
Arthur Villar <=>
2019-08-27 15:02:43 -0400

    Remove avalanche from youtube links

--- app/Services/Youtube.php
+++ app/Services/Youtube.php
@@ -5,7 +5,7 @@ namespace App\Services;
 class Youtube
 {
 	protected $highlight = '4V5OoQ8OLjY';
-	protected $favorites = ['RA_oZiqinFs', '78QlBf04SXs', 'CV17BV-GNvw', 'ucFvIzoBEeE', 'o6QzqCFTfDM', 'InZvYHx5Huw', '0VG-x-qlfUw', 'Sql0OJu0vwY', 'EKeESVEBhqY'];
+	protected $favorites = ['RA_oZiqinFs', 'pXBmdB37TY8', 'CV17BV-GNvw', 'ucFvIzoBEeE', 'o6QzqCFTfDM', 'InZvYHx5Huw', '0VG-x-qlfUw', 'Sql0OJu0vwY', 'EKeESVEBhqY'];
 
 	public function favorites($number)
 	{
