7023474ffdef66474a61ec53ef14504e855f751a
Arthur Villar <=>
2019-09-06 13:57:17 -0400

    Add max width to input note

--- resources/views/tools/chords/inputs/note.blade.php
+++ resources/views/tools/chords/inputs/note.blade.php
@@ -1,4 +1,4 @@
-<div class="col-lg-1 col-md-2 col-sm-3 col-4 p-1" style="max-width: 120px">
+<div class="col-lg-1 col-md-2 col-sm-3 col-4 p-1" style="max-width: 110px">
 	<div class="text-center note note-inactive t-2" data-name="{{$note}}" data-octave="{{$octave ?? null}}">
 		<h1 class="border rounded-top border-2x font-weight-bold m-0 cursor-pointer">{{$note}}<span data-steps="0"></span></h1>
 		<div class="d-flex justify-content-between border-bottom border-2x rounded-bottom px-2 pb-1">
