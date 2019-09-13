f635b9253b38360c92dc38a4cba67526bcee08df
Arthur Villar <=>
2019-09-13 14:45:43 -0400

    Create pianists tool page

--- resources/views/tools/pianists/card.blade.php
+++ resources/views/tools/pianists/card.blade.php
@@ -1,4 +1,4 @@
-<div class="col-lg-2 col-md-4 col-6 px-3 py-2 text-center">
+<div class="col-lg-2 col-md-4 col-6 text-center">
 	<a class="link-none" href="{{route('tools.pianists.show', $pianist->slug)}}">
 		<img src="{{storage($pianist->cover_path)}}" style="width: 100px" class="rounded-circle mb-2 shadow">
 		<p class="m-0"><strong>{{$pianist->name}}</strong></p>
