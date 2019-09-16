9aec4b6375a5bd54a3494ba155a7562fa4871b2e
Arthur Villar <=>
2019-09-16 11:39:43 -0400

    Randomize subscribe gift

--- resources/views/components/overlays/subscribe/model-2.blade.php
+++ resources/views/components/overlays/subscribe/model-2.blade.php
@@ -1,7 +1,8 @@
 @component('layouts.overlay', ['name' => 'gift', 'light' => false, 'position' => 'center', 'background' => '0,0,0,0.8'])
 <div class="mx-3" style="max-width: 512px">
-	<div class="rounded text-center shadow-light">
-		<img src="{{asset('images/gifts/modes.jpg')}}" class="w-100 rounded-top">
+	<div class="rounded text-center shadow-light bg-white pt-4">
+		@php($gifts = ['modes', 'chords', 'intervals', ' rhythm-values'])
+		<img src="{{asset('images/gifts/'.randval($gifts).'.jpg')}}" class="rounded-top border" style="width: 210px">
 		<div class="px-4 py-3 bg-white rounded-bottom">
 			<div class="mb-4">
 				<div>Subscribe today and get a <strong><u>FREE</u></strong> poster in your inbox!</div>
