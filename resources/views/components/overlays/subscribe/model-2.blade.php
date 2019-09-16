2d341dcd66dc3f45e45dbebe62bda84ef2605525
Arthur Villar <=>
2019-09-16 12:06:31 -0400

    Fix subscription gift bug

--- resources/views/components/overlays/subscribe/model-2.blade.php
+++ resources/views/components/overlays/subscribe/model-2.blade.php
@@ -1,7 +1,7 @@
 @component('layouts.overlay', ['name' => 'gift', 'light' => false, 'position' => 'center', 'background' => '0,0,0,0.8'])
 <div class="mx-3" style="max-width: 512px">
 	<div class="rounded text-center shadow-light bg-white pt-4">
-		@php($gift = randval(['modes', 'chords', 'intervals', ' rhythm-values']).'.jpg')
+		@php($gift = randval(['modes', 'chords', 'intervals', 'rhythm-values']).'.jpg')
 		<img src="{{asset('images/gifts/'.$gift)}}" class="rounded-top border" style="width: 210px">
 		<div class="px-4 py-3 bg-white rounded-bottom">
 			<div class="mb-4">
