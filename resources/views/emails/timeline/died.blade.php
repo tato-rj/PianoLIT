86d5f04a6ead4370a2c9408716dcc640b790f851
Arthur Villar <=>
2019-10-17 15:47:17 -0400

    Create timeline emails

--- resources/views/emails/timeline/died.blade.php
+++ resources/views/emails/timeline/died.blade.php
@@ -4,10 +4,9 @@
 	<img src="{{asset('images/icons/reaper.png')}}" style="margin-right: .5rem; width: 20px">
 	{{$composer->name . ' died ' . now()->diffInYears($composer->date_of_death) . ' years ago on ' . $composer->date_of_death->toFormattedDateString() . '.'}}
 </div>
-@endforeach
-
 @php($timeline = \App\Timeline::fromYear($composer->date_of_death->year))
 @if($timeline->exists())
 @include('emails.timeline.sameyear')
 @endif
+@endforeach
 @endcomponent
