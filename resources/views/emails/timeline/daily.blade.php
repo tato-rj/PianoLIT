a81471c5bbee83f028841d4bb3c0b31619dc8ae5
Arthur Villar <=>
2019-10-17 15:53:50 -0400

    Create timeline emails

--- resources/views/emails/timeline/daily.blade.php
+++ resources/views/emails/timeline/daily.blade.php
@@ -1,13 +1,13 @@
 @component('mail::message')
 # On the {{now()->format('jS \o\f F')}}...
 
-@if($composersBorn->count() > 0)
+{{-- @if($composersBorn->count() > 0)
 @include('emails.timeline.born')
 @endif
 
 @if($composersDied->count() > 0)
 @include('emails.timeline.died')
-@endif
+@endif --}}
 
 @component('mail::button', ['url' => ''])
 Recommend this to a friend
