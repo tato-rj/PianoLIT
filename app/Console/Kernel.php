58e313ed5303cd29764de4aba9a05c49ac8b658b
Arthur Villar <=>
2019-10-18 12:33:15 -0400

    Fix timeline email

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('pianolit:timeline-email')->dailyAt('12:33');
+        $schedule->command('pianolit:timeline-email')->dailyAt('12:34');
     }
 
     /**
