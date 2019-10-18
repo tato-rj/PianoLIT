ddfa11a8e2c77e4d7a41bfea37b2144493d48ccf
Arthur Villar <=>
2019-10-18 02:09:01 -0400

    Improve timeline email

--- resources/views/emails/timeline/born.blade.php
+++ resources/views/emails/timeline/born.blade.php
@@ -1,13 +1,6 @@
-<div style="text-align: center; position: relative; width: 80px; display: block; margin: 0 auto; margin-bottom: 1rem;">
-	<img src="{{asset('images/icons/birthday-cake.png')}}" style="padding: .5rem;">
-	<div style="background-color: #48a59d; color: white; width: 30px; height: 30px; border-radius: 50%; display: flex; justify-content: center; align-items: center; position: absolute; bottom: -10px; right: 0; font-size: 1.2em;">
-		<strong>{{$composersBorn->count()}}</strong>
-	</div>
-</div>
 @component('mail::panel')
 @foreach($composersBorn as $composer)
-<div style="{{$loop->last ? null : 'border-bottom: 1px solid #d5dade; margin-bottom: .5rem; padding-bottom: .5rem'}}">
-	<strong>{{$composer->name}}</strong> was born {{now()->diffInYears($composer->date_of_birth) . ' years ago on ' . $composer->date_of_birth->toFormattedDateString() . '.'}}
-</div>
+<strong>{{$composer->name}}</strong> was born {{now()->diffInYears($composer->date_of_birth) . ' years ago on ' . $composer->date_of_birth->toFormattedDateString() . '.'}}
+<div style="{{$loop->last ? null : 'border-bottom: 1px solid #d5dade; margin-bottom: .5rem; padding-bottom: .5rem'}}"></div>
 @endforeach
 @endcomponent
--- resources/views/emails/timeline/died.blade.php
+++ resources/views/emails/timeline/died.blade.php
@@ -1,13 +1,6 @@
-<div style="text-align: center; position: relative; width: 80px; display: block; margin: 0 auto; margin-bottom: 1rem;">
-	<img src="{{asset('images/icons/reaper.png')}}" style="padding: .5rem;">
-	<div style="background-color: #697a8c; color: white; width: 30px; height: 30px; border-radius: 50%; display: flex; justify-content: center; align-items: center; position: absolute; bottom: -10px; right: 0; font-size: 1.2em;">
-		<strong>{{$composersDied->count()}}</strong>
-	</div>
-</div>
 @component('mail::panel')
 @foreach($composersDied as $composer)
-<div style="border-bottom: {{$loop->last ? null : '1px solid'}}">
-	<strong>{{$composer->name}}</strong> died {{now()->diffInYears($composer->date_of_death) . ' years ago on ' . $composer->date_of_death->toFormattedDateString() . '.'}}
-</div>
+<strong>{{$composer->name}}</strong> died {{now()->diffInYears($composer->date_of_death) . ' years ago on ' . $composer->date_of_death->toFormattedDateString() . '.'}}
+<div style="{{$loop->last ? null : 'border-bottom: 1px solid #d5dade; margin-bottom: .5rem; padding-bottom: .5rem'}}"></div>
 @endforeach
 @endcomponent
