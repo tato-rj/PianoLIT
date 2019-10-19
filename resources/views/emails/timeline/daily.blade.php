b004f2e9705d1c9889ed51ab0fd8378541c1a7cf
Arthur Villar <=>
2019-10-19 17:22:02 -0400

    Add avatar to timeline email

--- resources/views/emails/timeline/daily.blade.php
+++ resources/views/emails/timeline/daily.blade.php
@@ -1,6 +1,6 @@
 @component('mail::message')
 <div style="margin-top: -1.5em;">
-	<div style="text-align: center; font-weight: 18px">🎉👏🤗🎁</div>
+	<div style="text-align: center; font-weight: 18px; margin-bottom: .3em">🎉👏🤗🎁</div>
 	<div style="
 		margin-bottom: 2em;
 		color: #3D4852;
@@ -12,7 +12,7 @@
 @include('emails.timeline.avatar')
 
 <div style="text-align: center; margin-bottom: 1.5em">
-	<div>{{$composer->gender == 'male' ? 'He' : 'She'}} was born {{now()->diffInYears($composer->date_of_birth)}} years ago on</div>
+	<div style="font-size: 16px">{{$composer->gender == 'male' ? 'He' : 'She'}} was born {{now()->diffInYears($composer->date_of_birth)}} years ago on</div>
 	<div style="color: #4dc0b5; font-size: 30px; font-weight: bold">{{$composer->date_of_birth->format('F jS, Y')}}</div>
 </div>
 
