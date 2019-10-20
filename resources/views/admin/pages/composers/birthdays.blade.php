a1f99a1ab02998b71c8819558efa63a91a9fa245
Arthur Villar <=>
2019-10-20 00:00:09 -0400

    Quick overview of all famous birthdays

--- resources/views/admin/pages/composers/birthdays.blade.php
+++ resources/views/admin/pages/composers/birthdays.blade.php
@@ -8,7 +8,7 @@
         </button>
       </div>
       <div class="modal-body">
-        <p>This is a list of the <strong>{{$composers->where('is_famous', true)->count()}} most famous</strong> composers. An email will be generated for <u>each one</u> on its respective birthday.</p>
+        <p>This is a list of the <strong>{{$composers->where('is_famous', true)->count()}} most famous</strong> composers. An email will be generated for <u>each one</u> on the respective birthday.</p>
         <table class="table table-sm table-hover table-borderless">
           @foreach($composers->where('is_famous', true)->sortBy('month_of_birth')->groupBy('month_of_birth') as $month => $list)
           <thead>
