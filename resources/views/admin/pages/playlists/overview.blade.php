264b4241b97450aad93bb26eb870ce2429c606db
Arthur Villar <=>
2019-10-21 12:03:11 -0400

    Generate overview of playlists

--- resources/views/admin/pages/playlists/overview.blade.php
+++ resources/views/admin/pages/playlists/overview.blade.php
@@ -1,5 +1,5 @@
 <div id="playlists-overview" class="modal" tabindex="-1" role="dialog">
-  <div class="modal-dialog modal-lg" role="document">
+  <div class="modal-dialog modal-xl" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title">Overview</h5>
@@ -10,7 +10,7 @@
       <div class="modal-body">
         <div class="row">
           @foreach($playlists as $playlist)
-          <div class="col-lg-4 col-md-6 col-12 mb-2">
+          <div class="col-lg-3 col-md-4 col-12 mb-2">
             <div class="bg-light text-muted mb-2 rounded px-1"><strong><span class="text-blue mr-2">{{$loop->iteration}}</span>{{$playlist->name}}</strong></div>
             <div class="px-1">
               @foreach($playlist->pieces as $piece)
