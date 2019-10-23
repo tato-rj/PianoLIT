8390020e61c21d728710bc24be787c90dce07fd4
Arthur Villar <=>
2019-10-23 11:30:02 -0400

    Fix read notification to bulk ids

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -8,15 +8,13 @@ class NotificationsController extends Controller
 {
     public function read(Request $request)
     {
-
     	if ($request->has('ids')) {
-    		return $request->all();
     		foreach (json_decode($request->ids) as $id) {
 		    	auth()->user()->notifications()->find($id)->markAsRead();
     		}
+    	} else {
+	    	auth()->user()->notifications->markAsRead();
     	}
-
-    	// auth()->user()->notifications->markAsRead();
 
     	return redirect()->to($request->url);
     }
