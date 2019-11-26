8e9c74aab85f911f5a8ec0f2f03d046c08b9f486
Arthur Villar <=>
2019-11-26 11:11:29 -0500

    Fix bug

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,14 +25,15 @@ class HomeController extends Controller
     public function index()
     {
         $collections = collect([
-            $this->api->order(0)->latest(),
-            $this->api->order(1)->composers(),
-            $this->api->order(2)->ranking('abrsm'),
-            $this->api->order(3)->ranking('rcm'),
-            $this->api->order(4)->tag('To improve your', randval(['scales', 'left hand', 'arpeggios'])),
-            $this->api->order(5)->women(),
-            $this->api->order(6)->tag('We love pieces that are', randval(['playful', 'melancholic', 'triumphant'])),
-            $this->api->order(7)->similar(),
+            $this->api->order(0)->latest('Latest pieces'),
+            $this->api->order(1)->composers('Most famous composers'),
+            $this->api->order(2)->women('Women composers'),
+            $this->api->order(3)->levels('Levels'),
+            $this->api->order(4)->ranking('rcm', 'Equivalent to the RCM levels'),
+            $this->api->order(5)->ranking('abrsm', 'Equivalent to the ABRSM levels'),
+            $this->api->order(6)->improve('Improve your'),
+            $this->api->order(7)->tag('Pieces that are '),
+            $this->api->order(8)->periods('Periods'),
         ]);
 
         $tags = Tag::display()->inRandomOrder()->get();
