0d8ce61650ded6846e93297d808339bfdb8f17d6
Arthur Villar <=>
2019-11-28 20:08:54 -0500

    Add youtube search link to admin piece row

--- resources/views/admin/pages/pieces/row.blade.php
+++ resources/views/admin/pages/pieces/row.blade.php
@@ -17,7 +17,7 @@
     </div>
     </div>
   </td>
-  <td>{{$piece->long_name}}</td>
+  <td>{{$piece->long_name}} <a href="{{youtube($piece->long_name . ' by ' . $piece->composer->name)}}" class="link-blue"><i class="fas fa-external-link-alt ml-2"></i></a></td>
   <td style="white-space: nowrap;">{{$piece->composer->short_name}}</td>
   <td style="white-space: nowrap;">
     @if($rcm = $piece->getRanking('rcm'))
