2e4e6b51f58256a89e03d3e5ceb17959ba8a5df0
Arthur Villar <=>
2019-11-30 13:50:36 -0500

    Improve tour

--- app/Api.php
+++ app/Api.php
@@ -14,7 +14,7 @@ class Api
 
     public function __construct()
     {
-        $this->colors = ['purple', 'darkblue', 'lightblue', 'teal', 'green', 'yellow', 'orange', 'red', 'darkpink'];
+        $this->colors = ['lightblue', 'teal', 'green', 'yellow', 'orange', 'red', 'darkpink', 'purple', 'darkblue'];
         $this->limit = mt_rand(12,18);
     }
 
