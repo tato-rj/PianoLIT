a3814b9952c298eb1d98a94ce68053dfd65e9524
Arthur Villar <=>
2019-11-30 19:28:38 -0500

    Fix users admin page

--- resources/views/admin/pages/users/status-icon/expired.blade.php
+++ resources/views/admin/pages/users/status-icon/expired.blade.php
@@ -1,3 +1,5 @@
+@if($user->trial_ends_at)
 <div class="text-muted" title="{{$user->first_name}}'s trial expired on {{$user->trial_ends_at->toFormattedDateString()}}">
 	<i class="fas fa-stopwatch"></i>
 </div>
+@endif
