f78756066513e2ae2c3f7b8bf4e97c93fd95e78a
Arthur Villar <=>
2019-12-16 00:17:38 -0500

    Update birthday email time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('pianolit:timeline-email')->dailyAt('10:00');
+        $schedule->command('pianolit:timeline-email')->dailyAt('7:00');
         $schedule->command('pianolit:admin-report')->weeklyOn(7, '20:30');
     }
 
