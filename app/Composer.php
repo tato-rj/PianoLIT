323640685df4efa20f88d88c6c81546a99bc8e02
Arthur Villar <=>
2019-12-23 15:05:26 -0500

    Optimize discover page queries

--- app/Composer.php
+++ app/Composer.php
@@ -9,7 +9,7 @@ class Composer extends Person
     use HasLimit;
     
     protected $casts = ['is_famous' => 'boolean'];
-    protected $appends = ['cover_image'];
+    protected $appends = ['cover_image', 'alive_on', 'short_name', 'born_at', 'died_at'];
     protected $withCount = ['pieces'];
 
     public function creator()
