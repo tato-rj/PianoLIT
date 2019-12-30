2ff354641ae3290aa18f745bb03be384453f030f
Arthur Villar <=>
2019-12-30 02:10:09 -0500

    Reset redis

--- app/Http/Controllers/CookiesController.php
+++ app/Http/Controllers/CookiesController.php
@@ -8,11 +8,18 @@ class CookiesController extends Controller
 {
 	public function test()
 	{
-        $visitors = \Redis::hgetall('visitor.*');
+		$visitors = [];
+        $records = \Redis::scan('0', 'match', 'visitor.*', 'count', '100')[1];
 
-        foreach ($visitors as $index => $visitor) {
-        	$visitors[$index]['info'] = collect(json_decode($visitor['info']));
-        	$visitors[$index]['visits'] = collect(json_decode($visitor['visits']));
+        foreach ($records as $record) {
+        	$visitor = \Redis::hgetall($record);
+	        dd($visitor);
+	        foreach ($visitor as $index => $data) {
+	        	$visitor[$index]['info'] = collect(json_decode($data['info']));
+	        	$visitor[$index]['visits'] = collect(json_decode($data['visits']));
+	        }
+
+	        array_push($visitors, $visitor);
         }
 
         return $visitors;
@@ -20,31 +27,31 @@ class CookiesController extends Controller
 
     public function store(Request $request)
     {
-		// cookie()->queue(cookie()->forget('visitor_id'));
-    	$visitorId = $request->cookie('visitor_id');
+		cookie()->queue(cookie()->forget('visitor_id'));
+    	// $visitorId = $request->cookie('visitor_id');
 
-    	if (! $visitorId) {
-		    $uniqueId = bcrypt(\Str::random() . time());
+    	// if (! $visitorId) {
+		   //  $uniqueId = bcrypt(\Str::random() . time());
 
-		    $data = [
-		    	'id' => $uniqueId,
-		    	'info' => collect(geoip(request()->ip())->toArray()),
-		    	'visits' => json_encode([['date' => now(config('app.timezone')), 'url' => url()->previous()]])
-		    ];
+		   //  $data = [
+		   //  	'id' => $uniqueId,
+		   //  	'info' => collect(geoip(request()->ip())->toArray()),
+		   //  	'visits' => json_encode([['date' => now(config('app.timezone')), 'url' => url()->previous()]])
+		   //  ];
 
-		    \Redis::hmset("visitor.$uniqueId", $data);
+		   //  \Redis::hmset("visitor.$uniqueId", $data);
 
-		    cookie()->queue(cookie()->forever('visitor_id', $uniqueId));
-    	} else {
-    		$visits = json_decode(\Redis::hgetall('visitor.' . request()->cookie('visitor_id'))['visits']);
-    		array_unshift($visits, ['date' => now(config('app.timezone')), 'url' => url()->previous()]);
+		   //  cookie()->queue(cookie()->forever('visitor_id', $uniqueId));
+    	// } else {
+    	// 	$visits = json_decode(\Redis::hgetall('visitor.' . request()->cookie('visitor_id'))['visits']);
+    	// 	array_unshift($visits, ['date' => now(config('app.timezone')), 'url' => url()->previous()]);
 
-    		$data = [
-		    	'info' => collect(geoip(request()->ip())->toArray()),
-    			'visits' => json_encode($visits)
-    		];
+    	// 	$data = [
+		   //  	'info' => collect(geoip(request()->ip())->toArray()),
+    	// 		'visits' => json_encode($visits)
+    	// 	];
 
-		    \Redis::hmset("visitor.$visitorId", $data);
-    	}
+		   //  \Redis::hmset("visitor.$visitorId", $data);
+    	// }
     }
 }
