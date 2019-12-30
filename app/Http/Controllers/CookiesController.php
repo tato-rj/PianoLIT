78d66085fb60ec0f618b301bf752c9fccb7a20fa
Arthur Villar <=>
2019-12-30 01:46:58 -0500

    Install Redis and GeoIp

--- app/Http/Controllers/CookiesController.php
+++ app/Http/Controllers/CookiesController.php
@@ -19,30 +19,30 @@ class CookiesController extends Controller
 
     public function store(Request $request)
     {
-		// cookie()->queue(cookie()->forget('visitor_id'));
-    	$visitorId = $request->cookie('visitor_id');
+		cookie()->queue(cookie()->forget('visitor_id'));
+    	// $visitorId = $request->cookie('visitor_id');
 
-    	if (! $visitorId) {
-		    $uniqueId = bcrypt(\Str::random() . time());
+    	// if (! $visitorId) {
+		   //  $uniqueId = bcrypt(\Str::random() . time());
 
-		    $data = [
-		    	'info' => collect(geoip(request()->ip())->toArray()),
-		    	'visits' => json_encode([['date' => now(config('app.timezone')), 'url' => url()->previous()]])
-		    ];
+		   //  $data = [
+		   //  	'info' => collect(geoip(request()->ip())->toArray()),
+		   //  	'visits' => json_encode([['date' => now(config('app.timezone')), 'url' => url()->previous()]])
+		   //  ];
 
-		    \Redis::hmset("visitor.$uniqueId", $data);
+		   //  \Redis::hmset("visitor.$uniqueId", $data);
 
-		    cookie()->queue(cookie()->forever('visitor_id', $uniqueId));
-    	} else {
-    		$visits = json_decode(\Redis::hgetall('visitor.' . request()->cookie('visitor_id'))['visits']);
-    		array_unshift($visits, ['date' => now(config('app.timezone')), 'url' => url()->previous()]);
-    		$data = [
-    			'visits' => json_encode($visits)
-    		];
+		   //  cookie()->queue(cookie()->forever('visitor_id', $uniqueId));
+    	// } else {
+    	// 	$visits = json_decode(\Redis::hgetall('visitor.' . request()->cookie('visitor_id'))['visits']);
+    	// 	array_unshift($visits, ['date' => now(config('app.timezone')), 'url' => url()->previous()]);
+    	// 	$data = [
+    	// 		'visits' => json_encode($visits)
+    	// 	];
 
-		    \Redis::hmset("visitor.$visitorId", $data);
-    	}
+		   //  \Redis::hmset("visitor.$visitorId", $data);
+    	// }
 
-    	return \Redis::hgetall('visitor.' . $visitorId);
+    	// return \Redis::hgetall('visitor.' . $visitorId);
     }
 }
