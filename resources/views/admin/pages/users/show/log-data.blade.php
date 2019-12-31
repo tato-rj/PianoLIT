cc4dfb29c2bd9d6f9a4480a7e576cf6c57f5a93d
Arthur Villar <=>
2019-12-31 18:40:01 -0500

    Include log data to app events

--- resources/views/admin/pages/users/show/log-data.blade.php
+++ resources/views/admin/pages/users/show/log-data.blade.php
@@ -1,15 +1,15 @@
 <table class="table table-borderless">
   <thead>
     <tr>
-      <th scope="col">Key</th>
-      <th scope="col">Value</th>
+      <th class="py-2 rounded-left" scope="col">Key</th>
+      <th class="py-2 rounded-right" scope="col">Value</th>
     </tr>
   </thead>
   <tbody>
     @foreach($data as $key => $value)
     <tr>
       <th scope="row">{{snake_str($key)}}</th>
-      <td>{{is_array($value) ? arrayToSentence($value) : $value}}</td>
+      <td style="width: 76%">{{is_array($value) ? arrayToSentence($value) : $value}}</td>
     </tr>
     @endforeach
   </tbody>
