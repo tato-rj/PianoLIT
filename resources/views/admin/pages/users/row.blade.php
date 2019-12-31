d49c4950e6553492909cdfc3030b77aaac90c817
Arthur Villar <=>
2019-12-31 18:52:34 -0500

    Include log data to app events

--- resources/views/admin/pages/users/row.blade.php
+++ resources/views/admin/pages/users/row.blade.php
@@ -41,7 +41,7 @@
       'other' => [
         ['route' => "mailto:$item->email", 'title' => "Send an email to $item->first_name", 'icon' => 'envelope'],
         ['route' => route('impersonate', $item), 'title' => "Impersonate user", 'icon' => 'user-secret'],
-        ['route' => route('admin.users.show', $item), 'title' => "More details", 'icon' => 'eye', 'target' => 'stay']
+        ['route' => route('admin.users.show', $item), 'title' => "More details", 'icon' => 'eye', 'target' => null]
       ],
       'delete' => route('admin.users.destroy', $item)
   ]])
--- resources/views/components/datatable/actions/other.blade.php
+++ resources/views/components/datatable/actions/other.blade.php
@@ -1,3 +1,3 @@
 @foreach($action as $data)
-<a href="{{$data['route']}}" title="{{$data['title']}}" target="{{$target ?? '_blank'}}" class="text-muted mr-2"><i class="fas fa-{{$data['icon']}} align-middle"></i></a>
+<a href="{{$data['route']}}" title="{{$data['title']}}" target="{{! array_key_exists('target', $data) ? '_blank' : null}}" class="text-muted mr-2"><i class="fas fa-{{$data['icon']}} align-middle"></i></a>
 @endforeach
