c14acf0d9fb7105db61cb758511706723f9c8ecc
Arthur Villar <=>
2020-01-03 13:38:26 -0500

    Remove auth only for quizzes

--- resources/views/tools/studio-policies/index.blade.php
+++ resources/views/tools/studio-policies/index.blade.php
@@ -50,11 +50,7 @@
 	</div>
 	
 	<div class="row mb-6">
-		<div class="col-lg-8 col-12 mx-auto">
-			<p><strong>Why do I need a studio policy?</strong></p>
-			<p>An effective studio policy is one of the most important tools a piano teacher has for creating a private studio. Designing your piano studio policy is a great way to go through all of the important aspects of your studio, learn and decide how you will handle the tricky situations that are likely to come by.</p>
-			{{-- <p><a href="" class="link-blue">Click here</a> if you want to learn more.</p> --}}
-		</div>
+		@include('tools.studio-policies.description')
 	</div>
 </div>
 
