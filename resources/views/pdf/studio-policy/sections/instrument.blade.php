3f62d8e31d407e4dfd8f469b883745eee862a027
Arthur Villar <=>
2020-01-02 23:57:56 -0500

    Update studio policy fields

--- resources/views/pdf/studio-policy/sections/instrument.blade.php
+++ resources/views/pdf/studio-policy/sections/instrument.blade.php
@@ -3,16 +3,15 @@
 		<p class="section-title">Instrument</p>
 
 		@if(in_array('acoustic_piano', $policy->get('instrument_type')))
-			<p>I recommend an acoustic piano for my students. It is impossible to replace the sound and touch of an acoustic piano and the vibrations of the strings create harmonic overtones that sound simultaneously, creating a full and rich sound. The phycial reaction you get from the keys in an acoustic piano is not just about the weight: all the tiny parts within the instrument move in sync to produce the sound, creating a direct link between finger motion and sound production. This may sound subtle, but it does make a different in both technical and ear development.</p>
+			<p>I recommend an acoustic piano for my students. It is impossible to replace the sound and touch of an acoustic piano and the vibrations of the strings create harmonic overtones that sound simultaneously, creating a full and rich sound. The physical reaction you get from the keys in an acoustic piano is not just about the weight: all the tiny parts within the instrument move in sync to produce the sound, creating a direct link between finger motion and sound production. This may seem subtle, but it does make a different in both technical and ear development.</p>
 		@endif
 
 		@if(in_array('digital_piano', $policy->get('instrument_type')))
-			<p>{{in_array('acoustic_piano', $policy->get('instrument_type')) ? 'If an acoustic piano is not an option at the moment, I can also' : 'I'}} recommend a good digital piano. They have come a long way in recent years and are an excellent substitute for an acoustic piano.</p>
+			<p>{{in_array('acoustic_piano', $policy->get('instrument_type')) ? 'If an acoustic piano is not an option at the moment, I can also' : 'I'}} recommend a good digital piano. They have come a long way in recent years and may be good substitutes for an acoustic piano.</p>
 			<p><u>Why a digital pianos might be good choice?</u></p>
 			<ul>
 				<li>They take up less space if compared to a grand or even upright piano.</li>
 				<li>They are are more affordable: 1K to 5K as opposed to 10K for a baby grand piano, for example.</li>
-				<li>They are are more affordable: 1K to 5K as opposed to 10K or more.</li>
 				<li>They are always in tune and require little maintenance.</li>
 				<li>They include a variety of nice features: recording options, use of headphones for private practice, different instrumental sounds, etc.</li>
 			</ul>
