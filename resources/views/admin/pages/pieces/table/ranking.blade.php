b27315a65d0dd71a726947dd101b8b6c7003c6f9
Arthur Villar <=>
2020-01-13 23:05:52 -0500

    Update datatables

--- resources/views/admin/pages/pieces/table/ranking.blade.php
+++ resources/views/admin/pages/pieces/table/ranking.blade.php
@@ -1,6 +1,6 @@
-<div class="text-norwap">
+<div class="d-flex">
 @if($rcm = $item->getRanking('rcm'))
-<div class="badge badge-pill alert-blue"><strong>RCM {{$rcm}}</strong></div>
+<div class="mr-1 badge badge-pill alert-blue"><strong>RCM {{$rcm}}</strong></div>
 @endif
 @if($abrsm = $item->getRanking('abrsm'))
 <div class="badge badge-pill alert-blue"><strong>ABRSM {{$abrsm}}</strong></div>
