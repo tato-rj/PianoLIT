e75984fd386fd939c85a7685bbca1aa9e9139e92
Arthur Villar <=>
2020-01-16 22:48:59 -0500

    Update landing page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -291,27 +291,27 @@
         return parseInt(piece * 100 / total);
     }
 
-    let showScrollProgressBar = function(content) {
-        let body = document.body,
-            html = document.documentElement;
+    // let showScrollProgressBar = function(content) {
+    //     let body = document.body,
+    //         html = document.documentElement;
 
-        let pageHeight = Math.max( body.scrollHeight, body.offsetHeight, 
-                               html.clientHeight, html.scrollHeight, html.offsetHeight);
+    //     let pageHeight = Math.max( body.scrollHeight, body.offsetHeight, 
+    //                            html.clientHeight, html.scrollHeight, html.offsetHeight);
 
-        let offset = content.offset().top;
-        let height = content.height();
+    //     let offset = content.offset().top;
+    //     let height = content.height();
 
-        let proportion = (height/pageHeight) + 1;
+    //     let proportion = (height/pageHeight) + 1;
 
-        let $progressbar = $('#page-progress .progress-bar');
+    //     let $progressbar = $('#page-progress .progress-bar');
         
-        if ($progressbar.length) {
-            $(window).scroll(function() {
-                let scrollTop = $(this).scrollTop();
-                $progressbar.css('width', percentage(scrollTop - offset*4, height)*proportion + '%');
-            });
-        }
-    };
+    //     if ($progressbar.length) {
+    //         $(window).scroll(function() {
+    //             let scrollTop = $(this).scrollTop();
+    //             $progressbar.css('width', percentage(scrollTop - offset*4, height)*proportion + '%');
+    //         });
+    //     }
+    // };
     </script>
 
 <script type="text/javascript">
