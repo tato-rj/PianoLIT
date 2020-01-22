fea42ade54f9b407a0a500c289aeefdcf311b543
Arthur Villar <=>
2020-01-21 22:41:03 -0500

    Show piece name in logs

--- app/Http/Controllers/LogsController.php
+++ app/Http/Controllers/LogsController.php
@@ -8,6 +8,6 @@ class LogsController extends Controller
 {
     public function data(Request $request)
     {
-    	return view('admin.pages.users.show.log-data', ['data' => json_decode($request->data), 'url' => $request->url])->render();
+    	return view('admin.pages.users.show.log-data', ['data' => json_decode($request->data), 'url' => json_decode($request->url)])->render();
     }
 }
