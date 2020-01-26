8694ef4016dfc986befa7eedd89600de3daa9653
Arthur Villar <=>
2020-01-26 17:52:57 -0500

    Fix email preview

--- resources/views/emails/lists/freepick.blade.php
+++ resources/views/emails/lists/freepick.blade.php
@@ -8,7 +8,7 @@
     "><span class="text-blue">Free</span> pick of the week</h1>
 
 <div style="width: 100%; background: url({{$piece->getBackground()}}); background-position: center; background-size: cover; position: relative;" class="mb-4">
-	<div style="max-width: 60%; padding-left: 16px; padding-bottom: 16px; padding-top: 80px;">
+	<div style="padding-left: 16px; padding-right: 16px; padding-bottom: 16px; padding-top: 80px;">
 		<h1 style="color: white; margin: 0; font-size: 24px">{{$piece->medium_name}}</h1>
 		<p style="color: white; margin-bottom: 4px">by {{$piece->composer->name}}</p>
 		<p style="color:white; font-size: 12px; margin: 0">
