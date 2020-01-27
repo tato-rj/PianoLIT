023b339c054ffb9dda92f76df57a4d448040a90d
Arthur Villar <=>
2020-01-26 19:50:08 -0500

    Remove major from names of pieces in major keys

--- app/Traits/PieceExtraAttributes.php
+++ app/Traits/PieceExtraAttributes.php
@@ -108,6 +108,8 @@ trait PieceExtraAttributes
         $key = (! in_array($this->key, ['Modal', 'Serial', 'Chromatic', 'Experimental', 'Atonal'])) ? ' in ' . $this->key : null;
         $name .= $this->catalogue_name || $this->collection_number ? " {$this->catalogue}" : $key;
 
+        $name = str_replace(' major', '', $name);
+
         return rm_whitespaces($name);
     }
 
