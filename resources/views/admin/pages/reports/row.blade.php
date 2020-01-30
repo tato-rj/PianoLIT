5ca430ea46792471bca80c55354de9815c39244d
Arthur Villar <=>
2020-01-30 15:23:11 -0500

    Create emails logs page on admin

--- resources/views/admin/pages/reports/row.blade.php
+++ resources/views/admin/pages/reports/row.blade.php
@@ -6,13 +6,13 @@
   
   <td>{{$item->emails_count}}</td>
 
-  <td>{{$item->delivered_count}}</td>
+  <td>{{$item->delivered_count}} <span class="badge badge-light badge-pill">{{percentage($item->delivered_count, $item->emails_count)}}%</span></td>
 
-  <td>{{$item->failed_count}}</td>
+  <td>{{$item->failed_count}} <span class="badge badge-light badge-pill">{{percentage($item->failed_count, $item->emails_count)}}%</span></td>
 
-  <td>{{$item->opens_count}}</td>
+  <td>{{$item->opens_count}} <span class="badge badge-light badge-pill">{{percentage($item->opens_count, $item->emails_count)}}%</span></td>
 
-  <td>{{$item->clicks_count}}</td>
+  <td>{{$item->clicks_count}} <span class="badge badge-light badge-pill">{{percentage($item->clicks_count, $item->emails_count)}}%</span></td>
 
   <td>
     @include('components.datatable.actions', ['actions' => [
