fc60ee284efdf7906103fcf3b41606b711de95d1
Arthur Villar <=>
2020-01-30 15:31:03 -0500

    Create emails logs page on admin

--- resources/views/admin/pages/reports/row.blade.php
+++ resources/views/admin/pages/reports/row.blade.php
@@ -6,13 +6,13 @@
   
   <td>{{$item->emails_count}}</td>
 
-  <td>{{percentage($item->delivered_count, $item->emails_count)}}%</td>
+  <td title="{{$item->delivered_count}} delivered">{{percentage($item->delivered_count, $item->emails_count)}}%</td>
 
-  <td>{{percentage($item->failed_count, $item->emails_count)}}%</td>
+  <td title="{{$item->failed_count}} failed">{{percentage($item->failed_count, $item->emails_count)}}%</td>
 
-  <td>{{percentage($item->opens_count, $item->emails_count)}}%</td>
+  <td title="{{$item->opens_count}} opened">{{percentage($item->opens_count, $item->emails_count)}}%</td>
 
-  <td>{{percentage($item->clicks_count, $item->emails_count)}}%</td>
+  <td title="{{$item->clicks_count}} clicked">{{percentage($item->clicks_count, $item->emails_count)}}%</td>
 
   <td>
     @include('components.datatable.actions', ['actions' => [
