31bf9a4fb025d61d0e6b4248b895e62a964d276b
Arthur Villar <=>
2020-01-30 15:29:21 -0500

    Create emails logs page on admin

--- app/Http/Controllers/Webhooks/MailgunWebhookController.php
+++ app/Http/Controllers/Webhooks/MailgunWebhookController.php
@@ -21,12 +21,24 @@ class MailgunWebhookController extends Controller
         $message_id = $data['message']['headers']['message-id'];
 
         if ($email = EmailLog::where('message_id', $message_id)->first()) {
-            if ($data['event'] === 'opened' || $data['event'] === 'clicked') {
+            if ($data['event'] === 'opened') {
                 $email->increment($data['event']);
+                $email->update(['unique_opened' => 1]);
             }
 
-            if ($data['event'] === 'delivered' || $data['event'] === 'failed') {
+            if ($data['event'] === 'clicked') {
+                $email->increment($data['event']);
+                $email->update(['unique_clicked' => 1]);
+            }
+
+            if ($data['event'] === 'delivered') {
                 $email->update(["{$data['event']}_at" => now()]);
+                $email->update(['unique_delivered' => 1]);
+            }
+
+            if ($data['event'] === 'failed') {
+                $email->update(["{$data['event']}_at" => now()]);
+                $email->update(['unique_failed' => 1]);
             }
         }
 
