621e77ce3204194c3de3711770c8ecef387e5682
Arthur Villar <=>
2020-02-07 00:04:07 -0500

    Improve infographs page

--- resources/views/components/cards/new.blade.php
+++ resources/views/components/cards/new.blade.php
@@ -1,3 +1,3 @@
 @if($is_new)
-<div class="absolute-top-right"><span class="badge badge-{{$color ?? 'light text-green'}}"><small><strong>NEW</strong></small></span></div>
+<div class="absolute-top-right" style="right: 1.45em"><span class="badge badge-{{$color ?? 'light text-green'}}"><small><strong>NEW</strong></small></span></div>
 @endif
