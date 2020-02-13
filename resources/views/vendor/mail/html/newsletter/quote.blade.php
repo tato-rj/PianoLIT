211c721dcd94293b63d5e40bdd9a778dd94aefb9
Arthur Villar <=>
2020-02-13 11:30:13 -0500

    Fix font size on newsletter email

--- resources/views/vendor/mail/html/newsletter/quote.blade.php
+++ resources/views/vendor/mail/html/newsletter/quote.blade.php
@@ -1,2 +1,2 @@
-<div class="text-center text-md text-italic mb-3">"{{ $quote }}"</div>
+<div class="text-center text-md text-italic mb-2">"{{ $quote }}"</div>
 <div class="text-right mb-4">- {{ $author }}</div>
