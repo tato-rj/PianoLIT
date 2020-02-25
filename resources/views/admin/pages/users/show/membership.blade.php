4c40c0129aab18a2f3227777390d9c6f2ca29f07
Arthur Villar <=>
2020-02-25 01:23:02 -0500

    Redo admin users page

--- resources/views/admin/pages/users/show/membership.blade.php
+++ resources/views/admin/pages/users/show/membership.blade.php
@@ -1,6 +1,5 @@
 @include('admin.pages.users.show.title', ['title' => 'Membership'])
 
 <div class="row">
-	<p>{{$user->getStatus()}}</p>
 	@include('admin.pages.users.show.status.'.$user->getStatus())
 </div>
