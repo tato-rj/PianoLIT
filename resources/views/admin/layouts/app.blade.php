0e2511eba8adf429f1ea6c49b9ea1b1090423375
Arthur Villar <=>
2020-02-25 13:59:05 -0500

    Add spinner to admin page

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -46,6 +46,13 @@
         @yield('head')
     </head>
 <body class="fixed-nav sticky-footer bg-dark" id="page-top">
+  <div id="loading-overlay" class="position-fixed h-100vh w-100" style="background-color: rgba(255,255,255,0.5); top: 0; left: 0; z-index: 100000000; display: none;">
+    <div class="h-100 w-100 d-flex flex-center">
+      <div class="spinner-border text-primary" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+    </div>
+  </div>
 
   @include('admin/layouts/menu')
 
@@ -68,6 +75,11 @@
 
   <script type="text/javascript" src="{{mix('js/admin.js')}}"></script>
 
+<script type="text/javascript">
+$('.navbar-brand').click(function() {
+  $('#loading-overlay').show();
+});
+</script>
 <script type="text/javascript">
   $('a[data-toggle="fixed-panel"]').on('click', function() {
     let $link = $(this);
