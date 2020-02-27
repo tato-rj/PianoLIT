2b28e20e9cf7dcafe4d86fe4d5fe080f643e2484
Arthur Villar <=>
2020-02-27 02:00:04 -0500

    Add favorites count to users table

---
+++ resources/views/admin/pages/users/show/log-data.blade.php
@@ -0,0 +1,19 @@
+<table class="table table-borderless">
+  <thead class="thead-light">
+    <tr>
+      <th class="py-2 rounded-left" scope="col">Key</th>
+      <th class="py-2 rounded-right" scope="col">Value</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach($data as $key => $value)
+    <tr>
+      <th scope="row">{{snake_str($key)}}</th>
+      @if($url == route('api.pieces.find'))
+      @php($value = "($value) " . \App\Piece::findOrFail($value)->medium_name_with_composer)
+      @endif
+      <td style="width: 76%">{{is_array($value) ? arrayToSentence($value) : $value}}</td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
