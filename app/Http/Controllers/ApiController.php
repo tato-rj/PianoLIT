984711812a8825005002b2d995f76b7980c735d0
Arthur Villar <=>
2020-03-07 01:14:21 -0500

    Add HWV catalogue for Handel

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -25,12 +25,12 @@ class ApiController extends Controller
                 $this->api->order(2)->latest('Latest pieces'),
                 $this->api->order(4)->women('From women composers'),
                 $this->api->order(5)->tag('Pieces that are'),
-                $this->api->order(7)->levels('Levels'),
-                $this->api->order(11)->similar('Like today\'s free pick', Piece::free()->first()),
-                $this->api->order(6)->improve('Improve your'),
-                $this->api->order(8)->for('Great for'),
-                $this->api->order(9)->ranking('rcm', 'Equivalent to the RCM levels'),
-                $this->api->order(10)->ranking('abrsm', 'Equivalent to the ABRSM levels'),
+                $this->api->order(6)->levels('Levels'),
+                $this->api->order(7)->similar('Like today\'s free pick', Piece::free()->first()),
+                $this->api->order(8)->improve('Improve your'),
+                $this->api->order(9)->for('Great for'),
+                $this->api->order(10)->ranking('rcm', 'Equivalent to the RCM levels'),
+                $this->api->order(11)->ranking('abrsm', 'Equivalent to the ABRSM levels'),
             ]);
         });
 
