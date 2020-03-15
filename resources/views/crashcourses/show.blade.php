d24ab11c73db28043effe3cc9bd74432a98abdea
Arthur Villar <=>
2020-03-14 20:59:08 -0400

    Make course sign up page

--- resources/views/crashcourses/show.blade.php
+++ resources/views/crashcourses/show.blade.php
@@ -20,12 +20,12 @@ p {letter-spacing: 1px;}
 @section('content')
 
 <div class="container-fluid p-4">
-  <div class="position-relative d-flex d-apart flex-column" style="min-height: 100vh; background-color: rgba(222,237,249,0.8)">
+  <div class="position-relative d-flex d-apart flex-column" style="background-color: rgba(222,237,249,0.8)">
     <div class="position-absolute bg-white rounded-circle d-flex flex-center" style="width: 140px; height: 140px; right: 140px; top: -55px;">
       <img src="{{asset('images/brand/app-icon.svg')}}" style="border-radius: 20%; width: 52px; margin-top: 32px;">
     </div>
 
-    <div class="text-center" style="padding-top: 90px; padding-bottom: 32px; margin-bottom: 40px">
+    <div class="text-center" style="padding-top: 90px; padding-bottom: 32px; margin-bottom: 60px">
       <h1 style="font-family: 'Roboto Condensed', sans-serif; font-size: 163px; letter-spacing: 4px" class="text-white m-0">CRASHCOURSE</h1>
       <h2 style="margin-top: -96px;">Daily lessons delivered to your email</h2>
     </div>
