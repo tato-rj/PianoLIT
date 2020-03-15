694a8bc9103f4a76bcff0d294306c7a986fe2a24
Arthur Villar <=>
2020-03-14 20:56:09 -0400

    Make course sign up page

--- resources/views/crashcourses/show.blade.php
+++ resources/views/crashcourses/show.blade.php
@@ -25,12 +25,12 @@ p {letter-spacing: 1px;}
       <img src="{{asset('images/brand/app-icon.svg')}}" style="border-radius: 20%; width: 52px; margin-top: 32px;">
     </div>
 
-    <div class="text-center" style="padding-top: 90px; padding-bottom: 32px; margin-bottom: 80px">
+    <div class="text-center" style="padding-top: 90px; padding-bottom: 32px; margin-bottom: 60px">
       <h1 style="font-family: 'Roboto Condensed', sans-serif; font-size: 163px; letter-spacing: 4px" class="text-white m-0">CRASHCOURSE</h1>
       <h2 style="margin-top: -96px;">Daily lessons delivered to your email</h2>
     </div>
 
-    <div class="mx-auto position-relative" style="max-width: 75%; padding: 0 182px; overflow-y: hidden;">
+    <div class="mx-auto position-relative" style="max-width: 72%; padding: 0 182px; overflow-y: hidden;">
       <div class="position-absolute" style="max-width: 312px; transform: rotate(-8deg); left: 40px; top: 32px">
         <img src="{{asset('images/mockup/crashcourse.png')}}" class="w-100">
       </div>
@@ -38,7 +38,7 @@ p {letter-spacing: 1px;}
         <div class="mb-4">
           <p class="text-warning" style="opacity: 0.9"><strong>...by signing up to this {{$crashcourse->lessons_count}}-day course you'll learn about:</strong></p>
           <h4 style="letter-spacing: 1px">{{$crashcourse->title}}</h4>
-          <p class="m-0 text-muted">{{$crashcourse->description}}</p>
+          {{-- <p class="m-0 text-muted">{{$crashcourse->description}}</p> --}}
         </div>
 
         <form method="POST" action="{{route('crashcourses.signup', $crashcourse)}}">
