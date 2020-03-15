74ed3fb4366e0a6ba12ddaaf0191976bc5663358
Arthur Villar <=>
2020-03-14 20:58:20 -0400

    Make course sign up page

--- resources/views/crashcourses/show.blade.php
+++ resources/views/crashcourses/show.blade.php
@@ -25,7 +25,7 @@ p {letter-spacing: 1px;}
       <img src="{{asset('images/brand/app-icon.svg')}}" style="border-radius: 20%; width: 52px; margin-top: 32px;">
     </div>
 
-    <div class="text-center" style="padding-top: 90px; padding-bottom: 32px; margin-bottom: 60px">
+    <div class="text-center" style="padding-top: 90px; padding-bottom: 32px; margin-bottom: 40px">
       <h1 style="font-family: 'Roboto Condensed', sans-serif; font-size: 163px; letter-spacing: 4px" class="text-white m-0">CRASHCOURSE</h1>
       <h2 style="margin-top: -96px;">Daily lessons delivered to your email</h2>
     </div>
