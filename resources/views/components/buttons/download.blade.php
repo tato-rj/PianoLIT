aa788cc6716079ef4a2c57a65e73e7b451370251
Arthur Villar <=>
2020-03-15 22:07:43 -0400

    Remove subscribe popup from main page and change the button color to pink

--- resources/views/components/buttons/download.blade.php
+++ resources/views/components/buttons/download.blade.php
@@ -1,4 +1,4 @@
-<a href="{{config('app.stores.ios')}}" target="_blank" class="btn {{-- btn-primary --}} shadow mb-2" style="min-width: 18.5rem; padding: .625rem 1rem;
+<a href="{{config('app.stores.ios')}}" target="_blank" class="btn {{-- btn-primary --}} shadow mb-2 text-white" style="min-width: 18.5rem; padding: .625rem 1rem;
 background-color: #fe3674; border-color: #fe3674;">
 	<h6 class="m-0" style="margin: 0; line-height: 1.25; font-size: .95rem;">Start free trial now</h6>
 </a>
