6d9d9658b78bfa6f34dba5bda8243e492d6a54fc
Arthur Villar <=>
2020-03-19 11:25:50 -0400

    Debug missing unsubscribe link on crash course email

--- app/Mail/CrashCourseEmail.php
+++ app/Mail/CrashCourseEmail.php
@@ -22,7 +22,7 @@ class CrashCourseEmail extends Mailable
     public function __construct($model, $email = null)
     {
         $this->manageData($model);
-        $this->lesson->email = $email;
+        $this->email = $email;
     }
 
     /**
--- resources/views/emails/crashcourse.blade.php
+++ resources/views/emails/crashcourse.blade.php
@@ -1,10 +1,9 @@
 @component('mail::raw')
-<h1>EMAIL: {{$lesson->email}}</h1>
-{{-- {!! $lesson->dynamic('body', $subscription) !!} --}}
+{!! $lesson->dynamic('body', $subscription) !!}
 
 <div>
 
-    <form method="POST" action="{{$lesson->email}}" target="_blank">
+    <form method="POST" action="UGH" target="_blank">
         @csrf
         @method('DELETE')
         If you wish to stop receiving these emails <button type="submit" style="padding: 0; border: 0; background: transparent; color: grey; cursor: pointer;"><strong>click here</strong></button> you can update your preferences or email your request to contact@pianolit.com.
