b76d95db8c7c3d6cf392537c26b8dfdef3ca10af
Arthur Villar <=>
2020-03-19 20:57:01 -0400

    Debug missing unsubscribe link on crash course email

--- resources/views/emails/crashcourse.blade.php
+++ resources/views/emails/crashcourse.blade.php
@@ -1,6 +1,8 @@
 @component('mail::raw')
-{!! $lesson->cancelUrl($subscription) !!}
-{{-- {!! $lesson->dynamic('body', $subscription) !!} --}}
 
+{!! $lesson->dynamic('body', $subscription) !!}
 
+<p style="font-size: 94%">
+    If you wish to stop receiving these emails <a href="{{$lesson->cancelUrl($subscription)}}" target="_blank">click here</a> you can update your preferences or email your request to contact@pianolit.com.
+</p>
 @endcomponent
