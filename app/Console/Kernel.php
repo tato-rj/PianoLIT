2e55372d80b22580698982fd4c2ba23266439770
Arthur Villar <=>
2020-03-22 19:09:18 -0400

    Finish crashcourses api

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('pianolit:timeline-email')->dailyAt('7:00');
         $schedule->command('pianolit:admin-report')->weeklyOn(7, '20:30');
         $schedule->command('pianolit:unconfirmed-emails')->weeklyOn(2, '10:00');
-        $schedule->command('crashcourse:send')->hourly();
+        $schedule->command('crashcourse:send')->everyFiveMinutes();
     }
 
     /**
