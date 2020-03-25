b6553cc83d9320a1dc8f57b979f35e3799760d4e
Arthur Villar <=>
2020-03-25 11:06:07 -0400

    Create memberships logs endpoint

--- app/Membership.php
+++ app/Membership.php
@@ -17,9 +17,17 @@ class Membership extends PianoLit
 	
 	public function reactivate($receipt)
 	{
+		try {
+			$latest_receipt_info = $receipt->in_app[0];
+		} catch (\Exception $e) {
+			$latest_receipt_info = null;
+		}
+
 		return $this->update([
 			'plan' => $receipt->product_id,
-			'renews_at' => carbon($receipt->expires_date)->timezone(config('app.timezone'))]);
+			'renews_at' => carbon($receipt->expires_date)->timezone(config('app.timezone')),
+			'latest_receipt_info' => $latest_receipt_info
+		]);
 	}
 
 	public function validate($request)
