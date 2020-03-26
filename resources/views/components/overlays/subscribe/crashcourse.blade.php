3c0bf6b37197a462e460ae18a098d4db48cb54ab
Arthur Villar <=>
2020-03-26 10:46:29 -0400

    Add crash course on quiz page

--- resources/views/components/overlays/subscribe/crashcourse.blade.php
+++ resources/views/components/overlays/subscribe/crashcourse.blade.php
@@ -7,16 +7,4 @@
   	'hidePhoneOnOverflow' => false,
   ])
 </div>
-{{-- <div class="mx-3" style="max-width: 512px">
-	<div class="rounded text-center shadow-light bg-white pt-4">
-		<img src="{{storage($gift->thumbnail_path)}}" class="rounded-top border" style="width: 210px">
-		<div class="px-4 py-3 bg-white rounded-bottom">
-			<div class="mb-4">
-				<div>Subscribe today and get a <strong><u>FREE</u></strong> poster in your inbox!</div>
-			</div>
-			
-			@include('components.form.subscription', ['gift_url' => route('infographs.download', $gift->slug)])
-		</div>
-	</div>
-</div> --}}
 @endcomponent
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,5 @@
 <?php
 
-Route::get('test-overlay', function() {
-	return $highlightedCrashcourse;
-});
-
 Route::resources([
     'subscriptions' => 'SubscriptionsController'
 ]);
