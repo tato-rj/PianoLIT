481cec09b17db809f16f5ff4f3087063a8ce86cc
Arthur Villar <=>
2020-03-31 10:59:10 -0400

    Update membership stats page

--- resources/views/admin/pages/stats/memberships/expired.blade.php
+++ resources/views/admin/pages/stats/memberships/expired.blade.php
@@ -1,13 +1,16 @@
 @foreach($memberships as $membership)
   @if($loop->iteration <= $limit)
   <tr class="sortable">
+    <td class="{{$loop->last ? 'pt-1 pb-2 ' : 'py-1'}}" style="width: 5%">
+      <div class="text-truncate">{{$membership->user->id}}</div>
+    </td>
     <td class="{{$loop->last ? 'pt-1 pb-2 ' : 'py-1'}}" style="width: 10%">
       <div class="text-truncate">{{$membership->user->full_name}}</div>
     </td>
     <td class="{{$loop->last ? 'pt-1 pb-2 ' : 'py-1'}}" style="width: 10%">
       <div class="text-truncate">{{$membership->plan_name}}</div>
     </td>
-    <td class="{{$loop->last ? 'pt-1 pb-2 ' : 'py-1'}} pr-0" style="width: 70%; vertical-align: inherit;">
+    <td class="{{$loop->last ? 'pt-1 pb-2 ' : 'py-1'}} pr-0" style="width: 65%; vertical-align: inherit;">
       <div class="text-muted">
         <strong>{{$membership->renews_at->diffForHumans()}}</strong>
       </div>
--- resources/views/admin/pages/stats/memberships/index.blade.php
+++ resources/views/admin/pages/stats/memberships/index.blade.php
@@ -15,7 +15,7 @@
           'hoverable' => 'no',
           'minWidth' => 820,
           'borderless' => true,
-          'headers' => ['User <i class="fas fa-sort"></i>', 'Progress <i class="fas fa-sort"></i>', 'Ends at <i class="fas fa-sort"></i>'],
+          'headers' => ['ID <i class="fas fa-sort"></i>', 'User <i class="fas fa-sort"></i>', 'Progress <i class="fas fa-sort"></i>', 'Ends at <i class="fas fa-sort"></i>'],
           'rows' => view('admin.pages.stats.memberships.trials', ['memberships' => $trials, 'limit' => 10, 'more' => true])
         ])
       </div>
@@ -27,7 +27,7 @@
           'title' => '<i class="fas fa-credit-card mr-2 text-green"></i>Active memberships',
           'minWidth' => 820,
           'borderless' => true,
-          'headers' => ['User <i class="fas fa-sort"></i>', 'Plan <i class="fas fa-sort"></i>', 'Time until next renewal <i class="fas fa-sort"></i>', 'Renews at <i class="fas fa-sort"></i>'],
+          'headers' => ['ID <i class="fas fa-sort"></i>', 'User <i class="fas fa-sort"></i>', 'Plan <i class="fas fa-sort"></i>', 'Time until next renewal <i class="fas fa-sort"></i>', 'Renews at <i class="fas fa-sort"></i>'],
           'rows' => view('admin.pages.stats.memberships.members', ['memberships' => $members, 'limit' => 10, 'more' => true])
         ])
       </div>
@@ -39,7 +39,7 @@
           'title' => '<i class="fas fa-credit-card mr-2 text-muted"></i>Expired memberships',
           'minWidth' => 820,
           'borderless' => true,
-          'headers' => ['User <i class="fas fa-sort"></i>', 'Plan <i class="fas fa-sort"></i>', 'Time since last renewal <i class="fas fa-sort"></i>', 'Last renew at <i class="fas fa-sort"></i>'],
+          'headers' => ['ID <i class="fas fa-sort"></i>', 'User <i class="fas fa-sort"></i>', 'Plan <i class="fas fa-sort"></i>', 'Time since last renewal <i class="fas fa-sort"></i>', 'Last renew at <i class="fas fa-sort"></i>'],
           'rows' => view('admin.pages.stats.memberships.expired', ['memberships' => $expired, 'limit' => 10, 'more' => true])
         ])
       </div>
