e451ab5ad35e30f11f48e96646991ec01c9c575f
Arthur Villar <=>
2020-04-02 23:30:07 -0400

    Show remaining days on tutorial requests page

--- resources/views/admin/pages/requests/table/users.blade.php
+++ resources/views/admin/pages/requests/table/users.blade.php
@@ -2,6 +2,6 @@
 	{{$item->user->full_name}}
 	@if($item->user->isOnTrial)
 	@php($daysLeft = 7 - $item->user->membership->created_at->diffInDays(now()))
-	<div class="badge badge-pill alert-yellow">{{$daysLeft}} {{str_plural('day', $daysLeft)}} left</div>
+	<div class="badge badge-pill alert-{{$daysLeft <= 2 ? 'red' : 'yellow'}}">{{$daysLeft}} {{str_plural('day', $daysLeft)}} left</div>
 	@endif
 </div>
