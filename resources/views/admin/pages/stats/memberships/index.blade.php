21c7c50a771ad2e0d3c91cc8704c05ba3218671c
Arthur Villar <=>
2020-04-20 00:23:07 -0400

    Fix row in membership stats page

--- resources/views/admin/pages/stats/memberships/index.blade.php
+++ resources/views/admin/pages/stats/memberships/index.blade.php
@@ -13,6 +13,7 @@
           'id' => 'trials-table',
           'title' => '<i class="fas fa-hourglass-half mr-2 text-warning"></i>Trials ('.$trials->count().')',
           'sortable' => true,
+          'borderless' => true,
           'more' => route('admin.memberships.load-trials'),
           'headers' => ['ID <i class="fas fa-sort"></i>', 'User <i class="fas fa-sort"></i>', 'Plan <i class="fas fa-sort"></i>', 'Progress <i class="fas fa-sort"></i>', 'Ends at <i class="fas fa-sort"></i>'],
           'rows' => view('admin.pages.stats.memberships.trials', ['memberships' => $trials->take(10)])
@@ -25,6 +26,7 @@
           'id' => 'members-table',
           'title' => '<i class="fas fa-credit-card mr-2 text-green"></i>Active memberships ('.$members->count().')',
           'sortable' => true,
+          'borderless' => true,
           'more' => route('admin.memberships.load-members'),
           'headers' => ['ID <i class="fas fa-sort"></i>', 'User <i class="fas fa-sort"></i>', 'Plan <i class="fas fa-sort"></i>', 'Time until next renewal <i class="fas fa-sort"></i>', 'Renews at <i class="fas fa-sort"></i>'],
           'rows' => view('admin.pages.stats.memberships.members', ['memberships' => $members->take(10)])
@@ -37,6 +39,7 @@
           'id' => 'expired-table',
           'title' => '<i class="fas fa-credit-card mr-2 text-muted"></i>Expired memberships ('.$expired->count().')',
           'sortable' => true,
+          'borderless' => true,
           'more' => route('admin.memberships.load-expired'),
           'headers' => ['ID <i class="fas fa-sort"></i>', 'User <i class="fas fa-sort"></i>', 'Plan <i class="fas fa-sort"></i>', 'Time since last renewal <i class="fas fa-sort"></i>', 'Last renew at <i class="fas fa-sort"></i>'],
           'rows' => view('admin.pages.stats.memberships.expired', ['memberships' => $expired->take(10)])
