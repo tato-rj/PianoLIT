52667e04f9796231beee8cf8fa1d56fae1c7793a
Arthur Villar <=>
2020-04-22 15:45:27 -0400

    Create piece view for webapp

--- resources/views/webapp/components/piece.blade.php
+++ resources/views/webapp/components/piece.blade.php
@@ -1,7 +1,7 @@
 <div class="border-bottom py-2">
 	<a href="" class="link-none">
 		<div class="mb-2 text-uppercase">@pill(['label' => $piece->extended_level_name, 'color' => $piece->level_name.'-raw'])</div>
-		<h6 class="m-0" style="line-height: 1">{{$piece->long_name}}</h6>
+		<h6 class="m-0">{{$piece->long_name}}</h6>
 		<p>{{$piece->composer->name}}</p>
 		<div class="d-flex d-apart">
 			<div class="text-muted">
