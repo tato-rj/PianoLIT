796072fa5d3cd292e2df5dc18a727cc11d076dbb
Arthur Villar <=>
2020-04-25 01:27:42 -0400

    Fix search input size on mobile for the webapp

--- resources/views/components/fontawesome.blade.php
+++ resources/views/components/fontawesome.blade.php
@@ -1,1 +1,1 @@
-<i class="fa{{$fa_type ?? 's'}} fa-{{$icon}} text-{{$color ?? null}} fa-{{$size ?? null}} mr-{{$mr ?? 2}} ml-{{$ml ?? null}} {{$classes ?? null}}"></i>
+<i title="{{$title ?? null}}" class="fa{{$fa_type ?? 's'}} fa-{{$icon}} text-{{$color ?? null}} fa-{{$size ?? null}} mr-{{$mr ?? 2}} ml-{{$ml ?? null}} {{$classes ?? null}}"></i>
--- resources/views/webapp/explore/index.blade.php
+++ resources/views/webapp/explore/index.blade.php
@@ -33,7 +33,7 @@
 	  <div class="input-icon">
 	    @fa(['icon' => 'search', 'color' => 'grey', 'size' => 'lg'])
 	    <input type="text" name="search" class="form-control border-bottom p-4 rounded-0 bg-transparent border-grey w-100" style="border: 0;" placeholder="Search here...">
-	    @fa(['icon' => 'algolia', 'fa_type' => 'b', 'color' => 'grey', 'size' => 'lg'])
+	    @fa(['icon' => 'algolia', 'fa_type' => 'b', 'color' => 'grey', 'size' => 'lg', 'title' => 'Powered by Algolia'])
 	  </div>
 	</form>
 </section>
