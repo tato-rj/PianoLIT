0712df579114663cae23eb82c69a7ea3bd2283cc
Arthur Villar <=>
2020-04-25 12:34:13 -0400

    Debug search resutls on webapp

--- app/Api/Api.php
+++ app/Api/Api.php
@@ -46,7 +46,7 @@ class Api extends Factory
 
         if ($request->has('count'))
             return response()->json(['count' => $query->count()]);
-dd($query->get());
+
         $results = $request->has('lazy-load') ? $query->paginate($options['hitsPerPage']) : $query->get();
 
         return $results->load(['tags', 'composer', 'favorites'])->each->isFavorited($request->user_id);            
