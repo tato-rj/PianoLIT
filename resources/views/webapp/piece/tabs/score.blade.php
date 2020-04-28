73b487007d7a22e6176ce1c8ddc9852a3363da44
Arthur Villar <=>
2020-04-28 00:23:38 -0400

    Create piece tabs view on webapp

--- resources/views/webapp/piece/tabs/score.blade.php
+++ resources/views/webapp/piece/tabs/score.blade.php
@@ -9,11 +9,9 @@
 
 	@if($piece->isPublicDomain)
 	<div class="text-center">
-<iframe src="https://docs.google.com/viewer?url=https://pianolit.com/storage/app/score/39qJigTzwJiCgBEFYl3zeQaMKik4YKTVMvLdfir2.pdf&embedded=true" style="border: none;" height="780" width="600">
-</iframe>
-{{-- 		<div class="embed-responsive embed-responsive-a4 mb-4">
+		<div class="embed-responsive embed-responsive-a4 mb-4">
 			<embed type="application/pdf" src="{{storage($piece->score_path)}}" class="embed-responsive-item" frameborder="0">
-		</div> --}}
+		</div>
 		<a href="{{storage($piece->score_path)}}" class="btn rounded-pill btn-default">@fa(['icon' => 'file-alt'])Download score</a>
 	</div>
 	@endif
