83a33e327e3eddd881b57769e2ace7c435ae142a
Arthur Villar <=>
2020-04-29 22:09:28 -0400

    Stop audio on tab change

--- resources/views/webapp/piece/audio/player.blade.php
+++ resources/views/webapp/piece/audio/player.blade.php
@@ -11,11 +11,13 @@
 	</div>
 	<div id="player-body" class="p-3 border-top">
 		<div class="d-flex mb-3">
-			<div class="d-flex align-items-center" id="select-hand" data-audio="{{$piece->audio}}">
+			@if($piece->audio_lh && $piece->audio_rh)
+			<div class="d-flex align-items-center mr-3 pr-3 border-right" id="select-hand" data-audio="{{$piece->audio}}">
 				<button class="btn-raw text-muted opacity-4" data-audio="{{$piece->audio_lh}}" title="Left hand only">@fa(['size' => 'lg', 'icon' => 'hand-paper', 'classes' => 'mirror', 'mr' => 0])</button>
 				<button class="btn-raw text-muted opacity-4" data-audio="{{$piece->audio_rh}}" title="Right hand only">@fa(['size' => 'lg', 'icon' => 'hand-paper', 'mr' => 0, 'ml' => 2])</button>
 			</div>
-			<div class="flex-grow ml-3 pl-3 border-left d-flex align-items-center">
+			@endif
+			<div class="flex-grow d-flex align-items-center">
 				<span class="badge badge-light">slow</span>
 				<input class="custom-range w-100 mx-2" id="audio-speed" type="range" value="1" min="0.25" max="1.85" step="0.25">
 				<span class="badge badge-light">fast</span>
