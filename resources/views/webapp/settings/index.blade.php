ef758212a59225f64bbc3bc11d24ddb6d7e27f8a
Arthur Villar <=>
2020-05-03 00:15:44 -0400

    Make pricing page

--- resources/views/webapp/settings/index.blade.php
+++ resources/views/webapp/settings/index.blade.php
@@ -8,7 +8,7 @@
 
 <div class="row">
 	<div class="col-lg-8 col-12 mx-auto p-0 list-group">
-	  <a href="{{route('memberships.pricing')}}" class="list-group-item py-4 rounded-0 list-group-item-action">Subscribe @fa(['icon' => 'star', 'color' => 'yellow'])</a>
+	  <a href="{{route('webapp.membership.pricing')}}" class="list-group-item py-4 rounded-0 list-group-item-action">Subscribe @fa(['icon' => 'star', 'color' => 'yellow'])</a>
 	  <a href="{{route('users.profile')}}" target="_blank" class="list-group-item py-4 rounded-0 list-group-item-action">My profile</a>
 	  <a href="{{route('contact')}}" target="_blank" class="list-group-item py-4 rounded-0 list-group-item-action">Contact us</a>
 	  <a href="{{config('app.url')}}" target="_blank" class="list-group-item py-4 rounded-0 list-group-item-action">Visit the website</a>
