d4f020abd8a3ec7e2c7e5b912ebfabf9663ef5ed
Arthur Villar <=>
2020-05-10 23:17:08 -0400

    Fix missing source for non members

--- resources/views/admin/pages/users/show/membership/info.blade.php
+++ resources/views/admin/pages/users/show/membership/info.blade.php
@@ -1,19 +1,21 @@
 <div class="col-6">
 	<table class="table table-striped table-borderless">
 	  <tbody>
+	  	@if($user->membership()->exists())
 		@include('admin.pages.users.show.list-item', 
 			['title' => 'Membership ID', 'value' => $user->membership->source->latest_receipt_info ? $user->membership->source->latest_receipt_info->original_transaction_id : null])
 		@include('admin.pages.users.show.list-item', 
 			['title' => 'Plan', 'value' => $user->membership->source->latest_receipt_info ? ucfirst($user->membership->source->latest_receipt_info->product_id) : null])
 		@include('admin.pages.users.show.list-item',
-			['title' => 'Start date', 'value' => $user->membership->source->created_at->toDayDateTimeString()])
-		@include('admin.pages.users.show.list-item',
 			['title' => 'Next due date', 'value' => $user->membership->source->renews_at ? $user->membership->source->renews_at->toDayDateTimeString() : '-'])
+		@endif
+		@include('admin.pages.users.show.list-item',
+			['title' => 'Start date', 'value' => $user->membership->source->created_at->toDayDateTimeString()])
 	  </tbody>
 	</table>
 </div>
 <div class="col-6">
-	@if($user->membership->source->isExpired() || ! $user->membership->source->renews_at)
+	@if($user->membership()->exists() && ($user->membership->source->isExpired() || ! $user->membership->source->renews_at))
 		<div class="mb-3">
 			<form method="POST" action="{{route('admin.memberships.validate.user', $user->id)}}">
 				@csrf
--- routes/mail.php
+++ routes/mail.php
@@ -4,20 +4,20 @@ use App\Mail\Welcome;
 use App\Mail\Timeline\OnThisDay;
 use App\{Composer, Subscription};
 
-// Route::prefix('birthday')->name('birthday.')->group(function() {
+Route::prefix('birthday')->name('birthday.')->group(function() {
 
-// 	Route::get('', function () {
-// 		$composer = request()->has('composer_id') ? Composer::find(request('composer_id')) : Composer::bornToday()->inRandomOrder()->first();
+	Route::get('', function () {
+		$composer = request()->has('composer_id') ? Composer::find(request('composer_id')) : Composer::bornToday()->inRandomOrder()->first();
 
-// 	    return new OnThisDay($composer, Subscription::admin());
-// 	})->name('web');
+	    return new OnThisDay($composer, Subscription::admin());
+	})->name('web');
 
-// 	Route::get('/{composer_id}', function () {
-// 		$email = new OnThisDay(Composer::find(request('composer_id')), Subscription::admin());
+	Route::get('/{composer_id}', function () {
+		$email = new OnThisDay(Composer::find(request('composer_id')), Subscription::admin());
 
-// 		\Mail::to(Subscription::admin())->send($email);
+		\Mail::to(Subscription::admin())->send($email);
 
-// 	    return $email;
-// 	})->name('mail');
+	    return $email;
+	})->name('mail');
 	
-// });
+});
