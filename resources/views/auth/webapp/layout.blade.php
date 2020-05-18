6daf79fb5502d106a9600a03e24d0d11fe91046e
Arthur Villar <=>
2020-05-18 15:59:05 -0400

    Create webapp auth pages

--- resources/views/auth/webapp/layout.blade.php
+++ resources/views/auth/webapp/layout.blade.php
@@ -8,7 +8,7 @@
             	<a class="navbar-brand mb-2" href="{{config('app.url')}}">
             		<img src="{{asset('images/brand/app-icon.svg')}}" style="border-radius: 20%; width: 60px">
             	</a>
-                <h3 class="accent-bottom mb-4">{{$title}}</h3>
+                <h3>{{$title}}</h3>
                 <p>{{$subtitle}}</p>
                 
                 {{$slot}}
@@ -19,9 +19,9 @@
         <div class="col-lg-6 col-md-12 col-12 shadow-light bg-light">
             <div class="d-flex flex-center h-100">
                 <div class="py-4">
-                    <div class="px-3 text-center">
-                        <h3 class="mb-1"><strong>Find music that inspires you.</strong></h3>
-                        <p class="text-muted mb-1 hide-on-sm" style="font-size: 110%">Where pianists discover new pieces and find inspiration<br>to play only what they love.</p>
+                    <div class="text-center">
+                        <h3 class="hide-on-sm">Find music that inspires you.</h3>
+                        <p class="text-muted mb-1" style="font-size: 110%">Where pianists discover new pieces and find inspiration<br>to play only what they love.</p>
                     </div>
                     <img src="{{asset('images/webapp/devices.png')}}" class="w-100">
                 </div>
