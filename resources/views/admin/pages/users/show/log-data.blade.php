9ac97da08d7f7c4c1a71696aa287ec415214b67b
Arthur Villar <=>
2020-05-22 21:34:26 -0400

    Debug missing apple latest receipt

--- resources/views/admin/pages/users/show/log-data.blade.php
+++ resources/views/admin/pages/users/show/log-data.blade.php
@@ -9,7 +9,7 @@
     @foreach($data as $key => $value)
     <tr>
       <th scope="row">{{snake_str($key)}}</th>
-      @if($url == route('api.pieces.find'))
+      @if(str_replace('my.', '', $url) == route('api.pieces.find'))
       @php($value = "($value) " . \App\Piece::findOrFail($value)->medium_name_with_composer)
       @endif
       <td style="width: 76%">{{is_array($value) ? arrayToSentence($value) : $value}}</td>
