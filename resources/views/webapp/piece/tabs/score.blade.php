c7a646d5c817b68a774bb8bb711f207a4bd19ebd
Arthur Villar <=>
2020-05-28 14:58:31 -0400

    Debug blank pdf on webapp iOS

--- resources/views/webapp/piece/tabs/score.blade.php
+++ resources/views/webapp/piece/tabs/score.blade.php
@@ -4,7 +4,7 @@
 		<div class="text-center mb-4">
 			@include('webapp.components.pdfviewer')
 
-			<button id="pdf-download" data-url="{{storage($piece->score_path)}}" class="btn rounded-pill btn-default">@fa(['icon' => 'file-alt'])Download score</button>
+			<a href="{{storage($piece->score_path)}}" target="_blank" class="btn rounded-pill btn-default">@fa(['icon' => 'file-alt'])Download score</a>
 		</div>
 	</div>
 	<div class="ios-only w-100" style="display: none;">
