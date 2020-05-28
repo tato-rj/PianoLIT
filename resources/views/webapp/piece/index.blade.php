c4e8b5d3a2ff3ca39dd2b045ce3ae6da9d2cba24
Arthur Villar <=>
2020-05-28 00:40:06 -0400

    Add temporary next button to webapp

--- resources/views/webapp/piece/index.blade.php
+++ resources/views/webapp/piece/index.blade.php
@@ -82,7 +82,7 @@ $(document).ready(function() {
 
 	let pdfDoc = null, pageNum = 1, padeIsRendering = false, pageNumIsPending = null;
 
-	let scale = 1.5, canvas = document.querySelector('#score-pdf'), ctx = canvas.getContext('2d'), $loading = $('#pdf-loading');
+	let scale = 1, canvas = document.querySelector('#score-pdf'), ctx = canvas.getContext('2d'), $loading = $('#pdf-loading');
 
 	function renderPage(num) {
 		pageIsRendering = true;
