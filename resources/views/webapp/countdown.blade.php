1fbfb544cd5b1c0d472079730bcdf62b0e05908e
Arthur Villar <=>
2020-05-29 09:37:47 -0400

    Lock webapp for launch

--- resources/views/webapp/countdown.blade.php
+++ resources/views/webapp/countdown.blade.php
@@ -7,19 +7,19 @@
 
 <div id="countdown" class="d-flex flex-wrap justify-content-center">
 	<div class="text-center mr-2 mb-2" style="width: 80px">
-		<h5 class="d-flex flex-center mx-auto py-3 bg-light rounded-top m-0 border-top border-left border-right" style="height: 60px" id="days"></h5>
+		<h4 class="d-flex flex-center mx-auto py-3 bg-light rounded-top m-0 border-top border-left border-right" style="height: 60px" id="days"></h4>
 		<div class="border bg-white py-2 rounded-bottom"><small><strong>days</strong></small></div>
 	</div>
 	<div class="text-center mr-2 mb-2" style="width: 80px">
-		<h5 class="d-flex flex-center mx-auto py-3 bg-light rounded-top m-0 border-top border-left border-right" style="height: 60px" id="hours"></h5>
+		<h4 class="d-flex flex-center mx-auto py-3 bg-light rounded-top m-0 border-top border-left border-right" style="height: 60px" id="hours"></h4>
 		<div class="border bg-white py-2 rounded-bottom"><small><strong>hours</strong></small></div>
 	</div>
 	<div class="text-center mr-2 mb-2" style="width: 80px">
-		<h5 class="d-flex flex-center mx-auto py-3 bg-light rounded-top m-0 border-top border-left border-right" style="height: 60px" id="minutes"></h5>
+		<h4 class="d-flex flex-center mx-auto py-3 bg-light rounded-top m-0 border-top border-left border-right" style="height: 60px" id="minutes"></h4>
 		<div class="border bg-white py-2 rounded-bottom"><small><strong>minutes</strong></small></div>
 	</div>
 	<div class="text-center mr-2 mb-2" style="width: 80px">
-		<h5 class="d-flex flex-center mx-auto py-3 bg-light rounded-top m-0 border-top border-left border-right" style="height: 60px" id="seconds"></h5>
+		<h4 class="d-flex flex-center mx-auto py-3 bg-light rounded-top m-0 border-top border-left border-right" style="height: 60px" id="seconds"></h4>
 		<div class="border bg-white py-2 rounded-bottom"><small><strong>seconds</strong></small></div>
 	</div>
 </div>
