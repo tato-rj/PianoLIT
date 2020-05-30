555cd1d17a146e334275b3e41236e10789a7e30f
Arthur Villar <=>
2020-05-29 22:37:13 -0400

    Create blog post on discover page on webapp

--- resources/views/webapp/discover/rows/blog.blade.php
+++ resources/views/webapp/discover/rows/blog.blade.php
@@ -1,4 +1,4 @@
-<div class="border-top border-bottom py-4 my-4">
+<div class="border-top border-bottom pt-4 pb-2 my-4">
 	<a href="{{route('webapp.blog.show', $post)}}" class="link-none">
 		<div class="row">
 			<div class="col-lg-3 col-md-4 col-12">
@@ -6,7 +6,7 @@
 			</div>
 			<div class="col-lg-9 col-md-8 col-12 py-2">
 				<div class="text-blue mb-1 text-uppercase"><small><strong>{{$post->topics()->exists() ? $post->topics->first()->name : 'TO READ'}}</strong></small></div>
-				<h5 class="mb-1 clamp-3"><strong>{{$post->title}}</strong></h5>
+				<h4 class="mb-1 clamp-2"><strong>{{$post->title}}</strong></h4>
 				<div class="hide-on-sm" style="line-height: 1"><small>{{$post->description}}</small></div>
 			</div>
 		</div>
