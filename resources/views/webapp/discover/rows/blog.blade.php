42c6cf87a6cc50e95a834cbf28fdcc53fffe0018
Arthur Villar <=>
2020-05-29 22:29:57 -0400

    Create blog post on discover page on webapp

--- resources/views/webapp/discover/rows/blog.blade.php
+++ resources/views/webapp/discover/rows/blog.blade.php
@@ -5,7 +5,7 @@
 				<div class="bg-align-center position-relative h-100 rounded" style="background-image: url({{$post->cover_image()}}); min-height: 140px"></div>
 			</div>
 			<div class="col-lg-9 col-md-8 col-12 py-2">
-				<div class="text-blue mb-1 text-uppercase"><small><strong>{{$post->topics->first()->name}}</strong></small></div>
+				<div class="text-blue mb-1 text-uppercase"><small><strong>{{$post->topics()->exists() ? $post->topics->first()->name : 'TO READ'}}</strong></small></div>
 				<h5 class="mb-1 clamp-3"><strong>{{$post->title}}</strong></h5>
 				<div class="hide-on-sm" style="line-height: 1"><small>{{$post->description}}</small></div>
 			</div>
