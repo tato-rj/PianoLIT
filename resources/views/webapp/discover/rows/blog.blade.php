b723cbd774e3ee8e621cd8218f05508fac52f653
Arthur Villar <=>
2020-05-29 22:26:43 -0400

    Create blog post on discover page on webapp

--- resources/views/webapp/discover/rows/blog.blade.php
+++ resources/views/webapp/discover/rows/blog.blade.php
@@ -2,7 +2,7 @@
 	<a href="{{route('webapp.blog.show', $post)}}" class="link-none">
 		<div class="row">
 			<div class="col-lg-3 col-md-4 col-12">
-				<div class="bg-align-center position-relative h-100 rounded" style="background-image: url({{$post->cover_image()}}); min-height: 120px"></div>
+				<div class="bg-align-center position-relative h-100 rounded" style="background-image: url({{$post->cover_image()}}); min-height: 140px"></div>
 			</div>
 			<div class="col-lg-9 col-md-8 col-12 py-2">
 				<div class="text-blue mb-1 text-uppercase"><small><strong>{{$post->topics->first()->name}}</strong></small></div>
