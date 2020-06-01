5dfb9b3f913af2855ad8bc77df294220452dfe9c
Arthur Villar <=>
2020-05-31 22:00:31 -0400

    Release WebApp

--- resources/views/webapp/blog/components/topics.blade.php
+++ resources/views/webapp/blog/components/topics.blade.php
@@ -1,11 +1,6 @@
 <div class="mb-2">
 	@forelse($post->topics as $topic)
-	<span class="text-blue mb-1 text-uppercase">
-		<small><strong>{{$topic->name}}</strong></small>
-	@if(! $loop->last)
-	•
-	@endif
-	</span>
+	<span class="text-blue mb-1 text-uppercase"><small><strong>{{$topic->name}}</strong></small>{{! $loop->last ? ' • ' : null}}</span>
 	@empty
 	<div class="text-blue mb-1 text-uppercase"><small><strong>TO READ</strong></small></div>
 	@endforelse
--- resources/views/webapp/piece/components/panel.blade.php
+++ resources/views/webapp/piece/components/panel.blade.php
@@ -14,7 +14,10 @@
 		<div class="panel-body px-2 py-3" style="overflow-y: auto; height: 85%;">
 			<div class="list-group">
 				<a href="{{route('webapp.pieces.composer', $piece)}}" class="link-none mb-3 px-3 d-flex d-apart">Meet the composer @fa(['icon' => 'chevron-right', 'color' => 'muted', 'mr' => 0, 'ml' => 4])</a>
+
+				@if($piece->itunes)
 				<a href="{{route('webapp.pieces.apple-music', $piece)}}" class="link-none mb-3 px-3 d-flex d-apart">On Apple Music @fa(['icon' => 'chevron-right', 'color' => 'muted', 'mr' => 0, 'ml' => 4])</a>
+				@endif
 
 				@if($piece->hasSiblings)
 				<a href="{{route('webapp.pieces.collection', $piece)}}" class="link-none mb-3 px-3 d-flex d-apart">From the same collection @fa(['icon' => 'chevron-right', 'color' => 'muted', 'mr' => 0, 'ml' => 4])</a>
