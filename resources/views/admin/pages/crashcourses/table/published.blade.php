ed7e03d801a65d325db6bd4c489735e714806b13
Arthur Villar <=>
2020-06-05 14:14:21 -0400

    Fix crash course admin index page

--- resources/views/admin/pages/crashcourses/table/published.blade.php
+++ resources/views/admin/pages/crashcourses/table/published.blade.php
@@ -1,3 +1,3 @@
 <div>
-  @toggle(['toggle' => $item->published_at, 'route' => route('admin.crashcourses.update-status', ['quiz' => $item->slug, 'attribute' => 'published_at'])])
+  @toggle(['toggle' => $item->published_at, 'route' => route('admin.crashcourses.update-status', ['crashcourse' => $item, 'attribute' => 'published_at'])])
 </div>
