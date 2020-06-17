73ed7a3469e778c2427619352f8a9f72b3befb19
Arthur Villar <=>
2020-06-17 01:14:22 -0400

    Remove WebApp link from main menu

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,4 +1,8 @@
 @empty($raw)
+<section class="px-2 py-1 border-bottom mb-2 text-center alert-blue">
+  <small>Find your next favorite piece in our WebApp! <a href="{{route('webapp.discover')}}" class="link-none"><strong><u>Login here</u></strong></a></small>
+</section>
+
 <header class="container">
     @include('layouts.menu')
 </header>
