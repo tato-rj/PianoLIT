de10843b7433519ef5b88cad5946a47db07ff8a0
Arthur Villar <=>
2020-06-21 21:01:54 -0400

    Make link to webapp open a new tab

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,6 +1,6 @@
 @empty($raw)
 <section id="webapp-banner" class="px-2 py-2 border-bottom mb-2 text-center alert-blue border-0" style="display: none;">
-  Find your next favorite piece in our WebApp! <a href="{{route('webapp.discover')}}" class="link-none"><strong><u>Login here</u></strong></a>
+  Find your next favorite piece in our WebApp! <a target="_blank" href="{{route('webapp.discover')}}" class="link-none"><strong><u>Login here</u></strong></a>
 </section>
 
 <header class="container">
