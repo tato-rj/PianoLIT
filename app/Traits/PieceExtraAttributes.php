19c90af44ec9889d435d0195f38d5c11403f0a80
Arthur Villar <=>
2020-07-13 10:17:52 -0400

    Fix who is it for autocomplete

--- app/Traits/PieceExtraAttributes.php
+++ app/Traits/PieceExtraAttributes.php
@@ -298,7 +298,9 @@ trait PieceExtraAttributes
             'non-traditional' => in_array($this->period->name, ['modern', 'contemporary']),
             'flashy' => $this->tags_array->contains('flashy'),
             'pedagogical' => $this->composer->is_pedagogical,
-            'relaxing' => (bool) $this->tags_array->intersect(['relaxing', 'dreamy', 'meditative'])
+            'relaxing' => in_array('relaxing', $this->tags_array) ||
+                          in_array('dreamy', $this->tags_array) ||
+                          in_array('meditative', $this->tags_array)
         ];
 
         $sentences = [
