a6ff659fc28faeb07c9a9deeff9fc9d69ad84363
Arthur Villar <=>
2020-08-12 13:21:09 -0400

    Fix youtube api bug

--- resources/js/youtube/YoutubeV3.js
+++ resources/js/youtube/YoutubeV3.js
@@ -2,21 +2,36 @@ class YoutubeV3
 {
 	init() {
 		let obj = this;
-        
-            $.get('https://www.googleapis.com/youtube/v3/channels', {
-                part: 'contentDetails',
-                id: 'UCOB67MpdySxyTCZHvWgxHeg',
-                key: app.youtube_key
-            }, function(data, status, xhr) {
-                $.each(data.items, function(key, item) {
-                    obj._getVideos(item.contentDetails.relatedPlaylists.uploads);
+
+        $.ajax({
+                    url: 'https://www.googleapis.com/youtube/v3/channels', 
+                    data: {
+                        part: 'contentDetails',
+                        id: 'UCOB67MpdySxyTCZHvWgxHeg',
+                        key: app.youtube_key
+                    },
+                    type: "GET",   
+                    dataType: 'jsonp',
+                    cache: false,
+                    success: function(data, status, xhr){                          
+                        $.each(data.items, function(key, item) {
+                            obj._getVideos(item.contentDetails.relatedPlaylists.uploads);
+                        });              
+                    }           
                 });
-            }, 
-            {dataType: 'jsonp'},
-            ).fail(function(response) {
-                console.log('We coudldn\'t load youtube videos in this page.');
-                console.log(response);
-            });
+            // $.get('https://www.googleapis.com/youtube/v3/channels', {
+            //     part: 'contentDetails',
+            //     id: 'UCOB67MpdySxyTCZHvWgxHeg',
+            //     key: app.youtube_key
+            // }, function(data, status, xhr) {
+            //     $.each(data.items, function(key, item) {
+            //         obj._getVideos(item.contentDetails.relatedPlaylists.uploads);
+            //     });
+            // }, 
+            // ).fail(function(response) {
+            //     console.log('We coudldn\'t load youtube videos in this page.');
+            //     console.log(response);
+            // });
     }
 
     _getVideos(id) {
