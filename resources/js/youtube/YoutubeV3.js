9d1474c56172abe658656a63a1ecda8d2b32a3e2
Arthur Villar <=>
2020-08-12 13:24:54 -0400

    Fix youtube api bug

--- resources/js/youtube/YoutubeV3.js
+++ resources/js/youtube/YoutubeV3.js
@@ -2,36 +2,19 @@ class YoutubeV3
 {
 	init() {
 		let obj = this;
-
-        $.ajax({
-                    url: 'https://www.googleapis.com/youtube/v3/channels', 
-                    data: {
-                        part: 'contentDetails',
-                        id: 'UCOB67MpdySxyTCZHvWgxHeg',
-                        key: app.youtube_key
-                    },
-                    type: "GET",   
-                    dataType: 'jsonp',
-                    cache: false,
-                    success: function(data, status, xhr){                          
-                        $.each(data.items, function(key, item) {
-                            obj._getVideos(item.contentDetails.relatedPlaylists.uploads);
-                        });              
-                    }           
+        
+            $.get('https://www.googleapis.com/youtube/v3/channels', {
+                part: 'contentDetails',
+                id: 'UCOB67MpdySxyTCZHvWgxHeg',
+                key: app.youtube_key
+            }, function(data, status, xhr) {
+                $.each(data.items, function(key, item) {
+                    obj._getVideos(item.contentDetails.relatedPlaylists.uploads);
                 });
-            // $.get('https://www.googleapis.com/youtube/v3/channels', {
-            //     part: 'contentDetails',
-            //     id: 'UCOB67MpdySxyTCZHvWgxHeg',
-            //     key: app.youtube_key
-            // }, function(data, status, xhr) {
-            //     $.each(data.items, function(key, item) {
-            //         obj._getVideos(item.contentDetails.relatedPlaylists.uploads);
-            //     });
-            // }, 
-            // ).fail(function(response) {
-            //     console.log('We coudldn\'t load youtube videos in this page.');
-            //     console.log(response);
-            // });
+            }).fail(function(response) {
+                console.log('We coudldn\'t load youtube videos in this page.');
+                console.log(response);
+            });
     }
 
     _getVideos(id) {
