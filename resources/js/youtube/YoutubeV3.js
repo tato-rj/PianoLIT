50635f183b9ca0e8fb0ff5c5f3d6d019010cfe63
Arthur Villar <=>
2020-08-13 13:34:45 -0400

    Fix youtube api bug

--- resources/js/youtube/YoutubeV3.js
+++ resources/js/youtube/YoutubeV3.js
@@ -2,33 +2,44 @@ class YoutubeV3
 {
 	init() {
 		let obj = this;
-        
-            $.get('https://www.googleapis.com/youtube/v3/channels', {
+
+        $.ajax({
+            url: 'https://www.googleapis.com/youtube/v3/channels', 
+            type: "GET",   
+            dataType: 'jsonp',
+            data: {
                 part: 'contentDetails',
                 id: 'UCOB67MpdySxyTCZHvWgxHeg',
                 key: app.youtube_key
-            }, function(data, status, xhr) {
+            },
+            cache: false,
+            success: function(data){
                 $.each(data.items, function(key, item) {
                     obj._getVideos(item.contentDetails.relatedPlaylists.uploads);
-                });
-            }).fail(function(response) {
-                console.log('We coudldn\'t load youtube videos in this page.');
-                console.log(response);
-            });
+                });              
+            }
+        });
     }
 
     _getVideos(id) {
 
-        $.get('https://www.googleapis.com/youtube/v3/playlistItems', {
-            part: 'snippet',
-            maxResults: 20,
-            playlistId: id,
-            key: app.youtube_key
-        }, function(data, status, xhr) {
-            let videos = getRandom(data.items, 2);
-            $('iframe.youtube-video-1').attr('src', 'https://www.youtube.com/embed/' + videos[0].snippet.resourceId.videoId);
-            $('iframe.youtube-video-2').attr('src', 'https://www.youtube.com/embed/' + videos[1].snippet.resourceId.videoId);
-            $('#youtube-previews').css('opacity', 1);
+        $.ajax({
+            url: 'https://www.googleapis.com/youtube/v3/playlistItems', 
+            type: "GET",   
+            dataType: 'jsonp',
+            data: {
+                part: 'snippet',
+                maxResults: 20,
+                playlistId: id,
+                key: app.youtube_key
+            },
+            cache: false,
+            success: function(data){                          
+                let videos = getRandom(data.items, 2);
+                $('iframe.youtube-video-1').attr('src', 'https://www.youtube.com/embed/' + videos[0].snippet.resourceId.videoId);
+                $('iframe.youtube-video-2').attr('src', 'https://www.youtube.com/embed/' + videos[1].snippet.resourceId.videoId);
+                $('#youtube-previews').css('opacity', 1); 
+            }
         });
     }
 }
