7827159df07cc39ae33346400f2ddc6f721f8c39
Arthur Villar <=>
2020-08-20 11:47:43 -0400

    Migrate tutorials to separate table

--- resources/views/webapp/piece/components/video/element.blade.php
+++ resources/views/webapp/piece/components/video/element.blade.php
@@ -1,4 +1,4 @@
 <video class="w-100" id="piece-video-{{$tutorial->id}}">
-	<source src="{{$tutorial->url}}" type="video/mp4">
+	<source src="{{$tutorial->video_url}}" type="video/mp4">
 	Your browser does not support the video tag.
 </video>
