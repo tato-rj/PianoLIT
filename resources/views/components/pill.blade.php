0ae4898675c06646ee60d7907227913f5ed72ed3
Arthur Villar <=>
2020-09-21 10:17:10 -0400

    Complete new explore page

--- resources/views/components/pill.blade.php
+++ resources/views/components/pill.blade.php
@@ -1,3 +1,3 @@
 @if($if ?? true)
-<span class="badge badge-pill bg-{{$color ?? null}} alert-{{$theme ?? null}} absolute-{{$pos ?? null}} text-{{$text ?? null}} noselect">{!! $label !!}</span>
+<span class="badge badge-pill bg-{{$color ?? null}} alert-{{$theme ?? null}} absolute-{{$pos ?? null}} text-{{$text ?? null}} noselect {{$classes ?? null}}" style="{{$styles ?? null}}">{!! $label !!}</span>
 @endif
--- resources/views/webapp/layouts/menu-item.blade.php
+++ resources/views/webapp/layouts/menu-item.blade.php
@@ -1,6 +1,7 @@
-<div class="text-center">
+<div class="text-center position-relative">
 	<a class="menu-link {{url()->current() == $url ? 'active' : null}}" href="{{$url}}">
 		<div>@fa(['icon' => $icon, 'classes' => 'menu-icon', 'mr' => 0])</div>
 		<div><small>{{$label}}</small></div>
 	</a>
+	<small>@pill(['theme' => 'green', 'label' => 'NEW!', 'styles' => 'position: absolute; top: 0;', 'if' => $new ?? false])</small>
 </div>
