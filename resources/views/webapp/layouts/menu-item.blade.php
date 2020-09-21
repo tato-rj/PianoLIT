dc380167896addd903e9db7b25c49e3e357fbb4e
Arthur Villar <=>
2020-09-21 10:18:05 -0400

    Complete new explore page

--- resources/views/webapp/layouts/menu-item.blade.php
+++ resources/views/webapp/layouts/menu-item.blade.php
@@ -2,6 +2,6 @@
 	<a class="menu-link {{url()->current() == $url ? 'active' : null}}" href="{{$url}}">
 		<div>@fa(['icon' => $icon, 'classes' => 'menu-icon', 'mr' => 0])</div>
 		<div><small>{{$label}}</small></div>
+		<small>@pill(['theme' => 'green', 'label' => 'NEW!', 'styles' => 'position: absolute; top: 0;', 'if' => $new ?? false])</small>
 	</a>
-	<small>@pill(['theme' => 'green', 'label' => 'NEW!', 'styles' => 'position: absolute; top: 0;', 'if' => $new ?? false])</small>
 </div>
