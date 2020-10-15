77cd19a9fb59eb6d0367ebf5399e32377d1c4162
Arthur Villar <=>
2020-10-14 22:01:26 -0400

    Fix button on webapp

--- routes/web/search.php
+++ routes/web/search.php
@@ -2,6 +2,8 @@
 
 Route::prefix('search')->name('search.')->group(function() {
 
+	Route::get('', 'SearchController@index')->name('index');
+
 	Route::get('global', 'SearchController@global')->name('global');
 
 });
