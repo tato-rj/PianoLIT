696ddabc633fa1c83994858921e3b7affb9aa8a7
Arthur Villar <=>
2020-10-15 23:41:12 -0400

    Fix quiz bug

--- resources/views/shop/components/display/lg.blade.php
+++ resources/views/shop/components/display/lg.blade.php
@@ -1,8 +1,5 @@
 <div class="row mb-5">
-	<div class="col-lg-6 col-md-6 col-12 mb-3">
-		@include('shop.components.cover', ['maxWidth' => '280px'])
-	</div>
-	<div class="col-lg-6 col-md-6 col-10 mx-auto d-flex align-items-center mb-3">
+	<div class="col-lg-6 col-md-6 col-10 mx-auto d-flex align-items-center mb-3 order-lg-1 order-md-1 order-2">
 		<div>
 			@topics(['topics' => $product->topics])
 			<div class="mb-2">
@@ -17,24 +14,8 @@
 			</div>
 		</div>
 	</div>
-</div>
-<div class="row mb-5">
-	<div class="col-lg-6 col-md-6 col-10 mx-auto mb-3">
-		@include('shop.components.cover', ['maxWidth' => '280px'])
-	</div>
-	<div class="col-lg-6 col-md-6 col-10 mx-auto d-flex align-items-center mb-3">
-		<div>
-			@topics(['topics' => $product->topics])
-			<div class="mb-2">
-				<h4 class=" clamp-2"><strong>{{$product->title}}</strong></h4>
-				<p>{{$product->subtitle}}</p>
-				@include('shop.components.price')
-			</div>
-			
-			<div>
-				@include('shop.components.action')
-				<a href="{{$product->showRoute()}}" class="btn btn-sm btn-wide btn-outline-secondary mb-2">@fa(['icon' => 'info-circle'])More details</a>
-			</div>
-		</div>
+
+	<div class="col-lg-6 col-md-6 col-12 mb-3 order-lg-2 order-md-2 order-1">
+		@include('shop.components.cover', ['maxWidth' => '320px'])
 	</div>
 </div>
