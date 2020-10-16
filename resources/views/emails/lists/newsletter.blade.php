bbdecc32cf1ce3d76b505178f160093012e86740
Arthur Villar <=>
2020-10-16 10:29:03 -0400

    Update Nov 2020 newsletter

--- resources/views/emails/lists/newsletter.blade.php
+++ resources/views/emails/lists/newsletter.blade.php
@@ -1,17 +1,13 @@
 @component('mail::message', ['subscription' => $subscription, 'list' => $list])
 
 @include('mail::newsletter.quote', [
-	'quote' => 'I often think in music. I live my daydreams in music. I see my life in terms of music.',
-	'author' => 'A. Einstein'
+	'quote' => 'When legendary cellist Pablo Casals was asked why he continued to practice at age 90, he responded: “Because I think I am making progress”',
+	'author' => 'P. Casals'
 ])
 
-<p>If September is your back-to-class or just back-to-reality month, we have some new great piano music that will make this transition from summer to early fall a bit easier. Get the PianoLIT app and start exploring.</p>
+<p>With October we have officially surpassed 3/4 of year 2020. And you know what this means: we are approaching the holidays and spending a whole lot more time indoor. So for your practicing time, we have some perfect pieces to get into the Halloween spirit.</p>
 
-<p>We are very excited to announce two new resources we are releasing this month!</p>
-
-<p>Our new <strong>eBooks</strong> will cover a wide variety of piano related topics, ranging from theory, sight reading, history and more. We are also developing <strong>eScores</strong>, which are our own editions of impossible-to-find piano scores. These are true gems of the piano repertoire for every mood and level.</p>
-
-<p class="text-center"><strong>STAY TUNED🤗!</strong></p>
+<p>Head over to the <a href="https://my.pianolit.com/playlists" target="_blank">PianoLIT App</a> and start exploring our featured playlist “Scary vibes”.</p>
 
 @component('mail::panel')
 # How do you think we're doing?
@@ -23,53 +19,51 @@ If you’ve downloaded the PianoLIT app, please tell us what you think <a href="
 @component('mail::newsletter.section', [
 	'badge' => [
 		'name' => 'listen', 
-		'color' => 'green',
+		'color' => 'orange',
 		'link' => 'https://www.youtube.com/watch?v=RPFURmmnEHs'
 	],
-	'title' => 'When a dream is translated for piano'
+	'title' => 'A haunted melody'
 ])
 
-<p>Many composers throughout music history have tried to capture this feeling of dreaming in their music. Whether is a day-dreaming kind of state, also called rêverie, or a night-dreaming the point is to create melodies that capture perfectly this lost-in-thought, meditative mood.</p>
+<p>When thinking about a piece to feature during the Halloween month, nothing better than Alkan’s “Song of the Madwoman by the Seashore”. A brilliant piece with a dark and sinister atmosphere. So evocative that it will get you chills down your spine while playing! While most of Alkan’s compositions are advanced music, this piece which is part of his Preludes Op. 31 is more around the late intermediate level.</p>
 
-<p><a href="https://www.youtube.com/watch?v=-mNdUXpYLsk" class="text-md"><strong>Watch Dreaming Opus 15 No.3 by Amy Beach</strong></a></p>
+<p><a href="https://youtu.be/mC_yUTPg_Kg" class="text-md"><strong>Watch “Song of the Madwoman by the Seashore” by C. V. Alkan</strong></a></p>
 
-<p><a href="https://my.pianolit.com/storage/app/score/pianolit-dreaming-in-gb-major-from-4-sketches-op15-no3-8596.pdf" class="text-md"><strong>Download the FREE score here</strong></a></p>
+<p><a href="https://my.pianolit.com/storage/app/score/pianolit-prelude-in-ab-minor-from-25-preludes-op31-no8-song-of-the-madwoman-on-the-seashore-1850.pdf" class="text-md"><strong>Download the FREE score here</strong></a></p>
 
-<p>Based on a poem by French poet Victor Hugo, this is a remarkable piece in every possible way. The writing is intricate and expressive, the musical ideas are original and encompass a wide range of emotions.</p>
+<p>Charles Valentine Alkan was a pianist active during the Parisian years that saw Chopin and Liszt a the heart of the musical scene. Alkan was there among them. Extremely talented and a virtuoso, his music is very demanding technically. When he turned 35, Alkan began to adopt a reclusive life style, while continuing with his compositions.</p>
+@endcomponent
 
-<p>The main challenge here is to play the accompaniment parts. The rhythm and direction of the notes are intentionally inconsistent, and the eighth notes move freely around the harmony as if it was floating in the air, like in a dream.</p>
+@component('mail::newsletter.section', [
+	'badge' => [
+		'name' => 'free ebook', 
+		'color' => 'green',
+		'link' => 'https://pianolit.com/ebooks/become-a-better-sight-reader-in-5-days'
+	],
+	'title' => 'Become a better sight-reader in 5 days - Start reading like a pro with practical tips and music scores for each day'
+])
+
+<p>If you've played piano before, you’re probably familiar with the concept of sight-reading. Sight-reading is the practice of reading and performing a piece that you've never seen before. The benefits of skillful sight-reading are many, but don’t fall in the mistake of thinking that sight-readers are born that way. The art of sight-reading can, and should, be taught.</p>
+
+@component('mail::button', ['url' => 'https://pianolit.com/ebooks/become-a-better-sight-reader-in-5-days'])
+Download the FREE eBook now
+@endcomponent
+
 @endcomponent
 
 @component('mail::newsletter.section', [
 	'badge' => [
 		'name' => 'read', 
-		'color' => 'orange',
-		'link' => 'https://pianolit.com/blog/the-romantic-period-in-a-nutshell'
-	],
-	'title' => 'The Romantic period in a nutshell'
-])
-
-<p>A quick glance at the Romantic period in music, with a special focus on the role of piano and the compositions that characterized this time in history.</p>
-
-@component('mail::button', ['url' => 'https://pianolit.com/blog/the-romantic-period-in-a-nutshell'])
-Read more about this
-@endcomponent
-
-@endcomponent
-
-@component('mail::newsletter.section', [
-	'badge' => [
-		'name' => 'quiz', 
 		'color' => 'pink',
-		'link' => 'https://pianolit.com/quizzes/how-many-correct-answers-can-you-get-the-basics-you-should-know-about-scriabin'
+		'link' => 'https://pianolit.com/blog/core-piano-repertoire-that-every-pianist-should-know'
 	],
-	'title' => 'QUIZ: How many correct answers can you get? The basics you should know about Scriabin'
+	'title' => 'Core piano repertoire that every pianist should know'
 ])
 
-<p>Test your knowledge about the great Russian composer Alexander Scriabin and his fascinating music!</p>
+<p>The piano repertoire is vast, but there are some pieces that every pianist, regardless of the level, should absolutely know. Here's the list.</p>
 
-@component('mail::button', ['url' => 'https://pianolit.com/quizzes/how-many-correct-answers-can-you-get-the-basics-you-should-know-about-scriabin'])
-Let me try!
+@component('mail::button', ['url' => 'https://pianolit.com/blog/core-piano-repertoire-that-every-pianist-should-know'])
+Read the blog post
 @endcomponent
 @endcomponent
 
@@ -77,24 +71,24 @@ Let me try!
 	'badge' => [
 		'name' => 'infographic', 
 		'color' => 'yellow',
-		'link' => 'https://pianolit.com/resources/infographs/women-composers'
+		'link' => 'https://pianolit.com/resources/infographs/speed-markings'
 	],
-	'title' => 'Women Composers of Classical Music'
+	'title' => 'Speed markings'
 ])
 
-<p>This infographic highlights 6 remarkable women composers of classical music, from the early 19th century until today.</p>
+<p>The most commonly used speed markings, along with a quick description for each of them.</p>
 
-@component('mail::button', ['url' => 'https://pianolit.com/resources/infographs/women-composers'])
+@component('mail::button', ['url' => 'https://pianolit.com/resources/infographs/speed-markings'])
 Download the infographic for FREE!
 @endcomponent
 @endcomponent
 
-<h1 class="text-lg mb-4">True or False: Dreaming in music</h1>
+<h1 class="text-lg mb-4">True or False: Charles Valentin Alkan’s music</h1>
 @include('mail::lists.numbered', ['items' => [
-	'<strong> Debussy composed not one but three pieces called <i>rêverie</i>:</strong> TRUE | FALSE',
-	'<strong>Liszt’s Liebestraum translates to Dreams of Love:</strong> TRUE | FALSE',
-	'<strong>The famous Schumann’s piece Traumerei (Dreaming) is part of the composer’s collection called "Scenes from Childhood”</strong> TRUE | FALSE',
-	'<strong>Amy Beach piece “Dreaming”, opus 15 no.3 is based on a poem by Victor Hugo:</strong> TRUE | FALSE'
+	'<strong>He composed a funeral march for the death of a parrot:</strong> TRUE | FALSE',
+	'<strong>He was a reclusive throughout his life and never met his famous colleagues Chopin and Liszt:</strong> TRUE | FALSE',
+	'<strong>He was a child prodigy and later on a virtuoso:</strong> TRUE | FALSE',
+	'<strong>He was a dedicated pedagogue and most of his repertoire is for beginner level:</strong> TRUE | FALSE'
 ]])
 
 <div class="text-center mt-4">Find the answers at the bottom of the newsletter!</div>
@@ -112,28 +106,36 @@ Check out the PianoLIT WebApp
 <h1 class="text-lg mb-4">COOL FACTS TO SHARE THIS MONTH:</h1>
 
 @component('mail::panel')
-Are you prone to road rage at times? The German government is worried about the high number of road accidents on the country’s motorways (2.4 million annually). Many of these accidents are caused by aggressive driving and road rage. 
+On the occasion of Beethoven's 250th birthday, the Kunsthistorisches Museum in Vienna serves up a very special exhibition called “”Beethoven moves. It combines paintings, sketchbooks, graphics, sculptures, and videos with music and Beethoven the person. 
 
-To counteract this, the German Ministry of Transport has released a CD for drivers which features Mozart’s Piano Concerto No.21. played by the Minister himself! He hopes that the soothing effects of music will calm drivers down. 
+The exhibition is to be seen as a poetic reflection of his creative work. Beethoven's masterpieces enter into a connection with the masterpieces of the visual arts (painting, sculpture, graphics, drawings, photography, and video) and thus create a new total work of art of Beethoven. 
 
-(Fun fact: There is no word in German for road rage).
+The visitor is invited to locate their own body in the space and thus to put it in relationship to the music, language, image, and movement. A whole-body experience that leads to a very special encounter with the great composer.
 
-Read more <a href="https://www.wqxr.org/story/168660-german-minister-combats-road-rage-mozart/" target="_blank">HERE</a>.
+Read more <a href="https://www.wien.info/en/sightseeing/museums-exhibitions/beethoven-moves-khm" target="_blank">HERE</a>.
+
+Watch the installation <a href="https://www.instagram.com/p/CF3yV48BBHz/?utm_source=ig_web_copy_link" target="_blank">HERE</a> (turn up your sound!).
 @endcomponent
 
 @component('mail::panel')
-Ever wondered how Clara Schumann would look like in person? Well, CGI artist Had Karimi is providing us with a window to the past! Check out Clara’s bust reconstruction here.
+Remember when Wilma and Betty headed out for a night of Leonard Bernstone conducting Rockymaninoff at the Hollyrock Bowl? Watch it again! :)
 
-Check it out <a href="https://cgsociety.org/c/featured/fcup/clara-schumann-1856" target="_blank">HERE</a>.
+Watch the video <a href="https://www.youtube.com/watch?v=Gy75VoLv04c" target="_blank">HERE</a>.
 @endcomponent
 
 @component('mail::panel')
-Fans have flocked to a church in Germany to hear a chord change in a musical composition that lasts for 639 years. Yes, it’s not a typo. The work is by the avant-garde American composer, John Cage called As Slow As Possible. It began 19 years ago and changed chord for first time in seven years. The score is made up of eight pages of music, to be played at the piano or organ - very slowly. So slowly that the piece will end in 2640.
+Have you ever heard of robots playing the cello? You’re in for a treat! Check out this Work In Progress: ”Cello Concerto No.1” - for industrial robots and string quintet (17′) (2019)
 
-Read more about this <a href="https://www.bbc.com/news/world-europe-54041568" target="_blank">HERE</a>.
+Watch it <a href="https://www.youtube.com/watch?v=8srZQ5iZDoY" target="_blank">HERE</a>.
 @endcomponent
 
-<div class="mb-4 mt-4 text-center">Answers: 1. FALSE, 2. TRUE, 3. TRUE, 4. TRUE</div>
+@component('mail::panel')
+One of the most distinctive concert halls in the world! 🤩 The Palau in Barcelona. We cannot wait till concert halls like this are filled with music once again.
+
+Watch the video <a href="https://www.youtube.com/watch?v=yr8TIFkppkA" target="_blank">HERE</a>.
+@endcomponent
+
+<div class="mb-4 mt-4 text-center">Answers: 1. TRUE, 2. FALSE, 3. TRUE, 4. FALSE</div>
 
 <p>Here at pianoLIT we are working non-stop to improve and expand our music library. If you wish to see the recording of any piece, please reply to this email with your request and we’ll jump on it. We’ve already received some great input (you’ll see them soon in the app!) and thanks our users for having such great ideas.</p>
 
