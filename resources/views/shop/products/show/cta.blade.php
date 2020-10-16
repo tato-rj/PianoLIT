fd73f02f75b4ec03ac4eb87b3cecc6ed6aaf9491
Arthur Villar <=>
2020-10-16 00:03:03 -0400

    Fix quiz bug

--- resources/views/shop/products/show/cta.blade.php
+++ resources/views/shop/products/show/cta.blade.php
@@ -1,17 +1,19 @@
-<div class="bg-light rounded row mb-5">
-	<div class="col-lg-9 col-md-9 col-12 p-5 d-flex align-items-center">
-		<div>
-			<h4 class="mb-4 clamp-2"><strong>{{$product->title}}</strong></h4>
-			@include('shop.components.highlights')
-			<div class="mb-2">
-				@include('shop.components.price')
-			</div>
+<div class="bg-light rounded p-5">
+	<div class="row">
+		<div class="col-lg-8 col-md-8 col-12 d-flex align-items-center">
 			<div>
-				@include('shop.components.action')
-			</div>	
+				<h4 class="mb-4 clamp-2"><strong>{{$product->title}}</strong></h4>
+				<p>{{$product->subtitle}}</p>
+				<div class="mb-2">
+					@include('shop.components.price')
+				</div>
+			</div>
 		</div>
-	</div>
-	<div class="col-lg-3 col-md-3 col-12 py-5 pr-5">
-		@include('shop.components.cover')
+		<div class="col-lg-4 col-md-4 col-12">
+			@include('shop.components.highlights')
+				<div>
+					@include('shop.components.action')
+				</div>	
+		</div>
 	</div>
 </div>
