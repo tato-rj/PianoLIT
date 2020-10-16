132895cbf533d4b617736a4db2a467110ad24eac
Arthur Villar <=>
2020-10-16 13:46:51 -0400

    Show who purchased ebooks

--- resources/views/components/tables/more.blade.php
+++ resources/views/components/tables/more.blade.php
@@ -1,5 +1,5 @@
 <div class="border-left border-bottom border-right mb-4">
-	<button data-target="#{{$id}}" data-url="{{$more}}" class="btn border-0 btn-light rounded-0 btn-block load-more">
+	<button data-target="#{{$id}}" data-url="{{$more}}" class="btn border-0 btn-light rounded-0 btn-block load-more" style="border-radius: 0 !important">
 	  LOAD MORE
 	</button>
 </div>
