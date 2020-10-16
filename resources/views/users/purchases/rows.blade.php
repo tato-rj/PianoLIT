35bfdee2a71e815e396c4a7d0df8ea97920f835b
Arthur Villar <=>
2020-10-16 13:50:07 -0400

    Show who purchased ebooks

--- resources/views/users/purchases/rows.blade.php
+++ resources/views/users/purchases/rows.blade.php
@@ -1,16 +1,18 @@
 @foreach($purchases as $purchase)
-  <tr>
-    <td class="text-nowrap">{{$purchase->created_at ? $purchase->created_at->toFormattedDateString() : null}}</td>
-    <td>{{$purchase->type}}</td>
-    <td class="text-nowrap">{{$purchase->item->title}}
-    @if($purchase->isFree)
-    <span class="badge badge-pill alert-green">FREE</span>
+    @if($purchase)
+      <tr>
+        <td class="text-nowrap">{{$purchase->created_at->toFormattedDateString()}}</td>
+        <td>{{$purchase->type}}</td>
+        <td class="text-nowrap">{{$purchase->item->title}}
+        @if($purchase->isFree)
+        <span class="badge badge-pill alert-green">FREE</span>
+        @endif
+        </td>
+        <td class="text-nowrap">
+            @foreach($purchase->item->links() as $label => $hash)
+            <a href="{{route('shop.download', ['purchase' => $purchase, 'path' => $hash])}}">{{$label}}</a> {{$loop->count > 1 && ! $loop->last ? ' | ' : null}}
+            @endforeach
+        </td>
+      </tr>
     @endif
-    </td>
-    <td class="text-nowrap">
-        @foreach($purchase->item->links() as $label => $hash)
-        <a href="{{route('shop.download', ['purchase' => $purchase, 'path' => $hash])}}">{{$label}}</a> {{$loop->count > 1 && ! $loop->last ? ' | ' : null}}
-        @endforeach
-    </td>
-  </tr>
 @endforeach
