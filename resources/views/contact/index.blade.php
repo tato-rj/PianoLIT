0b4c2053827c3baaadc82392e686be7c1f3891aa
Arthur Villar <=>
2020-10-26 15:16:41 -0400

    Redo all grids pages

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -6,7 +6,7 @@
         <div class="col-lg-6 col-md-6 col-sm-8 col-12 mx-auto">
             <h3 class="accent-bottom mb-4">Contact us</h3>
             <p class="mb-4">If you wish to ask us a question or just say hello, send us an email!</p>
-            <a href="mailto:{{config('app.emails.general')}}" class="link-blue"><i class="fas fa-envelope mr-2"></i>{{config('app.emails.general')}}</a>
+            <a href="mailto:{{config('app.emails.general')}}"><i class="fas fa-envelope mr-2"></i>{{config('app.emails.general')}}</a>
         </div>
     </div>
 </div>
