20dd213491e96a7c5827b4380314a5dd187d3e28
Arthur Villar <=>
2020-10-26 23:19:43 -0400

    Remove macintel from ios list

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -10,16 +10,16 @@
 	<div class="row position-relative"> 
 		@include('search.components.results.pieces', ['pieces' => $results->take(8)])
 
+		@if($results->count() > 4)
 		<div style="
 		position: absolute;
 		bottom: 0;
 		left: 0;
 		width: 100%;
 		height: 40%;
-background: rgb(255,255,255);
-background: linear-gradient(0deg, rgba(255,255,255,1) 25%, rgba(255,255,255,0) 100%);">
-			
-		</div>
+		background: rgb(255,255,255);
+		background: linear-gradient(0deg, rgba(255,255,255,1) 25%, rgba(255,255,255,0) 100%);"></div>
+		@endif
 	</div>
 </section>
 
