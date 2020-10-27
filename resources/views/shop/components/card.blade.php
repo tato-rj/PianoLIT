11d2fc584c91d6f72fe9027a81403568ec82ff18
Arthur Villar <=>
2020-10-26 22:18:32 -0400

    Add loader to masonry grids

--- resources/views/shop/components/card.blade.php
+++ resources/views/shop/components/card.blade.php
@@ -1,4 +1,4 @@
-<div class="grid-item rounded-0 thumbnail p-2 col-lg-4 col-md-6 col-12">
+<div class="grid-item rounded-0 thumbnail p-2 col-lg-4 col-md-6 col-6">
 	<div class="position-relative no-click border">
 		<a href="{{$item->showRoute()}}" class="link-none">
 			@include('shop.components.cover', ['product' => $item])
