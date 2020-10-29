0fdcd76feddb6631bdbf22b32d1eb1302309952b
Arthur Villar <=>
2020-10-29 00:05:37 -0400

    Add ads to pages

--- resources/views/admin/pages/requests/table/actions.blade.php
+++ resources/views/admin/pages/requests/table/actions.blade.php
@@ -6,7 +6,7 @@
 	    @if($item->isPublished())
 	    <div class="text-success text-nowrap"><i class="fas fa-check-circle mr-1"></i>Published</div>
 	    @else
-	    <a href="#" data-url="{{route('admin.tutorial-requests.publish', $item->id)}}" data-toggle="modal" data-target="#modal-publish-tutorial" class="btn btn-sm btn-warning text-nowrap"><i class="fas fa-hourglass-half mr-1"></i>Publish</a>
+	    <a href="#" data-url="{{route('admin.tutorial-requests.publish', $item->id)}}" data-toggle="modal" data-target="#publish-tutorial" class="btn btn-sm btn-warning text-nowrap"><i class="fas fa-hourglass-half mr-1"></i>Publish</a>
 	    @endif
 	</div>
 </div>
