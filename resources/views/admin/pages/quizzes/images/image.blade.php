81aa32ad8ffb682ca58e7f9135787e80d453d46e
Arthur Villar <=>
2020-10-29 11:21:29 -0400

    Fix missing quiz views on admin

---
+++ resources/views/admin/pages/quizzes/images/image.blade.php
@@ -0,0 +1,14 @@
+<div class="m-3">
+	<div class="position-relative w-100 shadow-sm">
+		<img src="{{storage($file['file'])}}" style="width: auto; height: 160px">
+		<div class="show-on-hover">
+		    <div class="m-0 absolute-center z-10 text-center">
+		        <a class="link-none" href="">
+		        	<div class="cursor-pointer text-warning clip mb-4" data-clipboard-text="/storage/{{$file['file']}}" data-toggle="tooltip" title="Copied!" data-trigger="manual"><strong>copy</strong></div>
+		        </a>
+		        <button class="border-0 bg-transparent p-0 text-danger cursor-pointer remove-file font-weight-bold" data-path="{{route('admin.quizzes.media.destroy', ['path' => $file['file']])}}">delete</button>
+		    </div>
+		    <div class="overlay w-100 h-100 bg-white z-0" style="opacity: 0.95"></div>                
+		</div>
+	</div>
+</div>
