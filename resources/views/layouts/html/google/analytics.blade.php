a42b18d42435ae9832e781607f565f3c0f72eed0
Arthur Villar <=>
2020-10-30 22:16:49 -0400

    Remove Google Analytics code and leave only the Tag Manager in

--- resources/views/layouts/html/google/analytics.blade.php
+++ resources/views/layouts/html/google/analytics.blade.php
@@ -1,8 +1,10 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-137232556-1"></script>
+@env('production')
+{{-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-137232556-1"></script>
 <script>
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
 gtag('config', 'UA-137232556-1');
-</script>
+</script> --}}
+@endenv
--- resources/views/layouts/html/google/manager-body.blade.php
+++ resources/views/layouts/html/google/manager-body.blade.php
@@ -1,2 +1,4 @@
+@env('production')
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KJW7XGP"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+@endenv
