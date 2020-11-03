8974d2acb3da206965a9551223948e3bdb00eeef
Arthur Villar <=>
2020-11-02 23:54:38 -0500

    Change freepick for video

--- resources/views/webapp/discover/rows/freepick.blade.php
+++ resources/views/webapp/discover/rows/freepick.blade.php
@@ -2,9 +2,6 @@
 	<h5 class="mb-3">Free weekly pick</h5>
 	<div class="">
 		@foreach($row['content'] as $piece)
-		@if(auth()->user()->id == 284)
-		@php($piece = \App\Piece::find(816))
-		@endif
 			@include('webapp.components.piece.highlight')
 		@endforeach
 	</div>
