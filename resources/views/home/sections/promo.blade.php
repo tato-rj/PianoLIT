bdf5de4c0a8971611ca4c980e5c3e87b5fe3000e
Arthur Villar <=>
2020-11-03 00:12:00 -0500

    Add promo video to website

--- resources/views/home/sections/promo.blade.php
+++ resources/views/home/sections/promo.blade.php
@@ -2,13 +2,23 @@
 	<section class="container mb-5">
 		<div class="row align-items-center py-5">
 			<div class="col-lg-6 col-md-6 col-sm-6 col-12">
-				<div class="p-2">
+				<div class="p-2 my-2">
 					<h2>What is PianoLIT?</h2>
-					<p class="mb-0 lead" style="font-size: 120%">PianoLIT is the first search engine for pianists that helps you navigate the repertoire and find pieces that match your level, mood, and interest.</p>
+					<p class="lead" style="font-size: 120%">PianoLIT is the first search engine for pianists that helps you navigate the repertoire and find pieces that match your level, mood, and interest.</p>
+				@button([
+					'href' => route('webapp.discover'),
+					'label' => 'Start FREE trial',
+					'styles' => [
+						'shadow' => true,
+						'size' => 'wide',
+						'theme' => 'white',
+					],
+					'data' => ['ios' => config('app.stores.ios')],
+					'classes' => 'free-trial-launch'])
 				</div>
 			</div>
 			<div class="col-lg-6 col-md-6 col-sm-6 col-12">
-				<div class="p-2 shadow bg-white" style="  position: relative;
+				<div class="p-2 my-2 shadow bg-white" style="  position: relative;
   overflow: hidden;
   width: 100%;
   height: 315px;
