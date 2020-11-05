62c7b4633711fbb7d02a6b52d3208342e9aa9e1e
Arthur Villar <=>
2020-11-05 13:46:52 -0500

    Update downloads page

--- resources/views/components/pagination/count.blade.php
+++ resources/views/components/pagination/count.blade.php
@@ -1,5 +1,5 @@
 @if($collection)
-<div class="text-{{$align ?? 'center'}} text-muted mb-2">
+<div class="text-{{$align ?? 'center'}} text-muted mb-1">
 <small>Showing {{$collection->firstItem()}}-{{$collection->lastItem()}} of {{$collection->total()}} {{str_plural($item ?? 'item', $collection->total())}}</small>
 </div>
 @endif
