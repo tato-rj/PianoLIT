b9b68866f58535d72ac5624d92788275c985ab4f
Arthur Villar <=>
2020-11-05 13:44:03 -0500

    Update downloads page

--- resources/views/users/purchases/index.blade.php
+++ resources/views/users/purchases/index.blade.php
@@ -6,7 +6,7 @@
 @section('content')
 <div class="container mb-6">
 	<div class="row">
-	  <div class="col-lg-8 col-md-8 col-sm-6 col-12 mx-auto">
+	  <div class="col-lg-8 col-md-8 col-12 mx-auto">
 		<div class="text-center mb-4">
 			<h5>My downloads</h5>
 			<p>Here is a list of all the products you've downloaded/purchased so far</p>
