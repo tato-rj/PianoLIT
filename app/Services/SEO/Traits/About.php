523fb759eaac0ec937b38d57ba6f5a4e2d5746f6
Arthur Villar <=>
2020-11-10 15:01:40 -0500

    Create a dedicated SEO class

--- app/Services/SEO/Traits/About.php
+++ app/Services/SEO/Traits/About.php
@@ -4,7 +4,7 @@ namespace App\Services\SEO\Traits;
 
 trait About
 {
-	protected $title = 'PianoLIT - Find music that inspires you';
+	protected $title = 'PianoLIT - Where pianists discover new pieces';
 
 	protected $description = 'PianoLIT is the first search engine for pianists that helps you navigate the repertoire and find pieces that match your level, mood, and interest.';
 }
