9e8844b6f5ed21826d89e3ed4d31f87a19c22ef1
Arthur Villar <=>
2020-11-10 15:16:03 -0500

    Create a dedicated SEO class

--- resources/views/webapp/components/piece/highlight.blade.php
+++ resources/views/webapp/components/piece/highlight.blade.php
@@ -1,4 +1,4 @@
-<div class=" cursor-pointer bg-align-center rounded d-flex d-apart flex-column p-3 piece-card" data-url="{{route('webapp.pieces.show', $piece)}}" style="background-image: url({{$piece->image_background}}); height: {{$height ?? '200px'}}; width: {{$width ?? null}}">
+<div class=" cursor-pointer bg-align-center rounded d-flex d-apart flex-column p-3 piece-card" role="img" aria-label="{{$piece->name}}" data-url="{{route('webapp.pieces.show', $piece)}}" style="background-image: url({{$piece->image_background}}); height: {{$height ?? '200px'}}; width: {{$width ?? null}}">
 	<div class="w-100 text-white">
 		<h6 class="m-0 text-white clamp-2">{{$piece->name}}</h6>
 		<p class="m-0 text-white">{{$piece->composer->short_name}}</p>
