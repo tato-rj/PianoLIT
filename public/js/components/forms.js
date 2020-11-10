5afc73b20c81befefabc2f9b14db32517ed9709c
Arthur Villar <=>
2020-11-10 14:11:46 -0500

    Add margin to loading icon on buttons

--- public/js/components/forms.js
+++ public/js/components/forms.js
@@ -4,7 +4,7 @@ $(document).on('submit', 'form[disable-on-submit]', function() {
                                 left: 50%;
                                 -webkit-transform: transform: translate(-50%, -50%);
                                 transform: translate(-50%, -50%);">
-                        <div class="spinner-border opacity-8" style="width: 1rem; height: 1rem; border-width: .16em"></div>
+                        <div class="spinner-border opacity-8" style="width: 1rem; height: 1rem; border-width: .16em; margin-bottom: .1rem;"></div>
                     </div>`;
     let $button = $(this).find('button[type="submit"]');
 
