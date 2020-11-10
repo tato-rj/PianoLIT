07d5870c5f4193c092a4cc87bc7562a861138740
Arthur Villar <=>
2020-11-10 10:50:36 -0500

    Block James Smith bot from registering

--- tests/Feature/SpamTest.php
+++ tests/Feature/SpamTest.php
@@ -3,10 +3,13 @@
 namespace Tests\Feature;
 
 use Tests\AppTest;
-use App\Subscription;
+use App\{Subscription, User};
+use Tests\Traits\ManageDatabase;
 
 class SpamTest extends AppTest
 {
+    use ManageDatabase;
+
     /** @test */
     public function if_the_hidden_input_is_filled_it_means_the_guest_is_a_bot_so_the_subscription_is_denied()
     {
@@ -37,5 +40,12 @@ class SpamTest extends AppTest
         $email = make(Subscription::class)->email;
 
         $this->subscribe($email, $bot = false, $wait = false);
+    }
+
+    /** @test */
+    public function users_named_james_smith_cannot_register_on_the_webapp()
+    {
+        $this->register(make(User::class, ['first_name' => 'James', 'last_name' => 'Smith']))
+             ->assertSessionHas('error');
     }
 }
