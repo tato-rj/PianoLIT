488e5f28465e3b2b3634418e74601b367afd8ee0
Arthur Villar <=>
2020-11-10 22:52:42 -0500

    Create a dedicated SEO class

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -16,9 +16,66 @@
 @endsection
 
 @push('scripts')
-<script type="text/javascript" src="{{asset('js/components/testimonials.js')}}"></script>
-<script type="text/javascript" src="{{asset('js/components/tagsearch.js')}}"></script>
-<script type="text/javascript" src="{{asset('js/animations/phonescreens.js')}}"></script>
+<script type="text/javascript">
+  var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      width: 326,
+      initialSlide: $('.swiper-slide').length/2,
+      spaceBetween: 30,
+      centeredSlides: true,
+      pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+      },
+      navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+      },
+  });
+</script>
+
+<script type="text/javascript">
+let scrollMark = $('#screens-composition').offset().top - 300;
+
+$(window).scroll(function() {
+  let scrollTop = $(this).scrollTop();
+
+  if (scrollTop > scrollMark){
+    $('#screens-container img:nth-child(2)').css('margin-left', '192px');
+    $('#screens-container img:nth-child(3)').css('margin-left', '-192px');
+    $('#screens-container img:nth-child(4)').css('margin-left', '328px');
+    $('#screens-container img:nth-child(5)').css('margin-left', '-328px');
+  } else {
+    $('#screens-container img').css('margin-left', '0');
+  }
+});
+</script>
+
+<script type="text/javascript">
+$('#tags-search .tag').on('click', function() {
+  let $btn = $(this);
+  let $container = $('#pieces-container');
+  let $label = $('#pieces-label');
+
+  $btn.disable().toggleClass('btn-teal');
+  $container.parent().addClass('opacity-4');
+
+  axios.get($container.attr('data-url'), {params: {
+    'ids': $('#tags-search .btn-teal').attrToArray('data-id'), 
+    'names': $('#tags-search .btn-teal').attrToArray('data-name')
+  }}).then(function(response) {
+      $container.html(response.data.view); 
+      $container.parent().removeClass('opacity-4');
+      $label.text(response.data.label);
+    })
+    .catch(function(error) {
+      console.log(error);
+    })
+    .then(function() {
+      $btn.enable();
+    });
+});
+</script>
 
 <script type="text/javascript">
 $(document).ready(function() {
