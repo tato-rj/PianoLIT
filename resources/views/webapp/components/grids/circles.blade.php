3487f7202b94a176a0d145aee25f0f6e624b7ab4
Arthur Villar <=>
2020-11-10 22:59:26 -0500

    Create a dedicated SEO class

--- resources/views/webapp/components/grids/circles.blade.php
+++ resources/views/webapp/components/grids/circles.blade.php
@@ -2,7 +2,7 @@
 	@foreach($collection as $model)
 	<div class="cursor-pointer mr-3 text-center search-card" data-url="{{route($route ?? 'webapp.search.results', ['search' => $model->$name])}}">
 		<img src="{{$model->$image}}" class="rounded-circle mb-2" style="width: 114px" alt="{{$model->$name}}">
-		<p class="m-0 clamp-2" style="line-height: 1"><small><strong>{{$model->$name}}</strong></small></p>
+		<p class="m-0 clamp-2 font-weight-bold" style="line-height: 1"><small>{{$model->$name}}</small></p>
 		<p class="text-muted m-0"><small>{{$model->$count}} pieces</small></p>
 	</div>
 	@endforeach
