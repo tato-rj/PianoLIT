9c8a5ed6332fbb3bad071bc60d973c5c9926af68
Arthur Villar <=>
2020-11-11 14:30:41 -0500

    Add recaptcha to all subscription forms

--- resources/views/components/sections/feedback.blade.php
+++ resources/views/components/sections/feedback.blade.php
@@ -4,7 +4,7 @@
 			<div style="max-width: 400px" class="mx-auto">
 				<h4 class="mb-3">Did you find this useful?</h4>
 				<p>Subscribe to our newsletter and be the first one to know when a new tool like this one is out!</p>
-				@include('components.form.subscription')
+				@include('components.form.subscription', ['id' => 'feedback-subscription-form'])
 			</div>
 		</div>
 	</div>
