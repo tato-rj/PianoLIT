3a4e66ca375597698d63267032411c6bfdb98811
Arthur Villar <=>
2020-11-13 11:00:58 -0500

    Create birthdays page

--- app/Http/Controllers/ComposersController.php
+++ app/Http/Controllers/ComposersController.php
@@ -9,7 +9,7 @@ class ComposersController extends Controller
 {
     public function birthdays()
     {
-        $months = ['january', 'february', 'march', 'april', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
+        $months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
         $calendar = Composer::all()->where('is_famous', true)->sortBy('month_of_birth')->groupBy('month_of_birth');
 
         if (request()->has('json'))
