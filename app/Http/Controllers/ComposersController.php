e8fbb387793d7881bb8b12c184084810305e9b13
Arthur Villar <=>
2020-11-13 10:59:28 -0500

    Create birthdays page

--- app/Http/Controllers/ComposersController.php
+++ app/Http/Controllers/ComposersController.php
@@ -12,6 +12,9 @@ class ComposersController extends Controller
         $months = ['january', 'february', 'march', 'april', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
         $calendar = Composer::all()->where('is_famous', true)->sortBy('month_of_birth')->groupBy('month_of_birth');
 
+        if (request()->has('json'))
+            return $calendar;
+
         return view('composers.birthdays.index', compact(['calendar', 'months']));
     }
 }
