5702d474381de97ba39330f9523a8036856cb8d0
Arthur Villar <=>
2020-11-23 22:45:10 -0500

    Optimize website according to google

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -83,7 +83,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapWebRoutes()
     {
-        Route::middleware(['web', 'log.web', 'cache.headers:private;max_age=3600'])
+        Route::middleware(['web', 'log.web'])
              ->namespace($this->namespace)
              ->group(function() {
                 $this->getFolder('routes/web');
--- resources/views/layouts/html/google/fonts.blade.php
+++ resources/views/layouts/html/google/fonts.blade.php
@@ -1,2 +1,2 @@
-<link rel="dns-prefetch" href="//fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css?family=Poppins:400,600,800" rel="stylesheet">
+<link rel="preload" as="font" href="//fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css?family=Poppins:400,600,800&display=swap" rel="stylesheet">
