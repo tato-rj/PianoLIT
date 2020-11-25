8f58675d24d7560de4545ce883f50082cc4c2369
Arthur Villar <=>
2020-11-25 14:34:38 -0500

    Generate better keywords for escore pages

--- resources/views/shop/products/escores.blade.php
+++ resources/views/shop/products/escores.blade.php
@@ -1,7 +1,7 @@
 @extends('shop.products.index', [
 	'title' => 'eScores', 
 	'subtitle' => 'A curated collection of unique piano pieces',
-	'keywords' => $product->keywords(),
+	'keywords' => 'music score', 'piano sheet music',
 	'thumbnail' => asset('images/misc/thumbnails/infographs.jpg'),
     'ads' => ['ebook']
 ])
