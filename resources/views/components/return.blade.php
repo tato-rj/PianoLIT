dbde2ad9bd339a0e2d8cb9a18c9420705bd8a55c
Arthur Villar <=>
2020-12-09 00:47:29 -0500

    Remove blue from back link

--- resources/views/components/return.blade.php
+++ resources/views/components/return.blade.php
@@ -1,5 +1,1 @@
-<p>
-	<a href="{{$url}}">
-		<i class="fas fa-long-arrow-alt-left mr-1 align-middle"></i>back to the {{$to}}
-	</a>
-</p>
+<p><a href="{{$url}}"><i class="fas fa-long-arrow-alt-left mr-1 align-middle"></i>back to the {{$to}}</a></p>
