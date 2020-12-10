17cf9eedfc44f121d84808eb976173d94afed69c
Arthur Villar <=>
2020-12-10 12:00:08 -0500

    Update admin layout

--- resources/views/admin/layouts/header/divider.blade.php
+++ resources/views/admin/layouts/header/divider.blade.php
@@ -1,2 +1,2 @@
 {{-- <div class="dropdown-divider"></div> --}}
-<label class="mb-0 mt-2 text-muted" style="padding-left: 1em"><small><strong>{{$label}}</strong></small></label>
+<label class="mb-0 mt-2 text-muted" style="padding-left: .8rem"><small><strong>{{$label}}</strong></small></label>
