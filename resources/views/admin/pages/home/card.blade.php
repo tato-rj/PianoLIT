87b4d5842baf6754082f5ca94e2dc1dbb1875130
Arthur Villar <=>
2020-12-11 14:20:22 -0500

    Update admin layout

--- resources/views/admin/pages/home/card.blade.php
+++ resources/views/admin/pages/home/card.blade.php
@@ -1,5 +1,5 @@
 <div class="col-lg-3 col-md-4 col-12 p-2">
-  <div class="rounded shadow-light d-flex">
+  <div class="rounded border d-flex">
     <div class="flex-grow p-3">
       <div class="pb-2 border-bottom">
           <strong>{{number_format($stat['counts'][1])}}</strong> {{$stat['label']}}
