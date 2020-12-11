97dee391b78407258df3e508e5aa85bebb953149
Arthur Villar <=>
2020-12-11 11:47:13 -0500

    Update admin layout

--- resources/views/admin/components/page/title.blade.php
+++ resources/views/admin/components/page/title.blade.php
@@ -1,12 +1,9 @@
-<div class="row mb-4">
-	<div class="col-lg-10 col-md-8 col-6 d-flex align-items-center">
-		<div class="bg-white rounded d-flex flex-center border mr-4" style="width: 60px; height: 60px">
-			<div>@fa(['icon' => $icon, 'mr' => 0, 'color' => 'grey', 'size' => 'lg'])</div>
-		</div>
-		<div class="">
-			<h5 class="m-0">{{$title}}</h5>
-			<div class="text-muted">{{$subtitle}}</div>
-		</div>
+<div class="d-flex align-items-center mb-4">
+	<div class="bg-white rounded d-flex flex-center border mr-4" style="width: 60px; height: 60px">
+		<div>@fa(['icon' => $icon, 'mr' => 0, 'color' => 'grey', 'size' => 'lg'])</div>
 	</div>
-	<div class="col-lg-2 col-md-4 col-6"></div>
+	<div class="">
+		<h5 class="m-0">{{$title}}</h5>
+		<div class="text-muted">{{$subtitle}}</div>
+	</div>
 </div>
