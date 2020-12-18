8ad16f57109bf1028ed05c05b39558bc72e31335
Arthur Villar <=>
2020-12-18 00:03:45 -0500

    Complete reviews api

--- resources/views/shop/components/reviews/layout.blade.php
+++ resources/views/shop/components/reviews/layout.blade.php
@@ -1,5 +1,7 @@
+@env('local')
 @forelse($product->reviews()->published()->get() as $review)
 	@include('shop.components.reviews.review')
 @empty
 	<div>Be the first to review this product!</div>
 @endforelse
+@endenv
