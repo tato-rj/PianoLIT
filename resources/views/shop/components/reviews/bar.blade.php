934a5c1be7d59510a51d1c25efd8c75366837ed9
Arthur Villar <=>
2020-12-20 16:04:29 -0500

    Create review feature for products on the website

--- resources/views/shop/components/reviews/bar.blade.php
+++ resources/views/shop/components/reviews/bar.blade.php
@@ -1,8 +1,8 @@
 @php($count = $product->publishedReviews()->byRating($i)->count())
 <div class="d-flex mb-2">
-	<div class="lead text-uppercase text-nowrap pr-2" style="font-size: 88%; width: 52px">{{$i}} {{str_plural('star', $i)}}</div>
-	<div class="flex-grow bg-light position-relative">
+	<div class="lead text-uppercase text-nowrap" style="font-size: 88%; width: 52px">{{$i}} {{str_plural('star', $i)}}</div>
+	<div class="flex-grow bg-light mx-3 position-relative">
 		<div class="bg-primary h-100 position-absolute" style="width: {{percentage($count, $product->publishedReviews()->count())}}%; left: 0; top: 0"></div>
 	</div>
-	<div style="font-size: 88%" class="pl-2">{{$count}}</div>
+	<div style="font-size: 88%">{{$count}}</div>
 </div>
