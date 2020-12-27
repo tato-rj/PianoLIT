653ca3efed28fa3d3c452b97cca5ed97ac22d2d8
Arthur Villar <=>
2020-12-27 16:32:03 -0500

    Improve find your match api

--- app/Resources/FindYourMatch/Search.php
+++ app/Resources/FindYourMatch/Search.php
@@ -39,7 +39,7 @@ class Search
 		if (! $query->exists())
 			return $this->tryAgain();
 
-		return $query->inRandomOrder()->first();
+		return $query->inRandomOrder()->count();
 	}
 
 	public function tryAgain()
