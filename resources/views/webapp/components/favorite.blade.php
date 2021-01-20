33ca5ab2e3d111d6c86cb09b29bcfabccb9bfbaf
Arthur Villar <=>
2021-01-19 22:59:23 -0500

    Fix favorite missing heart

--- resources/views/webapp/components/favorite.blade.php
+++ resources/views/webapp/components/favorite.blade.php
@@ -1,5 +1,4 @@
-{{-- @php($is_favorited = $piece->isFavorited(auth()->user()->id)) --}}
-@php($is_favorited = $piece->is_favorited)
+@php($is_favorited = $piece->isFavorited(auth()->user()->id))
 @env('local')
 <button class="btn-raw t-2" id="flag-{{$piece->id}}" data-dismiss="fixed-panel" data-manage="save-to" data-url="{{route('webapp.pieces.save-to', $piece)}}" style="font-size: 120%">
 	@fa(['icon' => 'heart', 'fa_type' => $is_favorited ? 's' : 'r', 'color' => 'red'])
