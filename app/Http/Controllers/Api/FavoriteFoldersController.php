dcd092eb36f907df9fc4fa84305e53242a870154
Arthur Villar <=>
2021-01-27 23:33:28 -0500

    Update response to updated folder api

--- app/Http/Controllers/Api/FavoriteFoldersController.php
+++ app/Http/Controllers/Api/FavoriteFoldersController.php
@@ -44,8 +44,24 @@ class FavoriteFoldersController extends Controller
         return FavoriteFolder::flat($request->user_id, $request->folder_id);
     }
 
-    public function store(Request $request, FavoriteFoldersForm $form)
+    public function store(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required|exists:users,id',
+            'piece_id' => 'sometimes|exists:pieces,id',
+            'name' => [
+                'required', 
+                'string',
+                'min:3',
+                Rule::unique('favorite_folders')->where(function ($query) use ($request) {
+                    return $query->where(['user_id' => $request->user_id, 'name' => $request->name]);
+                })
+            ]
+        ]);
+
+        if ($validator->fails())
+            return response()->json(['message' => $validator->messages()->first()]);
+
         $folder = FavoriteFolder::create([
             'user_id' => $form->user_id,
             'name' => $form->name
