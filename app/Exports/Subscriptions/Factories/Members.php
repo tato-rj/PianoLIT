1b9563abfef0d4f7e0231756a992c643c76cd3c7
Arthur Villar <=>
2021-02-07 14:42:50 -0500

    Improve email exports for admins

--- app/Exports/Subscriptions/Factories/Members.php
+++ app/Exports/Subscriptions/Factories/Members.php
@@ -10,6 +10,6 @@ class Members implements ExportFactory
 
 	public function generate()
 	{
-		return $this->data = User::has('membership')->get()->pluck('emails')->toArray();
+		return $this->data = User::has('membership')->get()->pluck('email')->toArray();
 	}
 }
