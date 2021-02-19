98f4b0797f9454fa1da26f5ddae1c7f6c63d3ce3
Arthur Villar <=>
2021-02-19 10:39:06 -0500

    Fix missing subject on emails

--- tests/Feature/EmailListTest.php
+++ tests/Feature/EmailListTest.php
@@ -4,7 +4,7 @@ namespace Tests\Feature;
 
 use Tests\AppTest;
 use App\{EmailList, Subscription, Piece};
-use App\Mail\FreePickEmail;
+use App\Mail\{FreePickEmail, NewsletterEmail};
 use App\Notifications\Emails\EmailListSentNotification;
 
 class EmailListTest extends AppTest
@@ -20,10 +20,16 @@ class EmailListTest extends AppTest
         
         $this->subscriber1 = create(Subscription::class);
         $this->subscriber2 = create(Subscription::class);
+        $this->subscriber3 = create(Subscription::class);
+        $this->subscriber4 = create(Subscription::class);
 
     	$this->freePickList = create(EmailList::class, ['name' => 'Free Pick']);
     	$this->freePickList->subscribers()->attach($this->subscriber1);
     	$this->freePickList->subscribers()->attach($this->subscriber2);
+
+        $this->newsletterList = create(EmailList::class, ['name' => 'Newsletter']);
+        $this->newsletterList->subscribers()->attach($this->subscriber3);
+        $this->newsletterList->subscribers()->attach($this->subscriber4);
     }
 
     /** @test */
@@ -62,6 +68,26 @@ class EmailListTest extends AppTest
     	$this->get(route('admin.subscriptions.lists.send-to', ['list' => $this->freePickList, 'email' => 'test@email.com']));
 
         \Mail::assertQueued(FreePickEmail::class, 1);
+    }
+
+    /** @test */
+    public function admins_can_send_out_a_newsletter_with_a_custom_subject_line()
+    {
+        $this->signIn();
+
+        \Mail::fake();
+
+        $this->get(route('admin.subscriptions.lists.send-to', ['list' => $this->newsletterList, 'email' => 'test@email.com', 'subject' => 'foo']));
+         
+        \Mail::assertQueued(NewsletterEmail::class, function($mail) {
+            return $mail->subject == 'foo';
+        });
+
+        $this->get(route('admin.subscriptions.lists.send', ['list' => $this->newsletterList, 'subject' => 'bar']));
+         
+        \Mail::assertQueued(NewsletterEmail::class, function($mail) {
+            return $mail->subject == 'bar';
+        });
     }
 
     /** @test */
