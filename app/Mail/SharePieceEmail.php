bd58abc6076d4b75c53746dd1c177d82fc6aaec2
Arthur Villar <arthur@iMac.local>
2021-08-05 01:25:55 -0400

    Add share functionality to pieces

--- app/Mail/SharePieceEmail.php
+++ app/Mail/SharePieceEmail.php
@@ -8,7 +8,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use App\{Piece, User};
 
-class SharePieceEmail extends Mailable
+class SharePieceEmail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
