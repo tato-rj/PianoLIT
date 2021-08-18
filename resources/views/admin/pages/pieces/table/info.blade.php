54d2ab919384b475d47c0fa88f07c71c82035458
Arthur Villar <arthur@iMac.local>
2021-08-17 23:06:07 -0400

    Show if a piece has already been chosen as a freepick

--- resources/views/admin/pages/pieces/table/info.blade.php
+++ resources/views/admin/pages/pieces/table/info.blade.php
@@ -9,9 +9,9 @@
       </div>
     </div>
     <div class="d-flex hide-on-sm">
-      <span class="{{$item->hasDescription() ? 'text-primary' : 'text-muted'}}" title="{{$item->description}}"><i class="fas fa-info-circle mr-1"></i></span>
+      <span class="{{$item->hasDescription() ? 'text-primary' : 'text-muted'}} mr-1" title="{{$item->description}}"><i class="fas fa-info-circle"></i></span>
       @include('admin.components.play', ['audio' => storage($item->audio_path)])
-      <span class="text-nowrap mr-1 {{$item->tutorials_count > 0 ? 'text-primary' : 'text-muted'}}"><i class="fab fa-youtube mr-1"></i></span>
+      <span class="text-nowrap mx-1 {{$item->tutorials_count > 0 ? 'text-primary' : 'text-muted'}}"><i class="fab fa-youtube"></i></span>
       <span class="{{$item->hasTutorials(['synthesia']) ? 'text-danger' : 'text-muted'}}"><i class="fas fa-fire"></i></span>
     </div>
   </div>
