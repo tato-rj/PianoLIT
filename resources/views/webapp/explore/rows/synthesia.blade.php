6bb736f8d140c871bda6debc01eee4f24a35a391
Arthur Villar <arthur@iMac.local>
2021-08-18 10:16:47 -0400

    Add synthesia to explore api on the webapp only

--- resources/views/webapp/explore/rows/synthesia.blade.php
+++ resources/views/webapp/explore/rows/synthesia.blade.php
@@ -7,7 +7,7 @@
 			@auth
 			@fa(['icon' => 'lock', 'classes' => 'absolute-center opacity-6', 'size' => '2x', 'color' => 'white', 'if' => $isAuthorized])
 			@endauth
-			<div class="border cursor-pointer rounded d-flex d-apart flex-column p-3 piece-card" role="img" aria-label="{{$tutorial->piece->name}}" data-url="{{route('webapp.pieces.show', $tutorial->piece)}}#tutorial">
+			<div class="border cursor-pointer rounded d-flex d-apart flex-column p-3 piece-card" role="img" aria-label="{{$tutorial->piece->name}}" data-url="{{route('webapp.pieces.show', $tutorial->piece)}}#tutorial" style="width: 220px">
 				<div class="mb-2 d-flex align-items-center">
 					@fa(['icon' => 'circle', 'classes' => 'color-'.lastword($tutorial->piece->level->name)])
 					<div class="text-dark clamp-1"><strong>{{$tutorial->piece->medium_name}}</strong></div>
