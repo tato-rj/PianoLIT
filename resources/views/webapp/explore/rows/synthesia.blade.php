0ed3f5cda39ae6180366efc9d7de2d1fb42330c8
Arthur Villar <arthur@iMac.local>
2021-08-18 15:03:38 -0400

    Add thumbnails to synthesia cards on explore tab

--- resources/views/webapp/explore/rows/synthesia.blade.php
+++ resources/views/webapp/explore/rows/synthesia.blade.php
@@ -5,18 +5,16 @@
 	@foreach($row['collection'] as $tutorial)
 		<div class="m-2 position-relative">
 			@auth
-			@fa(['icon' => 'lock', 'classes' => 'absolute-center opacity-6', 'size' => '2x', 'color' => 'white', 'if' => $isAuthorized])
+			@fa(['icon' => 'lock', 'classes' => 'absolute-center opacity-6', 'size' => '3x', 'color' => 'white', 'if' => $isAuthorized])
 			@endauth
-			<div class="border cursor-pointer rounded d-flex d-apart flex-column p-3 piece-card" role="img" aria-label="{{$tutorial->piece->name}}" data-url="{{route('webapp.pieces.show', $tutorial->piece)}}#tutorial" style="width: 220px">
-				<div class="mb-2 d-flex align-items-center">
+			<div class="border cursor-pointer rounded d-flex d-apart flex-column piece-card" role="img" aria-label="{{$tutorial->piece->name}}" data-url="{{route('webapp.pieces.show', $tutorial->piece)}}#tutorial" style="width: 240px">
+				<div class="d-flex align-items-center px-3 py-2">
 					@fa(['icon' => 'circle', 'classes' => 'color-'.lastword($tutorial->piece->level->name)])
 					<div class="text-dark clamp-1"><strong>{{$tutorial->piece->medium_name}}</strong></div>
 				</div>
 
-				<div class="w-100 text-center">
-					<img src="{{asset('images/webapp/synthesia-missing.svg')}}" class="mx-auto mb-2" style="width: 102px; opacity:  .1">
-				</div>
-				<div class="clamp-1">by {{$tutorial->piece->composer->short_name}}</div>
+				<img src="{{asset('images/webapp/synthesia-thumbnails/thumb-'.$loop->index.'.jpg')}}" class="w-100">
+				<div class="clamp-1 px-2 py-1"><small>{{$tutorial->piece->composer->name}}</small></div>
 			</div>
 		</div>
 	@endforeach
