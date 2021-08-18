3aa8fbbe99365645343a0364e4ab91e0a9df7401
Arthur Villar <arthur@iMac.local>
2021-08-17 22:48:04 -0400

    Show if a piece has already been chosen as a freepick

--- resources/views/admin/pages/pieces/table/actions.blade.php
+++ resources/views/admin/pages/pieces/table/actions.blade.php
@@ -5,17 +5,17 @@
 	]])
 	@if($item->is_free)
 	<div>
-		<button class="border-0 p-0 bg-transparent text-success mr-2 align-middle" disabled><i class="fas fa-award"></i></button>
+		<button class="border-0 p-0 bg-transparent text-alert mr-2 align-middle" disabled>@fa(['icon' => 'award', 'mr' => 0])</button>
 	</div>
 	@elseif($item->hasImage() && ! $item->highlighted_at)
 	<form method="POST" action="{{route('admin.pieces.highlight', $item->id)}}">
 		@csrf
 		@method('PATCH')
-		<button type="submit" class="border-0 p-0 bg-transparent text-grey mr-2 align-middle" title="Highlight this piece"><i class="fas fa-award"></i></button>
+		<button type="submit" class="border-0 p-0 bg-transparent text-grey mr-2 align-middle" title="Highlight this piece">@fa(['icon' => 'award', 'mr' => 0])</button>
 	</form>
 	@elseif($item->highlighted_at)
 	<div>
-		<button class="border-0 p-0 bg-transparent text-success mr-2 align-middle" disabled><i class="fas fa-check-circle"></i></button>
+		<button class="border-0 p-0 bg-transparent text-success mr-2 align-middle" disabled>@fa(['icon' => 'check-circle', 'mr' => 0])</button>
 	</div>
 	@endif
 
