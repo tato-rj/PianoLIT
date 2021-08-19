1b4e39c3d086a9a6b7dce2467a97218300d9a758
Arthur Villar <arthur@iMac.local>
2021-08-18 23:03:14 -0400

    Update synthesia card

--- resources/views/webapp/explore/rows/synthesia.blade.php
+++ resources/views/webapp/explore/rows/synthesia.blade.php
@@ -10,11 +10,13 @@
 			<div class="border cursor-pointer rounded d-flex d-apart flex-column piece-card" role="img" aria-label="{{$tutorial->piece->name}}" data-url="{{route('webapp.pieces.show', $tutorial->piece)}}#tutorial" style="width: 240px">
 				<div class="d-flex align-items-center px-3 py-2">
 					@fa(['icon' => 'circle', 'classes' => 'color-'.lastword($tutorial->piece->level->name)])
-					<div class="text-dark clamp-1"><strong>{{$tutorial->piece->medium_name}}</strong></div>
+					<div>
+						<div class="text-dark clamp-1 lh-1" style="margin-bottom: 2px"><strong>{{$tutorial->piece->medium_name}}</strong></div>
+						<div class="clamp-1 lh-1"><small>{{$tutorial->piece->composer->short_name}}</small></div>
+					</div>
 				</div>
 
-				<img src="{{$tutorial->thumbnail}}" class="w-100">
-				<div class="clamp-1 px-2 py-1"><small>{{$tutorial->piece->composer->name}}</small></div>
+				<img src="{{$tutorial->thumbnail}}" class="w-100 rounded-bottom">
 			</div>
 		</div>
 	@endforeach
