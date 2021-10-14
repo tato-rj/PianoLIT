0bb95b591c684f283cd8604d5331b8ece2f3f076
Arthur Villar <arthur@iMac.local>
2021-10-13 22:58:48 -0400

    Indicate if piece has synthesia

--- resources/views/webapp/components/piece/icons.blade.php
+++ resources/views/webapp/components/piece/icons.blade.php
@@ -1,4 +1,5 @@
-@fa(['icon' => 'headphones', 'if' => $piece->hasAudio()])
-@fa(['icon' => 'video', 'if' => $piece->tutorials()->exists()])
-@fa(['icon' => 'file-alt', 'if' => $piece->hasScore($publicDomain = true)])
+@fa(['icon' => 'headphones', 'if' => $piece->hasAudio(), 'title' => 'Audio'])
+@fa(['icon' => 'video', 'if' => $piece->tutorials()->exists(), 'title' => 'Video'])
+@fa(['icon' => 'fire', 'if' => $piece->hasTutorials(['synthesia']), 'title' => 'Synthesia'])
+@fa(['icon' => 'file-alt', 'if' => $piece->hasScore($publicDomain = true), 'title' => 'Score'])
 {{-- @fa(['icon' => 'eye', 'color' => 'muted']){{$piece->views_count}} {{ str_plural('view', $piece->views_count) }} --}}
