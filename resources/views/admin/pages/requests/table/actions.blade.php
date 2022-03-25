03b19a5b16b52fcb1d9e2bb765bdaeef7d43026d
Arthur Villar <arthur@iMac.local>
2022-03-25 10:34:20 -0400

    Allow admins to delete a tutorial request

--- resources/views/admin/pages/requests/table/actions.blade.php
+++ resources/views/admin/pages/requests/table/actions.blade.php
@@ -5,16 +5,18 @@
 	<div class="mr-2">
 		<button class="btn btn-sm btn-outline-secondary view-request-types" {{! $item->types ? 'disabled' : null}} data-types="{{json_encode(unserialize($item->types))}}">Details</button>
 	</div>
-	<div class="mr-2">
+	<div>
 	    @if($item->isPublished())
 	    <div class="text-success text-nowrap"><i class="fas fa-check-circle mr-1"></i>Published</div>
 	    @else
 	    <a href="#" data-url="{{route('admin.tutorial-requests.publish', $item->id)}}" data-toggle="modal" data-target="#publish-tutorial" class="btn btn-sm btn-warning text-nowrap"><i class="fas fa-hourglass-half mr-1"></i>Publish</a>
 	    @endif
 	</div>
-	<div>
+	@if(! $item->isPublished())
+	<div class="ml-2">
 		<a href="#" data-url="{{route('admin.tutorial-requests.destroy', $item)}}" title="Delete" data-toggle="modal" data-target="#delete-modal" class="delete text-muted d-none d-sm-block">
 			<i class="far fa-trash-alt align-middle"></i>
 		</a>
 	</div>
+	@endif
 </div>
