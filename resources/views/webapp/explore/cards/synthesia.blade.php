c638d8698965be6828521cda532c1c4ddf405296
Arthur Villar <arthur@iMac.local>
2022-04-27 13:01:26 -0400

    Add more like this row to piece view

--- resources/views/webapp/explore/cards/synthesia.blade.php
+++ resources/views/webapp/explore/cards/synthesia.blade.php
@@ -3,6 +3,7 @@
 			@fa(['icon' => 'lock', 'classes' => 'absolute-center opacity-6', 'size' => '3x', 'color' => 'white', 'if' => ! $isAuthorized])
 			@endauth
 			<div class="border cursor-pointer rounded d-flex d-apart flex-column piece-card" role="img" aria-label="{{$tutorial->piece->name}}" data-url="{{route('webapp.pieces.show', $tutorial->piece)}}#tutorial" style="width: 240px">
+				<img src="{{$tutorial->thumbnail}}" class="w-100 rounded-top">
 				<div class="d-flex align-items-center w-100 px-3 py-2">
 					@fa(['icon' => 'circle', 'classes' => 'color-'.lastword($tutorial->piece->level->name)])
 					<div>
@@ -10,7 +11,5 @@
 						<div class="clamp-1 lh-1"><small>{{$tutorial->piece->composer->short_name}}</small></div>
 					</div>
 				</div>
-
-				<img src="{{$tutorial->thumbnail}}" class="w-100 rounded-bottom">
 			</div>
 		</div>
