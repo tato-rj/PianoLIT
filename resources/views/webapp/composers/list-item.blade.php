0bb37b18811433d0e52e517275afd61687d92bca
Arthur Villar <arthur@iMac.local>
2022-06-06 15:24:39 -0400

    Create composers page

--- resources/views/webapp/composers/list-item.blade.php
+++ resources/views/webapp/composers/list-item.blade.php
@@ -1,10 +1,9 @@
-<div class="col-lg-3 col-md-4 col-6 text-center mb-3 composer-card">
+<div class="col-lg-3 col-md-4 col-6 text-center mb-2 composer-card">
 <a href="{{route('webapp.composers.show', $composer)}}" class="link-none px-2">
-	<div class="text-center mb-4">
+	<div class="text-center">
 		<img src="{{$composer->cover_image}}" style="width: 110px" class="rounded-circle shadow mb-3">
-		<h6 class="mb-0">{{$composer->name}}</h6>
+		<h6 class="mb-0 text-truncate">{{$composer->name}}</h6>
 		<div>
-			
 			<small class="text-muted">@flag(['code' => $composer->country->flag_code]){{$composer->country->name}}</small>
 		</div>
 	</div>
