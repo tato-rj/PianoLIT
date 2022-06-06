a979f977843803524b688affbde0a36455aef578
Arthur Villar <arthur@iMac.local>
2022-06-05 22:53:21 -0400

    Reduce items per row on discover page

--- resources/views/webapp/discover/composers/modal.blade.php
+++ resources/views/webapp/discover/composers/modal.blade.php
@@ -6,7 +6,7 @@ Composers
 @slot('body')
 	<div class="list-group list-group-flush">
 		@foreach($composers as $composer)
-			<a href="{{route('webapp.search.results', ['search' => $composer->name])}}" class="list-group-item list-group-item-action">{{$composer->reversed_name}} ({{$composer->pieces_count}})</a>
+			<a href="{{route('webapp.search.results', ['search' => $composer->name])}}" class="list-group-item list-group-item-action"><img src="{{$composer->cover_image}}" class="rounded-circle mr-2" style="width: 34px" alt="{{$composer->name}}"><span class="align-middle">{{$composer->reversed_name}} ({{$composer->pieces_count}})</span></a>
 		@endforeach
 	</div>
 @endslot
