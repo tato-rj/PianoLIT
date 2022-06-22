61c38754a81827f86e4cb866674f83689064f549
Arthur Villar <arthur@iMac.local>
2022-06-22 15:13:35 -0400

    Reskin freepick email

--- resources/views/vendor/mail/html/highlights/cover.blade.php
+++ resources/views/vendor/mail/html/highlights/cover.blade.php
@@ -4,7 +4,7 @@
             <table width="100%" cellpadding="0" cellspacing="0">
                 <tr>
                     <td class="panel-item">
-						<div style="width: 100%; min-height: 140px; border-radius: 15px 50px; background: url({{$piece->getBackground()}}); background-position: center; background-size: cover; position: relative;" class="mb-3">
+						<div style="position: relative; width: 100%; min-height: 140px; border-radius: 15px 50px; background: url({{$piece->getBackground()}}); background-position: center; background-size: cover; position: relative;" class="mb-3">
 							<div style="position: absolute; bottom: -36px; left: 10px;">
                                 <img src="{{$piece->composer->cover_image}}" width="102" style="border-radius: 60px; border: 4px solid white">
 							</div>
