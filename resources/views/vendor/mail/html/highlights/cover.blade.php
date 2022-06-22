37fa02847882f2a7ecfbd91389c33bd711ecae4d
Arthur Villar <arthur@iMac.local>
2022-06-22 15:37:49 -0400

    Reskin freepick email

--- resources/views/vendor/mail/html/highlights/cover.blade.php
+++ resources/views/vendor/mail/html/highlights/cover.blade.php
@@ -4,9 +4,9 @@
             <table width="100%" cellpadding="0" cellspacing="0">
                 <tr>
                     <td class="panel-item">
-						<div style="width: 100%; min-height: 140px; border-radius: 15px 50px; background: url({{$piece->getBackground()}}); background-position: center; background-size: cover; position: relative;" class="mb-3"></div>
+						<div style="width: 100%; min-height: 140px; background: url({{$piece->getBackground()}}); background-position: center; background-size: cover; position: relative;" class="mb-3"></div>
                         <div>
-                            <img src="{{$piece->composer->cover_image}}" width="98" style="border-radius: 60px; display: inline-block; margin-right: 12px; vertical-align: middle">
+                            <img src="{{$piece->composer->cover_image}}" width="78" style="border-radius: 60px; display: inline-block; margin-right: 12px; vertical-align: middle">
                             <div style="display: inline-block; vertical-align: middle; max-width: 70%;">
                                 <p class="m-0 text-sm">
                                 <span class="dot bg-{{$piece->level->name}}"></span> {{strtoupper($piece->extended_level_name)}}
