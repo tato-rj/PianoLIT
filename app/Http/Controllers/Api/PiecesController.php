a145d2406bf6248f671a041a1f3d46948a2b152d
Arthur Villar <arthur@iMac.local>
2023-05-02 22:45:16 -0400

    Quick update

--- app/Http/Controllers/Api/PiecesController.php
+++ app/Http/Controllers/Api/PiecesController.php
@@ -24,7 +24,7 @@ class PiecesController extends Controller
 
     public function freepicks(Request $request)
     {
-        return Piece::freePicks()->paginate(4);
+        return Piece::freePicks()->get();
     }
     
     public function timeline($pieceId)
