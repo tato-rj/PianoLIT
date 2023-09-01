31506785cd5493c53c8ed8b0a097e8f795b54524
Arthur Villar <arthur@iMac.local>
2023-09-01 10:44:24 -0400

    Quick update

--- routes/admin/users.php
+++ routes/admin/users.php
@@ -16,13 +16,13 @@ Route::prefix('users')->name('users.')->group(function() {
 
 	Route::prefix('performances')->name('performances.')->group(function() {
 
+		Route::post('test-connection', 'Admin\PerformancesController@testConnection')->name('test-connection');
+
 		Route::patch('{performance}', 'Admin\PerformancesController@update')->name('update');
 
 		Route::patch('{performance}/urls', 'Admin\PerformancesController@getUrls')->name('get-urls');
 		
 		Route::patch('{performance}/status', 'Admin\PerformancesController@approve')->name('approve');
-
-		Route::post('/test-connection', 'Admin\PerformancesController@testConnection')->name('test-connection');
 
 		Route::delete('{performance}', 'Admin\PerformancesController@destroy')->name('destroy');
 
