4b8c81871e48b08543cbadb66e33371b4cd79c30
Arthur Villar <arthur@iMac.local>
2023-09-05 14:41:14 -0400

    Quick update

--- resources/views/emails/performances/approved.blade.php
+++ resources/views/emails/performances/approved.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # Congratulations!
 
-Your performance of {{$piece->long_name_with_composer}} has been approved.
+Your performance of <strong>{{$piece->long_name_with_composer}}<strong> has been approved.
 
 Best<br>
 Elena from {{ config('app.name') }}
