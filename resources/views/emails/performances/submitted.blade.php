11309eb41bb25600c1ef2badd45bafe685ae3422
Arthur Villar <arthur@iMac.local>
2023-09-05 14:42:09 -0400

    Quick update

--- resources/views/emails/performances/submitted.blade.php
+++ resources/views/emails/performances/submitted.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # Hi there!
 
-Your performance of <strong>{{$piece->long_name_with_composer}}<strong> has been submitted, we will let you know when it goes live.
+Your performance of <strong>{{$piece->long_name_with_composer}}</strong> has been submitted, we will let you know when it goes live.
 
 Best<br>
 Elena from {{ config('app.name') }}
