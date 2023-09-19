e9b7124d89246e211dc31e7903867fa059bcc653
Arthur Villar <arthur@iMac.local>
2023-09-19 17:57:29 -0400

    Quick update

--- resources/views/webapp/piece/performances/overlay.blade.php
+++ resources/views/webapp/piece/performances/overlay.blade.php
@@ -1,6 +1,6 @@
 <div style="display: none" id="upload-overlay" class="screen-lock-overlay">
     <div class="h-100 text-white d-flex text-center justify-content-center align-items-center">
-        <div>
+        <div class="px-4">
             <div class="progress mb-3 animated shadow" style="height: 25px">
                 <div class="progress-bar progress-bar-striped progress-bar-animated fw-bold" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 0%; height: 100%"></div>
             </div>
