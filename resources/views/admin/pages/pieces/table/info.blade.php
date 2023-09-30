1857e820c5724a866a901fdd5484dacc29839416
Arthur Villar <arthur@iMac.local>
2023-09-30 01:08:15 -0400

    Quick update

--- resources/views/admin/pages/pieces/table/info.blade.php
+++ resources/views/admin/pages/pieces/table/info.blade.php
@@ -2,8 +2,10 @@
     <div class="dropdown d-inline-block align-text-bottom cursor-pointer mr-1">
       <i class="fas fa-ellipsis-v dropdown-toggle" data-toggle="dropdown"></i>
       <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        @if($item->hasAudio())
         <a href="{{route('admin.pieces.qr-code', $item)}}" target="_blank" class="dropdown-item">QR Code</a>
         <a href="{{route('clips.piece', $item)}}" target="_blank" class="dropdown-item">Clip</a>
+        @endif
         <a href="{{$item->timeline_url}}" target="_blank" class="dropdown-item">Timeline</a>
         <a href="{{route('api.pieces.collection', $item->id)}}" target="_blank" class="dropdown-item">Collection</a>
         <a href="{{route('api.pieces.similar', $item->id)}}" target="_blank" class="dropdown-item">More like this</a>
