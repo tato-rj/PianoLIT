b7625174d49f7f9b49ede59e17e4b6005cce4a77
Arthur Villar <arthur@iMac.local>
2023-10-15 15:36:55 -0400

    Quick update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('pianolit:admin-report')->everySixHours();
-        // $schedule->command('pianolit:admin-report')->weeklyOn(7, '22:30');
+        $schedule->command('pianolit:admin-report')->weeklyOn(7, '22:30');
 
         $schedule->command('subscriptions:remove-spam')->weeklyOn(7, '10:30');
 
