8166ef178c61f494bd036566246499abb44207f6
Arthur Villar <arthur@iMac.local>
2023-10-21 19:12:08 -0400

    Quick update

--- resources/views/webapp/membership/pricing/plans.blade.php
+++ resources/views/webapp/membership/pricing/plans.blade.php
@@ -1,6 +1,15 @@
-<section class="mb-5">
+<section class="mb-3">
 	<div class="row">
 		@each('webapp.membership.pricing.components.plan', $plans, 'plan')
 	</div>
 	<div class="text-right text-muted"><small>All prices are listed in US Dollars</small></div>
+</section>
+
+<section class="">
+	<div class="row p-2">
+		<div class="col-12 p-5 rounded border mb-5 text-center">
+			<h3 class="mb-4">@fa(['icon' => 'graduation-cap'])Institutional pricing</h3>
+			<p class="m-0">Looking to get PianoLIT for your entire studio, department, or school? Send an email to <a target="_blank" href="mailto:contact@pianolit.com?subject=Institutional pricing inquiry" class="link-primary">contact@pianolit.com</a> and we'll create a plan perfectly suited for your needs.</p>
+		</div>
+	</div>
 </section>
