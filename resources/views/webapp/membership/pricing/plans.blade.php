01e259dcadad6097977003ba9614394e58813fc7
Arthur Villar <arthur@iMac.local>
2023-10-21 19:12:49 -0400

    Quick update

--- resources/views/webapp/membership/pricing/plans.blade.php
+++ resources/views/webapp/membership/pricing/plans.blade.php
@@ -7,7 +7,7 @@
 
 <section class="">
 	<div class="row p-2">
-		<div class="col-12 p-5 rounded border mb-5 text-center">
+		<div class="col-lg-6 col-md-6 col-11 mx-auto p-5 rounded border mb-5 text-center">
 			<h3 class="mb-4">@fa(['icon' => 'graduation-cap'])Institutional pricing</h3>
 			<p class="m-0">Looking to get PianoLIT for your entire studio, department, or school? Send an email to <a target="_blank" href="mailto:contact@pianolit.com?subject=Institutional pricing inquiry" class="link-primary">contact@pianolit.com</a> and we'll create a plan perfectly suited for your needs.</p>
 		</div>
