ed1a01a70983a99c48790c626d968a62156864a2
Arthur Villar <arthur@iMac.local>
2023-10-23 11:38:11 -0400

    Quick update

--- resources/views/webapp/user/my-pieces/index.blade.php
+++ resources/views/webapp/user/my-pieces/index.blade.php
@@ -44,15 +44,15 @@
 @component('webapp.layouts.header', ['title' => 'My Pieces', 'subtitle' => 'Quickly access your favorites or see your tutorial requests'])
 <div class="list-group flex-row rounded-pill position-relative">
 	<button class="btn btn-wide rounded-pill list-group-item list-group-item-action active" data-anchor="favorites" data-toggle="list" href="#list-favorites">FAVORITES</button>
-	<button class="btn btn-wide rounded-pill list-group-item list-group-item-action" data-anchor="tutorials" data-toggle="list" href="#list-tutorials">TUTORIALS</button>
+	<button class="btn btn-wide rounded-pill list-group-item list-group-item-action" data-anchor="uploads" data-toggle="list" href="#list-uploads">UPLOADS</button>
 </div>
 @endcomponent
     <div class="tab-content" id="nav-tabContent">
       <div class="tab-pane fade show active" id="list-favorites" role="tabpanel" aria-labelledby="list-profile-list">
 		@include('webapp.user.my-pieces.favorites.index')
       </div>
-      <div class="tab-pane fade" id="list-tutorials" role="tabpanel" aria-labelledby="list-home-list">
-		@include('webapp.user.my-pieces.tutorial-requests')
+      <div class="tab-pane fade" id="list-uploads" role="tabpanel" aria-labelledby="list-home-list">
+		@include('webapp.user.my-pieces.uploads')
       </div>
     </div>
 @endsection
