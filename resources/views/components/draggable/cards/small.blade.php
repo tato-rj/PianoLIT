404ee0783329933c479046873272544d84c839e7
Arthur Villar <arthur@iMac.local>
2023-10-27 13:38:22 -0400

    Quick update

--- resources/views/components/draggable/cards/small.blade.php
+++ resources/views/components/draggable/cards/small.blade.php
@@ -1,8 +1,8 @@
 <div class="edit-control border d-flex align-items-center selected-piece ordered t-2 rounded bg-white hover-shadow-light mb-2" data-id="{{$model->id}}" style="padding: .375rem 0; user-select: none;">
   <div class="ml-2 text-muted opacity-4">{{! empty($loop) ? $loop->iteration : null}}</div>
-  <div class="sort-handle flex-grow d-flex text-truncate">
+  <div class="flex-grow d-flex text-truncate">
     {{-- SORT HANDLE --}}
-    <div class="px-2 mr-1">
+    <div class="px-2 mr-1 sort-handle">
       <i class="fas fa-sort"></i>
     </div>
     <div class="d-flex align-items-center text-truncate">
