07b298ff619c1c44caf6f5944ff31b37c141ef5d
Arthur Villar <arthur@iMac.local>
2023-11-02 21:53:27 -0400

    Quick update

--- resources/views/admin/pages/pieces/table/name.blade.php
+++ resources/views/admin/pages/pieces/table/name.blade.php
@@ -1,4 +1,7 @@
-<a href="{{route('admin.pieces.test-escore', $item)}}" class="link-blue" target="_blank">@fa(['icon' => 'file'])</a>{{$item->long_name}}
+@if($item->hasScore())
+<a href="{{route('admin.pieces.test-escore', $item)}}" class="link-blue" target="_blank">@fa(['icon' => 'file'])</a>
+@endif
+{{$item->long_name}}
 @if(! $item->hasAudio())
 <a href="{{youtube($item->long_name . ' by ' . $item->composer->name)}}" target="_blank" class="link-blue"><i class="fas fa-external-link-alt ml-1 fa-xs"></i></a>
 @endif
