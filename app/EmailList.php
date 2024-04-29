20b00d4df506ffe34a6dc2b468edac99a8eaeb32
Arthur Villar <arthur@iMac.local>
2024-04-29 11:27:08 -0400

    Quick update

--- app/EmailList.php
+++ app/EmailList.php
@@ -30,7 +30,7 @@ class EmailList extends PianoLit
 
         // ->where('id', '>', 5191)
         
-        $this->subscribers()->where('id', '>', 5191)->chunk(500, function($subscribers) use ($list_id) {
+        $this->subscribers()->chunk(500, function($subscribers) use ($list_id) {
             foreach ($subscribers as $subscriber) {
                 \Mail::to($subscriber->email)->queue($this->mailable($list_id, $subscriber));
             }
--- resources/views/admin/pages/reports/show/table/status.blade.php
+++ resources/views/admin/pages/reports/show/table/status.blade.php
@@ -1,7 +1,10 @@
-@if($item->delivered_at)
-<span class="text-success"><i class="fas fa-check-circle mr-1"></i>Delivered</span>
-@elseif($item->failed_at)
-<span class="text-danger"><i class="fas fa-times-circle mr-1"></i>Failed</span>
-@else
-<span class="text-muted">Unknown</span>
-@endif
+<div class="d-flex">
+	@if($item->delivered_at)
+	<span class="text-success"><i class="fas fa-check-circle mr-1"></i>Delivered</span>
+	@elseif($item->failed_at)
+	<span class="text-danger"><i class="fas fa-times-circle mr-1"></i>Failed</span>
+	@else
+	<span class="text-muted">Unknown</span>
+	@endif
+	<span class="ml-2">#{{$item->id}}</span>
+</div>
