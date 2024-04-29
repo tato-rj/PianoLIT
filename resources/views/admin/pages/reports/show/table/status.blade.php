3e9e9591f82b55afab85f6b3b555bc9e4c812a84
Arthur Villar <arthur@iMac.local>
2024-04-29 11:28:51 -0400

    Quick update

--- resources/views/admin/pages/reports/show/table/status.blade.php
+++ resources/views/admin/pages/reports/show/table/status.blade.php
@@ -1,10 +1,7 @@
-<div class="d-flex">
-	@if($item->delivered_at)
-	<span class="text-success"><i class="fas fa-check-circle mr-1"></i>Delivered</span>
-	@elseif($item->failed_at)
-	<span class="text-danger"><i class="fas fa-times-circle mr-1"></i>Failed</span>
-	@else
-	<span class="text-muted">Unknown</span>
-	@endif
-	<span class="ml-2">#{{$item->id}}</span>
-</div>
+@if($item->delivered_at)
+<span class="text-success"><i class="fas fa-check-circle mr-1"></i>Delivered</span>
+@elseif($item->failed_at)
+<span class="text-danger"><i class="fas fa-times-circle mr-1"></i>Failed</span>
+@else
+<span class="text-muted">Unknown</span>
+@endif
