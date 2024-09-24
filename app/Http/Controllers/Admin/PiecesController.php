77c276ab02b9d6d23f00d208c2d870fad9f22050
Arthur Villar <arthur@iMac.local>
2024-09-24 13:26:10 -0400

    Quick update

--- app/Http/Controllers/Admin/PiecesController.php
+++ app/Http/Controllers/Admin/PiecesController.php
@@ -167,7 +167,7 @@ class PiecesController extends Controller
      */
     public function edit(Piece $piece)
     {
-        return $piece;
+        // return $piece;
         $composers = Composer::orderBy('name')->get();
 
         $types = Tag::byTypes($except = ['level', 'period', 'length']);
