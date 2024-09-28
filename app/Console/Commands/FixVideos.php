1fae97d6ebd1bb3913fe24a87fdb27dfd7a7dd37
Arthur Villar <arthur@iMac.local>
2024-09-28 17:04:16 -0400

    Quick update

--- app/Console/Commands/FixVideos.php
+++ app/Console/Commands/FixVideos.php
@@ -39,12 +39,13 @@ class FixVideos extends Command
      */
     public function handle()
     {
+        //'https://leftlaneapps.com/storage/videos/performance/'.$tutorial->piece_id.'.mp4'
         // https://leftlaneapps.com/storage/dmitry-kabalevsky/op27-no1-01.mp4
         $tutorial = Tutorial::latest()->first();
 
-        // \DB::table('tutorials')
-        //     ->where('id', $tutorial->id)
-        //     ->update(['video_url' => 'https://leftlaneapps.com/storage/videos/performance/'.$tutorial->piece_id.'.mp4']);
+        \DB::table('tutorials')
+            ->where('id', $tutorial->id)
+            ->update(['video_url' => 'https://leftlaneapps.com/storage/dmitry-kabalevsky/op27-no1-01.mp4']);
 
         $this->info('Checking ' . $tutorial->video_url . '...');
 
