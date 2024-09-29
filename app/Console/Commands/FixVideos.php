055ef424ab2f88aaaed97c27618751f8e4a6478c
Arthur Villar <arthur@iMac.local>
2024-09-28 23:20:48 -0400

    Quick update

--- app/Console/Commands/FixVideos.php
+++ app/Console/Commands/FixVideos.php
@@ -40,7 +40,7 @@ class FixVideos extends Command
     public function handle()
     {
         $tutorial = Tutorial::whereIn('type', ['synthesia', 'performance'])->get();
-dd($tutorial->count());
+dd($tutorial->first()->id);
         $url = 'https://leftlaneapps.com/videouploader/fix';
 
         try {
