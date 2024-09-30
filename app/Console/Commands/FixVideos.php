c3b7468433f284aab4239f995cec5595bf96e5ff
Arthur Villar <arthur@iMac.local>
2024-09-29 23:02:52 -0400

    Quick update

--- app/Console/Commands/FixVideos.php
+++ app/Console/Commands/FixVideos.php
@@ -39,7 +39,7 @@ class FixVideos extends Command
      */
     public function handle()
     {
-        Piece::find(359)->tutorials()->first()->id;
+        $this->info(Piece::find(359)->tutorials()->first()->id);
         // $tutorial = Tutorial::whereIn('type', ['synthesia', 'performance'])->take(2)->get()->last();
 
         // dd($tutorial->video_url);
