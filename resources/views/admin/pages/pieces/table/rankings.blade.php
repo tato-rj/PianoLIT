d5df6be5cb145ef3f96f160df0578a6ad5aa26bf
Arthur Villar <arthur@iMac.local>
2025-11-16 23:39:28 -0500

    Quick update

--- resources/views/admin/pages/pieces/table/rankings.blade.php
+++ resources/views/admin/pages/pieces/table/rankings.blade.php
@@ -1,14 +1,8 @@
-@if($item->highlighted_at)
-<span>Already highlighted</span>
-{{-- <span class="text-green">{{$item->highlighted_at->toFormattedDateString()}}</span> --}}
-@else
-<span class="text-muted"><i>Never highlighted</i></span>
-@endif
-{{-- <div class="d-flex">
+<div class="d-flex">
 @if($rcm = $item->getRanking('rcm'))
 <div class="mr-1 badge badge-pill alert-blue"><strong>RCM {{$rcm}}</strong></div>
 @endif
 @if($abrsm = $item->getRanking('abrsm'))
 <div class="badge badge-pill alert-blue"><strong>ABRSM {{$abrsm}}</strong></div>
 @endif
-</div> --}}
+</div>
